a3449284c3bffadb25b168e9ea215d5b42950cbe
jihyeok-um <75592315+jihyeok-um@users.noreply.github.com>
2022-07-05 21:07:44 +0900

    chore: 리액트 개발 환경 구축
    Co-authored-by: kyoul <sion11290805@gmail.com>

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,21 @@
+export default {
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {},
+};
---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => {
+  const hello = 'qwer' as String;
+
+  return <div>{hello}</div>;
+};
+
+export default App;

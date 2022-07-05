4ec33af0f32712677cfb1bec8dfc134503f16f74
2yujeong <yjfflq4872@gmail.com>
2022-07-05 16:58:21 +0900

    feat: 백엔드 초기 환경 구성

---
+++ backend/levellog/src/main/java/com/woowacourse/levellog/LevellogApplication.java
@@ -0,0 +1,13 @@
+package com.woowacourse.levellog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevellogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LevellogApplication.class, args);
+	}
+
+}
---
+++ backend/levellog/src/test/java/com/woowacourse/levellog/LevellogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woowacourse.levellog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevellogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

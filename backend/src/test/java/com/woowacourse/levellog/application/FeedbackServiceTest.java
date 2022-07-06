5baf310cae41301084dbe2e83ee7d1adad041cc6
SuyeonChoi <tndus970120@gmail.com>
2022-07-06 19:07:28 +0900

    feat: 피드백 전체 조회, 삭제 기능 구현 (#17)
    * feat: FeedbackService 전체 조회 기능 구현
    
    Co-authored-by: nailseong <ilseongdev@gmail.com>
    Co-authored-by: kbsat <kbsat@naver.com>
    Co-authored-by: 2yujeong <yjfflq4872@gmail.com>
    Co-authored-by: ozragwort <ozragwort@gmail.com>
    
    * feat: FeedbackController 전체 조회 기능 구현
    
    Co-authored-by: nailseong <ilseongdev@gmail.com>
    Co-authored-by: kbsat <kbsat@naver.com>
    Co-authored-by: 2yujeong <yjfflq4872@gmail.com>
    Co-authored-by: ozragwort <ozragwort@gmail.com>
    
    * test: Feedback 전체 조회 시나리오 테스트
    
    Co-authored-by: nailseong <ilseongdev@gmail.com>
    Co-authored-by: kbsat <kbsat@naver.com>
    Co-authored-by: 2yujeong <yjfflq4872@gmail.com>
    Co-authored-by: ozragwort <ozragwort@gmail.com>
    
    * feat: FeedbackService 삭제 기능 구현
    
    Co-authored-by: nailseong <ilseongdev@gmail.com>
    Co-authored-by: kbsat <kbsat@naver.com>
    Co-authored-by: 2yujeong <yjfflq4872@gmail.com>
    Co-authored-by: ozragwort <ozragwort@gmail.com>
    
    * feat: FeedbackController 삭제 기능 구현
    
    Co-authored-by: nailseong <ilseongdev@gmail.com>
    Co-authored-by: kbsat <kbsat@naver.com>
    Co-authored-by: 2yujeong <yjfflq4872@gmail.com>
    Co-authored-by: ozragwort <ozragwort@gmail.com>
    
    * test: Feedback 삭제 시나리오 테스트
    
    Co-authored-by: nailseong <ilseongdev@gmail.com>
    Co-authored-by: kbsat <kbsat@naver.com>
    Co-authored-by: 2yujeong <yjfflq4872@gmail.com>
    Co-authored-by: ozragwort <ozragwort@gmail.com>
    
    Co-authored-by: nailseong <ilseongdev@gmail.com>
    Co-authored-by: kbsat <kbsat@naver.com>
    Co-authored-by: 2yujeong <yjfflq4872@gmail.com>
    Co-authored-by: ozragwort <ozragwort@gmail.com>

--- backend/src/test/java/com/woowacourse/levellog/application/FeedbackServiceTest.java
+++ backend/src/test/java/com/woowacourse/levellog/application/FeedbackServiceTest.java
@@ -6,6 +6,7 @@ import com.woowacourse.levellog.domain.Feedback;
 import com.woowacourse.levellog.domain.FeedbackRepository;
 import com.woowacourse.levellog.dto.FeedbackContentDto;
 import com.woowacourse.levellog.dto.FeedbackCreateRequest;
+import com.woowacourse.levellog.dto.FeedbacksResponse;
 import java.util.Optional;
 import javax.transaction.Transactional;
 import org.junit.jupiter.api.DisplayName;
@@ -15,6 +16,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 @Transactional
+@DisplayName("FeedbackService의")
 class FeedbackServiceTest {
 
     @Autowired
@@ -24,7 +26,7 @@ class FeedbackServiceTest {
     private FeedbackRepository feedbackRepository;
 
     @Test
-    @DisplayName("피드백을 저장한다.")
+    @DisplayName("save 메서드는 피드백을 저장한다.")
     void save() {
         // given
         final FeedbackContentDto feedbackContentDto = new FeedbackContentDto("Spring에 대한 학습을 충분히 하였습니다.",
@@ -38,5 +40,35 @@ class FeedbackServiceTest {
         // then
         final Optional<Feedback> feedback = feedbackRepository.findById(id);
         assertThat(feedback).isPresent();
+    }
+
+    @Test
+    @DisplayName("findAll 메서드는 모든 피드백을 조회한다.")
+    void findAll() {
+        // given
+        feedbackRepository.save(new Feedback("로마", "로마 스터디", "로마 말하기", "로마 기타"));
+        feedbackRepository.save(new Feedback("알린", "알린 스터디", "알린 말하기", "알린 기타"));
+
+        // when
+        final FeedbacksResponse feedbacksResponse = feedbackService.findAll();
+
+        // then
+        assertThat(feedbacksResponse.getFeedbacks()).hasSize(2);
+    }
+
+    @Test
+    @DisplayName("delete 메서드는 피드백을 삭제한다.")
+    void delete() {
+        // given
+        feedbackRepository.save(new Feedback("로마", "로마 스터디", "로마 말하기", "로마 기타"));
+        final Feedback savedFeedback = feedbackRepository.save(new Feedback("알린", "알린 스터디", "알린 말하기", "알린 기타"));
+        final Long id = savedFeedback.getId();
+
+        // when
+        feedbackService.deleteById(id);
+
+        // then
+        final Optional<Feedback> deletedFeedback = feedbackRepository.findById(id);
+        assertThat(deletedFeedback).isEmpty();
     }
 }

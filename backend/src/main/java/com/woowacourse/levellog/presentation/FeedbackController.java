681dbbb3464b29b66ef057f0c51ac28bb81f3ba2
SuyeonChoi <tndus970120@gmail.com>
2022-07-06 18:36:20 +0900

    feat: FeedbackController 전체 조회 기능 구현
    Co-authored-by: nailseong <ilseongdev@gmail.com>
    Co-authored-by: kbsat <kbsat@naver.com>
    Co-authored-by: 2yujeong <yjfflq4872@gmail.com>
    Co-authored-by: ozragwort <ozragwort@gmail.com>

--- backend/src/main/java/com/woowacourse/levellog/presentation/FeedbackController.java
+++ backend/src/main/java/com/woowacourse/levellog/presentation/FeedbackController.java
@@ -2,10 +2,12 @@ package com.woowacourse.levellog.presentation;
 
 import com.woowacourse.levellog.application.FeedbackService;
 import com.woowacourse.levellog.dto.FeedbackCreateRequest;
+import com.woowacourse.levellog.dto.FeedbacksResponse;
 import java.net.URI;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +24,11 @@ public class FeedbackController {
     public ResponseEntity<Void> save(@RequestBody @Valid final FeedbackCreateRequest request) {
         feedbackService.save(request);
         return ResponseEntity.created(URI.create("/api/feedbacks")).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<FeedbacksResponse> findAll() {
+        final FeedbacksResponse response = feedbackService.findAll();
+        return ResponseEntity.ok(response);
     }
 }

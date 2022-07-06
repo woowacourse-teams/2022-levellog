c9fd098d780493cbfa70a1518dfe0ec4916112f9
jihyeok-um <75592315+jihyeok-um@users.noreply.github.com>
2022-07-06 13:51:36 +0900

    feat: `등록!` 버튼을 클릭하여 `FeedbackList` 페이지로 이동

--- frontend/src/pages/FeedbackAdd.tsx
+++ frontend/src/pages/FeedbackAdd.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { ROUTES_PATH } from '../constants/constants';
 
 const FeedbackAdd = () => {
-  return <div>피드백 등록 페이지</div>;
+  return (
+    <div>
+      <h2>피드백 등록 페이지</h2>
+      <Link to={ROUTES_PATH.HOME}>
+        <button>등록!</button>
+      </Link>
+    </div>
+  );
 };
 
 export default FeedbackAdd;

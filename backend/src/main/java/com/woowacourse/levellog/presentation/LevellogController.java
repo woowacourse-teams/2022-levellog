dcfb808155e21c06d01be15965e070678692a7a0
nailseong <nnis9857@gmail.com>
2022-07-12 16:41:44 +0900

    feat: LevellogController 상세 조회 기능 구현

--- backend/src/main/java/com/woowacourse/levellog/presentation/LevellogController.java
+++ backend/src/main/java/com/woowacourse/levellog/presentation/LevellogController.java
@@ -2,10 +2,13 @@ package com.woowacourse.levellog.presentation;
 
 import com.woowacourse.levellog.application.LevellogService;
 import com.woowacourse.levellog.dto.LevellogCreateRequest;
+import com.woowacourse.levellog.dto.LevellogResponse;
 import java.net.URI;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +25,11 @@ public class LevellogController {
     public ResponseEntity<Void> create(@RequestBody @Valid final LevellogCreateRequest request) {
         final Long id = levellogService.save(request);
         return ResponseEntity.created(URI.create("/api/levellogs/" + id)).build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<LevellogResponse> find(@PathVariable final Long id) {
+        final LevellogResponse response = levellogService.find(id);
+        return ResponseEntity.ok(response);
     }
 }

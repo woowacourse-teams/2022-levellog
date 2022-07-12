420fe01e17ed2f4004205951e22a3907640297ca
nailseong <nnis9857@gmail.com>
2022-07-12 19:53:26 +0900

    feat: LogAspect 메서드 실행 시간 측정 로그 추가

---
+++ backend/src/main/java/com/woowacourse/levellog/aspect/LogAspect.java
@@ -0,0 +1,28 @@
+package com.woowacourse.levellog.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+@Slf4j
+public class LogAspect {
+
+    @Around("execution(* com.woowacourse.levellog..*(..))")
+    public Object logExecutionTime(final ProceedingJoinPoint joinPoint) throws Throwable {
+        final long startTime = System.currentTimeMillis();
+        final Object result;
+        result = joinPoint.proceed();
+        final long endTime = System.currentTimeMillis();
+
+        final long executionTimeMillis = endTime - startTime;
+        final String declaringTypeName = joinPoint.getSignature()
+                .getDeclaringTypeName();
+        log.info("{} took {}ms", declaringTypeName, executionTimeMillis);
+        return result;
+
+    }
+}

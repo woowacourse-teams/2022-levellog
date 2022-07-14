b3403052917896a2dd25d6e545cab7c02c3a398c
nailseong <nnis9857@gmail.com>
2022-07-14 14:39:56 +0900

    feat: 500번 예외 핸들러 로그 출력 기능

--- backend/src/main/java/com/woowacourse/levellog/exception/ControllerAdvice.java
+++ backend/src/main/java/com/woowacourse/levellog/exception/ControllerAdvice.java
@@ -1,6 +1,7 @@
 package com.woowacourse.levellog.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -13,21 +14,22 @@ public class ControllerAdvice {
 
     @ExceptionHandler(LevellogException.class)
     public ResponseEntity<ExceptionResponse> handleLevellogException(final LevellogException e) {
-        return toResponseEntity(e, e.getHttpStatus());
+        return toResponseEntity(e.getMessage(), e.getHttpStatus());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ExceptionResponse> handleValidationException(final MethodArgumentNotValidException e) {
-        return toResponseEntity(e, HttpStatus.BAD_REQUEST);
+        return toResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ExceptionResponse> handleUnexpectedException(final Exception e) {
-        return toResponseEntity(e, HttpStatus.INTERNAL_SERVER_ERROR);
+        log.warn("[{}]", MDC.get("correlationId"), e);
+        return toResponseEntity("예상치 못한 예외가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    private ResponseEntity<ExceptionResponse> toResponseEntity(final Exception e, final HttpStatus httpStatus) {
-        final ExceptionResponse response = new ExceptionResponse(e.getMessage());
+    private ResponseEntity<ExceptionResponse> toResponseEntity(final String message, final HttpStatus httpStatus) {
+        final ExceptionResponse response = new ExceptionResponse(message);
         return ResponseEntity
                 .status(httpStatus)
                 .body(response);

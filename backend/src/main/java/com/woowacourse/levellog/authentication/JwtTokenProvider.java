78432ff8fcd1f26273cdba7912e180035a1e1e5e
BeomsooKim <kbsat@naver.com>
2022-07-14 15:16:42 +0900

    fix: JwtTokenProvider `@Component` 어노테이션 삽입
    Co-authored-by: SuyeonChoi <tndus970120@naver.com>

--- backend/src/main/java/com/woowacourse/levellog/authentication/JwtTokenProvider.java
+++ backend/src/main/java/com/woowacourse/levellog/authentication/JwtTokenProvider.java
@@ -9,7 +9,9 @@ import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import javax.crypto.SecretKey;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
 public class JwtTokenProvider {
 
     private final SecretKey secretKey;

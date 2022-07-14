74b6dcf89e4f2e2637a474eb9b7b77fdc4455810
BeomsooKim <kbsat@naver.com>
2022-07-14 11:22:53 +0900

    feat: GithubOAuthClient 구현
    Co-authored-by: SuyeonChoi <tndus970120@naver.com>

---
+++ backend/src/main/java/com/woowacourse/levellog/authentication/application/GithubOAuthClient.java
@@ -0,0 +1,59 @@
+package com.woowacourse.levellog.authentication.application;
+
+import com.woowacourse.levellog.authentication.dto.GithubAccessTokenRequest;
+import com.woowacourse.levellog.authentication.dto.GithubAccessTokenResponse;
+import com.woowacourse.levellog.authentication.dto.GithubProfileResponse;
+import java.util.Objects;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class GithubOAuthClient {
+
+    private static final String TOKEN_URL = "https://github.com/login/oauth/access_token";
+    private static final String USER_ACCESS_URL = "https://github.com/user";
+
+    private final String clientId;
+    private final String clientSecret;
+
+    public GithubOAuthClient(@Value("${security.github.client-id}") final String clientId,
+                             @Value("${security.github.client-secret}") final String clientSecret) {
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
+    }
+
+    public String getGithubAccessToken(final String code) {
+        final GithubAccessTokenRequest githubAccessTokenRequest = new GithubAccessTokenRequest(clientId, clientSecret,
+                code);
+        final HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
+
+        final HttpEntity<GithubAccessTokenRequest> httpEntity =
+                new HttpEntity<>(githubAccessTokenRequest, headers);
+
+        final GithubAccessTokenResponse response = new RestTemplate()
+                .exchange(TOKEN_URL, HttpMethod.POST, httpEntity, GithubAccessTokenResponse.class)
+                .getBody();
+
+        if (Objects.isNull(response)) {
+            throw new IllegalStateException("Github 요청에 실패했습니다.");
+        }
+
+        return response.getAccessToken();
+    }
+
+    public GithubProfileResponse getGithubProfile(final String accessToken) {
+        final HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.AUTHORIZATION, "token " + accessToken);
+
+        final HttpEntity<Void> httpEntity = new HttpEntity<>(headers);
+        return new RestTemplate()
+                .exchange(USER_ACCESS_URL, HttpMethod.GET, httpEntity, GithubProfileResponse.class)
+                .getBody();
+    }
+}
---
+++ backend/src/main/java/com/woowacourse/levellog/authentication/dto/GithubAccessTokenRequest.java
@@ -0,0 +1,21 @@
+package com.woowacourse.levellog.authentication.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@Getter
+public class GithubAccessTokenRequest {
+
+    @JsonProperty("client_id")
+    private String clientId;
+
+    @JsonProperty("client_secret")
+    private String clientSecret;
+
+    private String code;
+}

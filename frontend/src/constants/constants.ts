b76e7bda7407aa69b22b19f52afc3809307607fa
(yunjin) <sion11290805@gmail.com>
2022-07-14 09:54:05 +0900

    feat: 로그인 route 추가

--- frontend/src/constants/constants.ts
+++ frontend/src/constants/constants.ts
@@ -3,4 +3,5 @@ export const API_URL = 'http://52.79.235.135:8080';
 export const ROUTES_PATH = Object.freeze({
   HOME: '/',
   FEEDBACK_ADD: '/feedback/add',
+  LOGIN: '/login',
 });
--- frontend/src/routes/Routes.tsx
+++ frontend/src/routes/Routes.tsx
@@ -3,6 +3,8 @@ import { Routes, Route } from 'react-router-dom';
 
 import FeedbackList from '../pages/FeedbackList';
 import FeedbackAdd from '../pages/FeedbackAdd';
+import Login from '../pages/Login';
+
 import { ROUTES_PATH } from '../constants/constants';
 
 export const routes = [
@@ -13,5 +15,9 @@ export const routes = [
   {
     path: ROUTES_PATH.FEEDBACK_ADD,
     element: <FeedbackAdd />,
+  },
+  {
+    path: ROUTES_PATH.LOGIN,
+    element: <Login />,
   },
 ];

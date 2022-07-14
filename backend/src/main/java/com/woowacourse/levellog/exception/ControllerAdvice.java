eb3abe869cbdba971b227caa93192db9b5c25af6
nailseong <nnis9857@gmail.com>
2022-07-14 11:42:44 +0900

    feat: LevellogException을 처리하는 핸들러

---
+++ backend/src/main/java/com/woowacourse/levellog/exception/ControllerAdvice.java
@@ -0,0 +1,19 @@
+package com.woowacourse.levellog.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class ControllerAdvice {
+
+    @ExceptionHandler(LevellogException.class)
+    public ResponseEntity<ExceptionResponse> handleLevellogException(final LevellogException e) {
+        final ExceptionResponse response = new ExceptionResponse(e.getMessage());
+        return ResponseEntity
+                .status(e.getHttpStatus())
+                .body(response);
+    }
+}
---
+++ backend/src/main/java/com/woowacourse/levellog/exception/ExceptionResponse.java
@@ -0,0 +1,11 @@
+package com.woowacourse.levellog.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ExceptionResponse {
+
+    private final String message;
+}

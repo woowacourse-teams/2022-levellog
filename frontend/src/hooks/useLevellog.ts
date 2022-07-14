761a3d15042ecd78781a8d2ebd0887868cc2d7d5
jihyeok-um <75592315+jihyeok-um@users.noreply.github.com>
2022-07-14 10:49:07 +0900

    refactor: return 문 변경, 함수명 변경

--- frontend/src/hooks/useLevellog.ts
+++ frontend/src/hooks/useLevellog.ts
@@ -2,22 +2,23 @@ import { postLevellog } from '../api/levellog';
 import { LevellogType } from '../types';
 
 const useLevellog = () => {
-  const stringToLevellogFormat = (inputvalue: string) => {
+  const stringToLevellog = (inputvalue: string) => {
     const levellogContent: LevellogType = {
       content: inputvalue,
     };
+
     return levellogContent;
   };
 
   const levellogAdd = async (inputvalue: string) => {
     try {
-      await postLevellog(stringToLevellogFormat(inputvalue));
+      await postLevellog(stringToLevellog(inputvalue));
     } catch (err) {
       console.log(err);
     }
   };
 
-  return { stringToLevellogFormat, levellogAdd };
+  return { levellogAdd };
 };
 
 export default useLevellog;

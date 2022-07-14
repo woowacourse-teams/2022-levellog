60e7f53a38923c50ac5db09d4a833f8cf7475f7e
(yunjin) <sion11290805@gmail.com>
2022-07-14 14:39:36 +0900

    feat: 프로필 dropdown 추가

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,29 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import { useUserState } from '../hooks/useContext';
+
 import Button from './@commons/Button';
 
-const Header = () => {
-  const profileUrl = useUserState();
+import { useUserState } from '../hooks/useContext';
 
-  const handleClickLoginButton = () => {
-    window.location.href =
-      'https://github.com/login/oauth/authorize?client_id=fc4c9ab6e6d189931371&redirect_uri=http://localhost:3000/login';
-  };
+import ProfileDropdown from './ProfileDropdown';
+
+const Header = () => {
+  const [isProfileDropdownShow, setIsProfileDropdownShow] = useState(false);
+  const profileUrl = useUserState();
 
   const handleErrorProfileImage = (e: React.SyntheticEvent<EventTarget>) => {
     const target = e.target as HTMLImageElement;
     target.src = 'http://localhost:3000/img/defaultProfile.png';
   };
 
+  const handleClickLoginButton = () => {
+    window.location.href =
+      'https://github.com/login/oauth/authorize?client_id=fc4c9ab6e6d189931371&redirect_uri=http://localhost:3000/login';
+  };
+
+  const handleClickProfileImage = () => {
+    setIsProfileDropdownShow((prev) => !prev);
+  };
+
   return (
     <HeaderStyle>
       <LogoStyle src={'http://localhost:3000/img/levellog-logo.png'} alt="레벨로그 로고" />
       {profileUrl ? (
-        <ProfileImgStyle onError={handleErrorProfileImage} src={profileUrl} alt="프로필 이미지" />
+        <ProfileImageStyle
+          onClick={handleClickProfileImage}
+          onError={handleErrorProfileImage}
+          src={profileUrl}
+          alt="프로필 이미지"
+        />
       ) : (
         <Button onClick={handleClickLoginButton}>로그인</Button>
       )}
+      <ProfileDropdown
+        isProfileDropdownShow={isProfileDropdownShow}
+        setIsProfileDropdownShow={setIsProfileDropdownShow}
+      />
     </HeaderStyle>
   );
 };
@@ -35,6 +53,7 @@ export const HeaderStyle = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  position: relative;
 `;
 
 export const LogoStyle = styled.img`
@@ -42,10 +61,11 @@ export const LogoStyle = styled.img`
   height: 42px;
 `;
 
-export const ProfileImgStyle = styled.img`
+export const ProfileImageStyle = styled.img`
   width: 44px;
   height: 44px;
   border-radius: 22px;
+  cursor: pointer;
 `;
 
 export default Header;

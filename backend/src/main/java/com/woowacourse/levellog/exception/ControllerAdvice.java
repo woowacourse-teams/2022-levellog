d5b6c8ec9a008d5eb27fb54d9390621e9ce82e86
nailseong <nnis9857@gmail.com>
2022-07-14 13:43:46 +0900

    refactor: 중복 코드 제거

--- backend/src/main/java/com/woowacourse/levellog/exception/ControllerAdvice.java
+++ backend/src/main/java/com/woowacourse/levellog/exception/ControllerAdvice.java
@@ -1,6 +1,7 @@
 package com.woowacourse.levellog.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,25 +13,23 @@ public class ControllerAdvice {
 
     @ExceptionHandler(LevellogException.class)
     public ResponseEntity<ExceptionResponse> handleLevellogException(final LevellogException e) {
-        final ExceptionResponse response = new ExceptionResponse(e.getMessage());
-        return ResponseEntity
-                .status(e.getHttpStatus())
-                .body(response);
+        return toResponseEntity(e, e.getHttpStatus());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ExceptionResponse> handleValidationException(final MethodArgumentNotValidException e) {
-        final ExceptionResponse response = new ExceptionResponse(e.getMessage());
-        return ResponseEntity
-                .badRequest()
-                .body(response);
+        return toResponseEntity(e, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ExceptionResponse> handleUnexpectedException(final Exception e) {
+        return toResponseEntity(e, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private ResponseEntity<ExceptionResponse> toResponseEntity(final Exception e, final HttpStatus httpStatus) {
         final ExceptionResponse response = new ExceptionResponse(e.getMessage());
         return ResponseEntity
-                .internalServerError()
+                .status(httpStatus)
                 .body(response);
     }
 }

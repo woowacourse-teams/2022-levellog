92c27480f5720d5d68dd18144e3fd0969c6b672c
BeomsooKim <kbsat@naver.com>
2022-07-15 13:45:03 +0900

    refactor: ControllerTest를 상속 받는 구조로 변경
    Co-authored-by: SuyeonChoi <tndus970120@naver.com>

--- backend/src/test/java/com/woowacourse/levellog/authentication/presentation/OAuthControllerTest.java
+++ backend/src/test/java/com/woowacourse/levellog/authentication/presentation/OAuthControllerTest.java
@@ -4,40 +4,17 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.woowacourse.levellog.application.MemberService;
-import com.woowacourse.levellog.authentication.application.OAuthService;
-import com.woowacourse.levellog.authentication.domain.JwtTokenProvider;
 import com.woowacourse.levellog.authentication.dto.GithubCodeRequest;
+import com.woowacourse.levellog.support.ControllerTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
-@WebMvcTest(OAuthController.class)
 @DisplayName("OAuthController의")
-class OAuthControllerTest {
-
-    @MockBean
-    private MemberService memberService;
-
-    @MockBean
-    private JwtTokenProvider jwtTokenProvider;
-
-    @MockBean
-    private OAuthService oAuthService;
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Autowired
-    private ObjectMapper objectMapper;
+class OAuthControllerTest extends ControllerTest {
 
     @ParameterizedTest
     @ValueSource(strings = {" "})
---
+++ backend/src/test/java/com/woowacourse/levellog/support/ControllerTest.java
@@ -0,0 +1,39 @@
+package com.woowacourse.levellog.support;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.woowacourse.levellog.application.FeedbackService;
+import com.woowacourse.levellog.application.MemberService;
+import com.woowacourse.levellog.authentication.application.OAuthService;
+import com.woowacourse.levellog.authentication.domain.JwtTokenProvider;
+import com.woowacourse.levellog.authentication.presentation.OAuthController;
+import com.woowacourse.levellog.presentation.FeedbackController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest({
+        FeedbackController.class,
+        OAuthController.class
+})
+public abstract class ControllerTest {
+
+    @MockBean
+    protected MemberService memberService;
+
+    @MockBean
+    protected FeedbackService feedbackService;
+
+    @MockBean
+    private OAuthService oAuthService;
+
+    @MockBean
+    protected JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+}

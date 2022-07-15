0d8f7c2033d19a4e53a252844850fc04102d0378
(yunjin) <sion11290805@gmail.com>
2022-07-15 13:53:42 +0900

    chore: import 순서 컨벤션 추가
    Co-authored-by: jihyeok-um <jihyeok-um@users.noreply.github.com>

--- frontend/.prettierrc.js
+++ frontend/.prettierrc.js
@@ -1,6 +1,18 @@
 module.exports = {
   printWidth: 100,
   singleQuote: true,
-  trailingComma: "all",
-  endOfLine: "auto",
+  trailingComma: 'all',
+  endOfLine: 'auto',
+  importOrder: [
+    '^((?!/).)*$',
+    '^(@|components)(.*)$',
+    '^(pages)(.*)$',
+    '^(hooks)(.*|$)',
+    '^(api)(.*|$)',
+    '^(styles)(.*)$',
+    '^(routes)(.*)$',
+    '^(constants)(.*)$',
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
 };

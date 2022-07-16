6166b1473a3deee88c4080bfa0f2632467a5f98e
BeomsooKim <kbsat@naver.com>
2022-07-16 21:59:07 +0900

    style: MemberService 애노테이션 순서 변경
    Co-authored-by: SuyeonChoi <tndus970120@naver.com>

--- backend/src/main/java/com/woowacourse/levellog/application/MemberService.java
+++ backend/src/main/java/com/woowacourse/levellog/application/MemberService.java
@@ -10,8 +10,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@RequiredArgsConstructor
 @Transactional
+@RequiredArgsConstructor
 public class MemberService {
 
     private final MemberRepository memberRepository;

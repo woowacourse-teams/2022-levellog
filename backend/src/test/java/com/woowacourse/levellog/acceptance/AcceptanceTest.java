f7f09f7ea4392fc74be078f4a0be41e1cd0987b4
nailseong <nnis9857@gmail.com>
2022-07-17 17:18:44 +0900

    chore: spring rest docs 설정 코드 추가

--- backend/src/test/java/com/woowacourse/levellog/acceptance/AcceptanceTest.java
+++ backend/src/test/java/com/woowacourse/levellog/acceptance/AcceptanceTest.java
@@ -1,22 +1,34 @@
 package com.woowacourse.levellog.acceptance;
 
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.documentationConfiguration;
+
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@ExtendWith(RestDocumentationExtension.class)
 @ActiveProfiles("test")
-class AcceptanceTest {
+abstract class AcceptanceTest {
 
+    protected RequestSpecification specification;
     @LocalServerPort
     private int port;
 
     @BeforeEach
-    public void setUp() {
+    public void setUp(final RestDocumentationContextProvider contextProvider) {
         RestAssured.port = port;
+        specification = new RequestSpecBuilder()
+                .addFilter(documentationConfiguration(contextProvider))
+                .build();
     }
 }

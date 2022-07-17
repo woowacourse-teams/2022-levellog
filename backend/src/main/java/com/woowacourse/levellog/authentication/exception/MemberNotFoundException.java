01932a4edc9d1d4409675dc3f1d8dd02be646d29
BeomsooKim <kbsat@naver.com>
2022-07-17 18:21:20 +0900

    refactor: Auth 관련 예외를 LevellogException 상속 받도록 변경

--- backend/src/main/java/com/woowacourse/levellog/authentication/exception/MemberNotFoundException.java
+++ backend/src/main/java/com/woowacourse/levellog/authentication/exception/MemberNotFoundException.java
@@ -1,17 +1,20 @@
 package com.woowacourse.levellog.authentication.exception;
 
+import com.woowacourse.levellog.exception.LevellogException;
+import org.springframework.http.HttpStatus;
+
 /**
  * 존재하지 않는 멤버를 조회하는 경우 발생하는 예외입니다.
  */
-public class MemberNotFoundException extends RuntimeException {
+public class MemberNotFoundException extends LevellogException {
 
     private static final String ERROR_MESSAGE = "멤버가 존재하지 않습니다.";
 
-    public MemberNotFoundException(final String message) {
-        super(message);
+    public MemberNotFoundException() {
+        super(ERROR_MESSAGE, HttpStatus.BAD_REQUEST);
     }
 
-    public MemberNotFoundException() {
-        super(ERROR_MESSAGE);
+    public MemberNotFoundException(final String message) {
+        super(message, HttpStatus.BAD_REQUEST);
     }
 }

8c18fcbdd9f688e415bc61e13c04ae141fb44ffd
nailseong <nnis9857@gmail.com>
2022-07-18 19:54:00 +0900

    feat: 생성, 수정 시간을 기록하기 위한 MappedSuperclass

---
+++ backend/src/main/java/com/woowacourse/levellog/common/BaseTimeEntity.java
@@ -0,0 +1,24 @@
+package com.woowacourse.levellog.common;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+}

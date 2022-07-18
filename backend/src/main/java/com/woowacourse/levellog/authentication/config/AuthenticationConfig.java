5bb60ccc4f0e5ead2014a41a586e85efaf623468
SuyeonChoi <tndus970120@gmail.com>
2022-07-18 17:44:43 +0900

    feat: 멤버 전체 조회 시 인터셉터 적용

--- backend/src/main/java/com/woowacourse/levellog/authentication/config/AuthenticationConfig.java
+++ backend/src/main/java/com/woowacourse/levellog/authentication/config/AuthenticationConfig.java
@@ -29,7 +29,8 @@ public class AuthenticationConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
         registry.addInterceptor(new LoginInterceptor(jwtTokenProvider))
-                .excludePathPatterns("/**");
+                .excludePathPatterns("/api/auth/**", "/api/feedback/**", "/api/levellogs/**")
+                .addPathPatterns("/api/members");
     }
 
     @Override

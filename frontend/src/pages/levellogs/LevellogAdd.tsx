0e1006cd9df3aaa9a57b89da6af3cba88bee98d3
jihyeok-um <75592315+jihyeok-um@users.noreply.github.com>
2022-07-20 06:40:50 +0900

    chore: 불필요한 console.log 제거

--- frontend/src/pages/levellogs/LevellogAdd.tsx
+++ frontend/src/pages/levellogs/LevellogAdd.tsx
@@ -17,7 +17,6 @@ const LevellogAdd = () => {
 
   const handleSubmitLevellogForm = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(teamId);
     levellogAdd(teamId, levellogRef.current.value);
   };
 

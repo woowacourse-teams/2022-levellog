c27f5a7164dde5f26d711c8a807051b2f420b147
SuyeonChoi <tndus970120@gmail.com>
2022-07-19 16:57:21 +0900

    feat: ParticipantRepository 구현

---
+++ backend/src/main/java/com/woowacourse/levellog/domain/ParticipantRepository.java
@@ -0,0 +1,6 @@
+package com.woowacourse.levellog.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ParticipantRepository extends JpaRepository<Participant, Long> {
+}

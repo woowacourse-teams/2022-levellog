94c46442825f290c00ae0adee9b58085da9002b0
SuyeonChoi <tndus970120@gmail.com>
2022-07-19 16:57:01 +0900

    feat: Participant가 BaseEntity 상속 및 생성자 추가

--- backend/src/main/java/com/woowacourse/levellog/domain/Participant.java
+++ backend/src/main/java/com/woowacourse/levellog/domain/Participant.java
@@ -1,24 +1,20 @@
 package com.woowacourse.levellog.domain;
 
+import com.woowacourse.levellog.common.BaseEntity;
 import javax.persistence.Entity;
 import javax.persistence.ForeignKey;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Getter
-public class Participant {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class Participant extends BaseEntity {
 
     @ManyToOne
     @JoinColumn(nullable = false, foreignKey = @ForeignKey(name = "fk_participant_team"))

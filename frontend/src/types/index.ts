38e0d35798a90a98784832d906885220699d474e
(yunjin) <sion11290805@gmail.com>
2022-07-21 21:55:32 +0900

    feat: type 수정

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,6 +1,19 @@
 export interface FeedbackType {
   id: number;
-  name: string;
+  updatedAt: string;
+  from: {
+    id: number;
+    nickname: string;
+    profileUrl: string;
+  };
+  feedback: {
+    study: string;
+    speak: string;
+    etc: string;
+  };
+}
+
+export interface FeedbackPostType {
   feedback: {
     study: string;
     speak: string;

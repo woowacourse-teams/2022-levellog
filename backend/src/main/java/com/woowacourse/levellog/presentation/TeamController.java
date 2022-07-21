2765fb0e67d5777674fe754d5e3ccbc5d45ff438
SuyeonChoi <tndus970120@gmail.com>
2022-07-21 17:10:09 +0900

    feat: 팀 수정, 삭제 기능 구현 (#98)
    * feat: TeamController 팀 정보 수정 API 구현
    
    Co-authored-by: 2yujeong <yjfflq4872@gmail.com>
    
    * feat: TeamService deleteById 구현
    
    Co-authored-by: 2yujeong <yjfflq4872@gmail.com>
    
    * feat: TeamController delete 구현
    
    Co-authored-by: 2yujeong <yjfflq4872@gmail.com>
    
    * feat: 호스트가 아닌 멤버가 팀을 수정하는 경우 예외를 던지는 기능 구현
    
    Co-authored-by: 2yujeong <yjfflq4872@gmail.com>
    
    * refactor: 팀 삭제 메서드 인자 순서 변경
    
    Co-authored-by: 2yujeong <yjfflq4872@gmail.com>
    
    * fix: 팀에서 작성한 레벨로그만 조회하도록 기능 수정
    
    Co-authored-by: 2yujeong <yjfflq4872@gmail.com>
    
    * test: Team 인수테스트 작성
    
    - 팀 삭제 시 참가자도 삭제되도록 구현
    
    Co-authored-by: 2yujeong <yjfflq4872@gmail.com>
    
    * fix: host가 participant에 포함되도록 수정
    
    * test: assertAll을 사용하여 테스트 코드 리팩토링 및 깨지는 테스트 수정
    
    * docs: 팀 관련 adoc 문서 생성
    
    * feat: Team 도메인이 BaseEntity를 상속받도록 구현
    
    * fix: ParticipantsResponse id가 memberId를 반환하도록 변경
    
    * refactor: 사용하지 않는 ParticipantsResponse DTO 삭제
    
    * refactor: TeamService private 메서드가 하단에 위치하도록 변경
    
    Co-authored-by: 2yujeong <yjfflq4872@gmail.com>

--- backend/src/main/java/com/woowacourse/levellog/presentation/TeamController.java
+++ backend/src/main/java/com/woowacourse/levellog/presentation/TeamController.java
@@ -4,14 +4,17 @@ import com.woowacourse.levellog.application.TeamService;
 import com.woowacourse.levellog.authentication.support.LoginMember;
 import com.woowacourse.levellog.dto.TeamRequest;
 import com.woowacourse.levellog.dto.TeamResponse;
+import com.woowacourse.levellog.dto.TeamUpdateRequest;
 import com.woowacourse.levellog.dto.TeamsResponse;
 import java.net.URI;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,5 +43,19 @@ public class TeamController {
     public ResponseEntity<TeamResponse> findById(@PathVariable final Long id) {
         final TeamResponse response = teamService.findById(id);
         return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@PathVariable final Long id,
+                                       @RequestBody @Valid final TeamUpdateRequest request,
+                                       @LoginMember final Long memberId) {
+        teamService.update(id, request, memberId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable final Long id, @LoginMember final Long memberId) {
+        teamService.deleteById(id, memberId);
+        return ResponseEntity.noContent().build();
     }
 }

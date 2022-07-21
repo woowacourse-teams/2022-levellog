52c419af3bfa42d0bb046c80b9ca283fc57473d7
(yunjin) <sion11290805@gmail.com>
2022-07-21 22:12:50 +0900

    feat: api uri 수정

--- frontend/src/apis/teams.ts
+++ frontend/src/apis/teams.ts
@@ -5,11 +5,11 @@ import { InterviewTeamType } from 'types/index';
 export const getTeams = (): AxiosPromise<Record<'teams', InterviewTeamType[]>> =>
   axios({
     method: 'get',
-    url: '/api/teams',
+    url: 'https://levellog.app/api/teams',
   });
 
 export const getTeam = (teamId: string): AxiosPromise<InterviewTeamType> =>
   axios({
     method: 'get',
-    url: `/api/teams/${teamId}`,
+    url: `https://levellog.app/api/teams/${teamId}`,
   });

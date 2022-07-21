3f44b4d2a2595f47ab282c34839d39aedff6dbce
jihyeok-um <75592315+jihyeok-um@users.noreply.github.com>
2022-07-22 07:22:13 +0900

    fix: null 에러 재수정

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -12,7 +12,7 @@ import { TeamProvider } from 'contexts/teamContext';
 const main = () => {
   if (process.env.NODE_ENV === 'development') {
     const { worker } = require('./mocks/browser');
-    worker.start();
+    //worker.start();
   }
 
   const root = ReactDOM.createRoot(document.getElementById('root'));
--- frontend/src/pages/feedback/FeedbackList.tsx
+++ frontend/src/pages/feedback/FeedbackList.tsx
@@ -22,13 +22,13 @@ const FeedbackList = () => {
     setFeedbacks(res.feedbacks);
   };
 
+  if (!levellogId) {
+    return <h3>레벨로그가 작성되지 않아 피드백을 작성할 수 없습니다</h3>;
+  }
+
   useEffect(() => {
     requestFeedbackLookup();
   }, []);
-
-  if (!levellogId) {
-    <h3>레벨로그가 작성되지 않아 피드백을 작성할 수 없습니다</h3>;
-  }
 
   return (
     <>

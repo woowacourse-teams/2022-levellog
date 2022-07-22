d242c62049fd00f7c2ebde65a10a98384cab73de
(yunjin) <sion11290805@gmail.com>
2022-07-22 09:38:43 +0900

    fix: 피드백 수정

--- frontend/src/components/feedbacks/FeedbackForm.tsx
+++ frontend/src/components/feedbacks/FeedbackForm.tsx
@@ -12,19 +12,19 @@ const FeedbackForm = ({ feedbackRef, handleSubmitFeedbackForm }: any) => {
         <Input
           width="100%"
           height="300px"
-          inputRef={(el: HTMLInputElement) => (feedbackRef.current[1] = el)}
+          inputRef={(el: HTMLInputElement) => (feedbackRef.current[0] = el)}
         />
         <label>인터뷰, 말하기 측면에서 좋은 점과 개선할 부분은?</label>
         <Input
           width="100%"
           height="300px"
-          inputRef={(el: HTMLInputElement) => (feedbackRef.current[2] = el)}
+          inputRef={(el: HTMLInputElement) => (feedbackRef.current[1] = el)}
         />
         <label>기타 피드백 (위 2 질문 외에 다른 피드백도 주세요.)</label>
         <Input
           width="100%"
           height="200px"
-          inputRef={(el: HTMLInputElement) => (feedbackRef.current[3] = el)}
+          inputRef={(el: HTMLInputElement) => (feedbackRef.current[2] = el)}
         />
       </FormStyle>
     </FeedbackContainer>

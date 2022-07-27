11c58e1bbfa43ce3987cb0cec310b8cbbb6bb765
2yujeong <yjfflq4872@gmail.com>
2022-07-27 12:25:57 +0900

    test: 피드백 수정 인수 테스트 작성

--- backend/src/test/java/com/woowacourse/levellog/acceptance/FeedbackAcceptanceTest.java
+++ backend/src/test/java/com/woowacourse/levellog/acceptance/FeedbackAcceptanceTest.java
@@ -122,13 +122,80 @@ class FeedbackAcceptanceTest extends AcceptanceTest {
     }
 
     /*
-     * Scenario: 피드백 삭제
+     * Scenario: 피드백 수정
      *   given: 피드백이 등록되어 있다.
-     *   given: 피드백을 모두 조회한다.
-     *   when: 특정 피드백을 삭제한다.
+     *   given: 등록된 피드백을 조회한다.
+     *   when: 조회한 피드백 내용을 수정한다.
      *   then: 204 No Content 상태 코드를 응답받는다.
      */
+    @Test
+    @DisplayName("피드백 수정")
+    void updateFeedback() {
+        // given
+        final RestAssuredResponse hostLoginResponse = login("릭");
+        final Long rick_id = hostLoginResponse.getMemberId();
+        final String rickToken = hostLoginResponse.getToken();
 
+        final RestAssuredResponse romaLoginResponse = login("로마");
+        final Long roma_id = romaLoginResponse.getMemberId();
+        final String romaToken = romaLoginResponse.getToken();
+
+        final String teamId = requestCreateTeam("릭 and 로마", rickToken, rick_id, roma_id).getTeamId();
+
+        final LevellogRequest levellogRequest = new LevellogRequest("레벨로그");
+        final String rick_levellogId = RestAssuredTemplate.post("/api/teams/" + teamId + "/levellogs", rickToken, levellogRequest)
+                .getLevellogId();
+
+        final FeedbackContentDto givenFeedbackContentDto = new FeedbackContentDto(
+                "Spring에 대한 학습을 충분히 하였습니다.", "아이 컨텍이 좋습니다.", "윙크하지 마세요.");
+        final CreateFeedbackDto givenRequest = new CreateFeedbackDto(givenFeedbackContentDto);
+
+        RestAssured.given(specification).log().all()
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + romaToken)
+                .body(givenRequest)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .filter(document("feedback/save"))
+                .when()
+                .post("/api/levellogs/{levellogId}/feedbacks", rick_levellogId)
+                .then().log().all();
+
+        final Long updateId = requestFindAllFeedbacks(rick_levellogId)
+                .extract()
+                .as(FeedbacksDto.class)
+                .getFeedbacks()
+                .get(0)
+                .getId();
+
+        // when
+        final FeedbackContentDto feedbackContentDto = new FeedbackContentDto(
+                "수정된 Study 피드백", "수정된 Speak 피드백", "수정된 Etc 피드백");
+        final CreateFeedbackDto request = new CreateFeedbackDto(feedbackContentDto);
+
+        final ValidatableResponse response = RestAssured.given(specification).log().all()
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + romaToken)
+                .body(request)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .filter(document("feedback/update"))
+                .when()
+                .put("/api/levellogs/{levellogId}/feedbacks/{feedbackId}", rick_levellogId, updateId)
+                .then().log().all();
+
+        // then
+        response.statusCode(HttpStatus.NO_CONTENT.value());
+        requestFindAllFeedbacks(rick_levellogId)
+                .body("feedbacks.feedback.study", contains("수정된 Study 피드백"),
+                "feedbacks.feedback.speak", contains("수정된 Speak 피드백"),
+                "feedbacks.feedback.etc", contains("수정된 Etc 피드백")
+        );
+    }
+
+    /*
+     * Scenario: 피드백 삭제
+     *   given: 피드백이 등록되어 있다.
+     *   given: 등록된 피드백을 조회한다.
+     *   when: 조회한 피드백을 삭제한다.
+     *   then: 204 No Content 상태 코드를 응답받는다.
+     */
     @Test
     @DisplayName("피드백 삭제")
     void deleteFeedback() {

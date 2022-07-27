376cceb9dadf3c92e30856c16abb0122ce668250
BeomsooKim <kbsat@naver.com>
2022-07-27 17:25:33 +0900

    refactor: Member DTO에 equals, hashCode 애노테이션 삽입

--- backend/src/main/java/com/woowacourse/levellog/member/dto/MemberCreateDto.java
+++ backend/src/main/java/com/woowacourse/levellog/member/dto/MemberCreateDto.java
@@ -2,12 +2,14 @@ package com.woowacourse.levellog.member.dto;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor
 @Getter
+@EqualsAndHashCode
 public class MemberCreateDto {
 
     private String nickname;
--- backend/src/main/java/com/woowacourse/levellog/member/dto/MembersDto.java
+++ backend/src/main/java/com/woowacourse/levellog/member/dto/MembersDto.java
@@ -3,12 +3,14 @@ package com.woowacourse.levellog.member.dto;
 import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor
+@EqualsAndHashCode
 public class MembersDto {
 
     private List<MemberDto> members;

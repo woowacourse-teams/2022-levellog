5b77e375accdc24ed37bca261901c029ff915f18
BeomsooKim <kbsat@naver.com>
2022-07-27 13:48:51 +0900

    feat: Member 도메인 검증 로직 추가

--- backend/src/main/java/com/woowacourse/levellog/member/domain/Member.java
+++ backend/src/main/java/com/woowacourse/levellog/member/domain/Member.java
@@ -7,18 +7,17 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
 @Getter
 @Table(uniqueConstraints = {@UniqueConstraint(name = "uk_member_github_id", columnNames = {"githubId"})})
 public class Member extends BaseEntity {
 
-    public static final int NICKNAME_MAX_LENGTH = 50;
+    private static final int NICKNAME_MAX_LENGTH = 50;
+    private static final int PROFILE_URL_MAX_LENGTH = 2048;
 
     @Column(nullable = false, length = 50)
     private String nickname;
@@ -29,25 +28,52 @@ public class Member extends BaseEntity {
     @Column(nullable = false, length = 2048)
     private String profileUrl;
 
-    public void updateNickname(final String nickname) {
-        validateNickname(nickname);
+    public Member(final String nickname, final Integer githubId, final String profileUrl) {
         this.nickname = nickname;
+        this.githubId = githubId;
+        this.profileUrl = profileUrl;
+
+        validateNickname(this.nickname);
+        validateGithubId(this.githubId);
+        validateProfileUrl(this.profileUrl);
+    }
+
+    public void updateNickname(final String nickname) {
+        this.nickname = nickname;
+        validateNickname(this.nickname);
     }
 
     private void validateNickname(final String nickname) {
-        validateBlank(nickname);
-        validateNicknameLength(nickname);
+        checkBlank(nickname, "닉네임은 공백이나 null일 수 없습니다.");
+        checkLength(nickname, NICKNAME_MAX_LENGTH,
+                "닉네임은 " + NICKNAME_MAX_LENGTH + "자 이하여야합니다. [현재 길이:" + nickname.length() + "]");
     }
 
-    private void validateBlank(final String nickname) {
-        if (nickname == null || nickname.isBlank()) {
-            throw new InvalidFieldException("닉네임은 공백이나 null일 수 없습니다.");
+    private void validateGithubId(final Integer githubId) {
+        checkNull(githubId, "깃허브 ID는 null일 수 없습니다.");
+    }
+
+    private void validateProfileUrl(final String profileUrl) {
+        checkBlank(profileUrl, "프로필 url은 공백이나 null일 수 없습니다.");
+        checkLength(profileUrl, PROFILE_URL_MAX_LENGTH,
+                "프로필 url은 " + PROFILE_URL_MAX_LENGTH + "자 이하여야합니다. [현재 길이:" + profileUrl.length() + "]");
+    }
+
+    private void checkLength(final String target, final int length, final String message) {
+        if (target.length() > length) {
+            throw new InvalidFieldException(message);
         }
     }
 
-    private void validateNicknameLength(final String nickname) {
-        if (nickname.length() > NICKNAME_MAX_LENGTH) {
-            throw new InvalidFieldException("닉네임은 " + NICKNAME_MAX_LENGTH + "자 이하여야합니다.");
+    private void checkBlank(final String target, final String message) {
+        if (target == null || target.isBlank()) {
+            throw new InvalidFieldException(message);
+        }
+    }
+
+    private void checkNull(final Object target, final String message) {
+        if (target == null) {
+            throw new InvalidFieldException(message);
         }
     }
 }

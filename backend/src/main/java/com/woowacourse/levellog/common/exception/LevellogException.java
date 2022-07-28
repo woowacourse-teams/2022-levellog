5688f2f54dd69f60ba271fc32b126da527b70ef1
nailseong <nnis9857@gmail.com>
2022-07-28 14:13:10 +0900

    chore: deprecated 처리

--- backend/src/main/java/com/woowacourse/levellog/common/exception/LevellogException.java
+++ backend/src/main/java/com/woowacourse/levellog/common/exception/LevellogException.java
@@ -3,13 +3,16 @@ package com.woowacourse.levellog.common.exception;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
-// TODO: 2022/07/26 clientMessage 필드 추가 & ControllerAdvice 수정
+// TODO: 2022/07/26 clientMessage 필드 추가 ControllerAdvice 수정
+// TODO: 2022/07/26 ControllerAdvice 수정
 @Getter
 public abstract class LevellogException extends RuntimeException {
 
     private final HttpStatus httpStatus;
     private final String clientMessage;
 
+    // TODO: 2022/07/28 삭제되어야함 
+    @Deprecated
     protected LevellogException(final String message, final HttpStatus httpStatus) {
         super(message);
         this.httpStatus = httpStatus;

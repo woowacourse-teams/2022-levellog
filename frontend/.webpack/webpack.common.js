85192d37241491b7182719bb63d6cac1d6108543
(yunjin) <sion11290805@gmail.com>
2022-07-28 09:57:55 +0900

    chore: 배포 환경 분리

--- frontend/.webpack/webpack.common.js
+++ frontend/.webpack/webpack.common.js
@@ -1,7 +1,74 @@
+// const webpack = require('webpack');
+// const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// const Dotenv = require('dotenv-webpack');
+// require('dotenv').config();
+//
+// module.exports = {
+//   entry: '../src/index.tsx',
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(ts|tsx)$/,
+//         use: ['babel-loader', 'ts-loader'],
+//       },
+//       {
+//         test: /\.(png|jpe?g|gif|svg)$/,
+//         use: [
+//           {
+//             loader: 'file-loader',
+//             options: {
+//               name: 'assets/images/[name].[ext]',
+//             },
+//           },
+//         ],
+//       },
+//       {
+//         test: /\.(woff|woff2|eot|ttf|otf)$/,
+//         use: [
+//           {
+//             loader: 'file-loader',
+//             options: {
+//               name: 'assets/fonts/[name].[ext]',
+//             },
+//           },
+//         ],
+//       },
+//       {
+//         test: /\.css$/,
+//         use: ['style-loader', 'css-loader'],
+//       },
+//     ],
+//   },
+//   resolve: {
+//     modules: [path.resolve(__dirname, '../src'), 'node_modules'],
+//     extensions: ['.tsx', '.ts', '.js'],
+//   },
+//   output: {
+//     path: path.resolve(__dirname, '../dist'),
+//     filename: 'bundle.js',
+//     publicPath: '/',
+//   },
+//   plugins: [
+//     new Dotenv({
+//       path: '../env/development.env',
+//       safe: true,
+//     }),
+//     new HtmlWebpackPlugin({ template: 'public/index.html' }),
+//     new CleanWebpackPlugin(),
+//     new webpack.EnvironmentPlugin({
+//       API_URI: 'http://localhost:3000/api/',
+//       CLIENT_ID: false,
+//     }),
+//   ],
+// };
+
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
+const Dotenv = require('dotenv-webpack');
+require('dotenv').config();
 module.exports = {
   entry: '../src/index.tsx',
   module: {
@@ -47,5 +114,9 @@ module.exports = {
     filename: 'bundle.js',
     publicPath: '/',
   },
-  plugins: [new HtmlWebpackPlugin({ template: 'public/index.html' }), new CleanWebpackPlugin()],
+  plugins: [
+    new Dotenv(),
+    new HtmlWebpackPlugin({ template: 'public/index.html' }),
+    new CleanWebpackPlugin(),
+  ],
 };

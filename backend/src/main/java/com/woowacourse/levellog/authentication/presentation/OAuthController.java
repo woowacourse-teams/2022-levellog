95b6319148df4d62d53c3fc21c4fbddde120f6d8
nailseong <nnis9857@gmail.com>
2022-07-28 14:10:44 +0900

    refactor: CrossOrigin 어노테이션 삭제

--- backend/src/main/java/com/woowacourse/levellog/authentication/presentation/OAuthController.java
+++ backend/src/main/java/com/woowacourse/levellog/authentication/presentation/OAuthController.java
@@ -7,7 +7,6 @@ import com.woowacourse.levellog.authentication.support.PublicAPI;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +20,6 @@ public class OAuthController {
 
     private final OAuthService oAuthService;
 
-    @CrossOrigin
     @PostMapping("/login")
     public ResponseEntity<LoginDto> login(@RequestBody @Valid final GithubCodeDto request) {
         return ResponseEntity.ok(oAuthService.login(request));

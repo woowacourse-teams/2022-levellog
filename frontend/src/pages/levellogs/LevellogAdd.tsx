abd5d9fb175e576c1136dda482223a78456404d9
(yunjin) <sion11290805@gmail.com>
2022-07-28 17:40:05 +0900

    refactor: 레벨로그 타입 추가
    Co-authored-by: jihyeok-um <jihyeok-um@users.noreply.github.com>

--- frontend/src/pages/levellogs/LevellogAdd.tsx
+++ frontend/src/pages/levellogs/LevellogAdd.tsx
@@ -1,16 +1,27 @@
-import { useParams } from 'react-router-dom';
+import React from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 
 import useLevellog from 'hooks/useLevellog';
+
+import { ROUTES_PATH } from 'constants/constants';
 
 import LevellogForm from 'components/levellogs/LevellogForm';
 
 const LevellogAdd = () => {
   const { levellogRef, onSubmitLevellogPostForm } = useLevellog();
   const { teamId } = useParams();
+  const navigate = useNavigate();
 
-  const handleSubmitLevellogForm = (e: any) => {
+  const handleSubmitLevellogForm = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    onSubmitLevellogPostForm({ teamId });
+    if (typeof teamId === 'string') {
+      onSubmitLevellogPostForm({ teamId });
+
+      return;
+    }
+
+    alert('잘못된 접근입니다.');
+    navigate(ROUTES_PATH.HOME);
   };
 
   return (
--- frontend/src/pages/levellogs/LevellogEdit.tsx
+++ frontend/src/pages/levellogs/LevellogEdit.tsx
@@ -1,21 +1,35 @@
-import { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 
 import useLevellog from 'hooks/useLevellog';
+
+import { ROUTES_PATH } from 'constants/constants';
 
 import LevellogForm from 'components/levellogs/LevellogForm';
 
 const LevellogEdit = () => {
   const { levellogRef, onSubmitLevellogEditForm, getLevellogOnRef } = useLevellog();
   const { teamId, levellogId } = useParams();
+  const navigate = useNavigate();
 
-  const handleSubmitLevellogForm = (e: any) => {
+  const handleSubmitLevellogForm = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    onSubmitLevellogEditForm({ teamId, levellogId });
+    if (typeof teamId === 'string' && typeof levellogId === 'string') {
+      onSubmitLevellogEditForm({ teamId, levellogId });
+    }
+
+    alert('잘못된 접근입니다.');
+    navigate(ROUTES_PATH.HOME);
   };
 
   useEffect(() => {
-    getLevellogOnRef({ teamId, levellogId });
+    if (typeof teamId === 'string' && typeof levellogId === 'string') {
+      getLevellogOnRef({ teamId, levellogId });
+      return;
+    }
+
+    alert('잘못된 접근입니다.');
+    navigate(ROUTES_PATH.HOME);
   }, []);
 
   return (

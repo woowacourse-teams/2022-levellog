a4e7a73964e3b5ad6b528fe018b7be550a89b199
(yunjin) <sion11290805@gmail.com>
2022-07-29 12:04:35 +0900

    refactor: 타입추가
    Co-authored-by: jihyeok-um <jihyeok-um@users.noreply.github.com>

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,26 +1,3 @@
-export interface FeedbackType {
-  id: number;
-  updatedAt: string;
-  from: {
-    id: number;
-    nickname: string;
-    profileUrl: string;
-  };
-  feedback: {
-    study: string;
-    speak: string;
-    etc: string;
-  };
-}
-
-export interface FeedbackPostType {
-  feedback: {
-    study: string;
-    speak: string;
-    etc: string;
-  };
-}
-
 export interface LevellogType {
   content: string;
 }
@@ -31,20 +8,3 @@ export interface UserInfoType {
 }
 
 export type ImageSizeType = 'HUGE' | 'LARGE' | 'MEDIUM' | 'SMALL';
-
-export interface InterviewTeamType {
-  id: string;
-  teamImage: string;
-  hostId: string;
-  title: string;
-  place: string;
-  startAt: string;
-  participants: ParticipantType[];
-}
-
-export interface ParticipantType {
-  id: string;
-  levellogId: string;
-  nickname: string;
-  profileUrl: string;
-}

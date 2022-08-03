ba65bd9cd2372119159202428867ca15da54aba8
jihyeok-um <75592315+jihyeok-um@users.noreply.github.com>
2022-08-03 13:14:22 +0900

    fix: 로그인이 필요한 페이지에서 두 번의 `alert`이 뜨는 버그 수정 (#183)
    * chore: release 자동화 추가 (#120)
    
    * refactor: 패키지 구조 변경 (#118)
    
    * refactor: 패키지 구조 수정
    
    * chore: release 자동화 추가
    
    Co-authored-by: Roma <52696169+kbsat@users.noreply.github.com>
    
    * refactor: axios 호출 함수 컨벤션 변화에 따른 변경
    
    * chore: 헤더 로그인 위치 변경
    
    * refactor: 커스텀 훅 API 컨벤션 변경에 따른 변경
    
    * refactor: 커스텀 훅으로 컴포넌트 로직 이동
    
    * refactor: 레벨로그 CRUD 버그 및 구조 수정
    
    Co-authored-by: yunjin-kim <yunjin-kim@users.noreply.github.com>
    
    * chore: 더 이상 필요없는 파일 제거
    
    Co-authored-by: yunjin-kim <yunjin-kim@users.noreply.github.com>
    
    * refactore: 상수화 진행
    
    Co-authored-by: yunjin-kim <yunjin-kim@users.noreply.github.com>
    
    * refactor: 컴포넌트 로직 커스텀 훅으로 분리
    
    * fix: 피드백 CRUD가 되지 않는 버그 수정
    
    * fix: 팀 조회 API 변동에 따라 `id` -> `memberId` 로 변경
    
    * docs: `팀 생성` 기능 요구사항 작성
    
    * feat: `추가하기`버튼을 눌러 페이지를 이동할 수 있다.
    
    * feat: `InterviewTeamAdd` form을 만든다
    
    * feat: 팀 생성 폼 간단히 입력받기
    
    * feat: 팀 생성 폼은 `참가자`를 추가할 수 있아야한다.
    
    * feat: 팀 생성 기능 구현 완료
    
    * feat: 본인이 처음에 참가자에 추가되어있도록 수정
    
    * feat: 반응형 UI를 구현한다.
    
    * feat: 멤버 검색에 쓰로틀링 적용
    
    * refactor: `px` -> `rem`
    
    * refactor: `return` 개행 수정
    
    * fix: 로그인이 필요한 페이지에서 이중 `alert` 뜨는 버그 수정
    
    * Squashed commit of the following:
    
    commit 2d34c268e9305b758e25548575a30ff9be5ff7e9
    Author: SuyeonChoi <tndus970120@gmail.com>
    Date:   Tue Aug 2 23:00:42 2022 +0900
    
        fix: 속하지 않은 팀의 피드백 조회를 요청시 예외 발생 (#185)
    
        * refactor: FeedbackController findAll 메서드에 memberId 추가
    
        * fix: FeedbackService 팀 참가자가 아닌 경우 피드백을 조회할 수 없도록 수정
    
        * refactor: Deprecated 메서드 & 변수 삭제
    
    commit 5190896f923c415a1dbb090005629a2e16ab80b1
    Author: yunjin-kim <79692272+yunjin-kim@users.noreply.github.com>
    Date:   Tue Aug 2 20:43:45 2022 +0900
    
        feat: 편한 글쓰기를 지원한다. (#179)
    
        * chore: toast editor ui 설치
    
        * feat: UiEditor 추가
    
        * feat: UiViewer 추가
    
        * feat: 레벨로그 에디터로 변경
    
        * feat: 피드백 에디터로 변경
    
        Co-authored-by: jihyeok-um <75592315+jihyeok-um@users.noreply.github.com>
    
    Co-authored-by: yunjin-kim <79692272+yunjin-kim@users.noreply.github.com>
    Co-authored-by: Roma <52696169+kbsat@users.noreply.github.com>
    Co-authored-by: yunjin-kim <yunjin-kim@users.noreply.github.com>

--- frontend/src/components/levellogs/LevellogViewModal.tsx
+++ frontend/src/components/levellogs/LevellogViewModal.tsx
@@ -23,7 +23,6 @@ const LevellogViewModal = ({
   const { loginUserId } = useUser();
 
   const handleClickDeleteLevellog = async () => {
-    // levellogId가 number라 일단 null이 아닌지만 체크하고 요청하기로 함.
     if (teamId && levellogId) {
       await onClickDeleteLevellog({ teamId, levellogId });
       getTeam();

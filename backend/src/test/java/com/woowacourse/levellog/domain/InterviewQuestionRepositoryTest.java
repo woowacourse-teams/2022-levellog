41aa2ba57831433a49f8eceb6eaaa623411f3a04
nailseong <nnis9857@gmail.com>
2022-08-08 21:04:36 +0900

    refactor: InterviewQuestionRepository 픽스처를 활용해서 개선

--- backend/src/test/java/com/woowacourse/levellog/domain/InterviewQuestionRepositoryTest.java
+++ backend/src/test/java/com/woowacourse/levellog/domain/InterviewQuestionRepositoryTest.java
@@ -5,9 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.woowacourse.levellog.interviewquestion.domain.InterviewQuestion;
 import com.woowacourse.levellog.levellog.domain.Levellog;
 import com.woowacourse.levellog.member.domain.Member;
-import com.woowacourse.levellog.team.domain.Participant;
 import com.woowacourse.levellog.team.domain.Team;
-import java.time.LocalDateTime;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -19,18 +17,14 @@ class InterviewQuestionRepositoryTest extends RepositoryTest {
     @DisplayName("findAllByLevellogAndAuthor 메서드는 levellog와 author 멤버가 모두 일치하는 인터뷰 질문 목록를 반환한다.")
     void findAllByLevellogAndAuthor() {
         // given
-        final Member eve = memberRepository.save(new Member("eve", 111, "profile.img"));
-        final Member toMember = memberRepository.save(new Member("toMember", 333, "profile.img"));
-        final Team team = teamRepository.save(
-                new Team("잠실 네오조", "작은 강의실", LocalDateTime.now().plusDays(3), "team.img", 1));
-        participantRepository.save(new Participant(team, eve, true));
-        participantRepository.save(new Participant(team, toMember, false));
-        final Levellog levellog = levellogRepository.save(Levellog.of(toMember, team, "levellog"));
+        final Member eve = getMember("eve");
+        final Member toMember = getMember("toMember");
 
-        final InterviewQuestion savedInterviewQuestion1 = interviewQuestionRepository.save(
-                InterviewQuestion.of(eve, levellog, "스프링을 왜 사용하였나요?"));
-        final InterviewQuestion savedInterviewQuestion2 = interviewQuestionRepository.save(
-                InterviewQuestion.of(eve, levellog, "AOP란?"));
+        final Team team = getTeam(eve, toMember);
+        final Levellog levellog = getLevellog(toMember, team);
+
+        final InterviewQuestion savedInterviewQuestion1 = getInterviewQuestion("스프링을 왜 사용하였나요?", levellog, eve);
+        final InterviewQuestion savedInterviewQuestion2 = getInterviewQuestion("AOP란?", levellog, eve);
 
         // when
         final List<InterviewQuestion> interviewQuestions = interviewQuestionRepository.findAllByLevellogAndAuthor(
--- backend/src/test/java/com/woowacourse/levellog/domain/RepositoryTest.java
+++ backend/src/test/java/com/woowacourse/levellog/domain/RepositoryTest.java
@@ -2,12 +2,22 @@ package com.woowacourse.levellog.domain;
 
 import com.woowacourse.levellog.common.config.JpaConfig;
 import com.woowacourse.levellog.feedback.domain.FeedbackRepository;
+import com.woowacourse.levellog.interviewquestion.domain.InterviewQuestion;
 import com.woowacourse.levellog.interviewquestion.domain.InterviewQuestionRepository;
+import com.woowacourse.levellog.interviewquestion.dto.InterviewQuestionDto;
+import com.woowacourse.levellog.levellog.domain.Levellog;
 import com.woowacourse.levellog.levellog.domain.LevellogRepository;
+import com.woowacourse.levellog.member.domain.Member;
 import com.woowacourse.levellog.member.domain.MemberRepository;
 import com.woowacourse.levellog.prequestion.domain.PreQuestionRepository;
+import com.woowacourse.levellog.team.domain.Participant;
 import com.woowacourse.levellog.team.domain.ParticipantRepository;
+import com.woowacourse.levellog.team.domain.Team;
 import com.woowacourse.levellog.team.domain.TeamRepository;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
@@ -36,4 +46,38 @@ abstract class RepositoryTest {
 
     @Autowired
     protected TeamRepository teamRepository;
+
+    protected Member getMember(final String nickname) {
+        final Member member = new Member(nickname, ((int) System.nanoTime()), nickname + ".org");
+        return memberRepository.save(member);
+    }
+
+    protected Team getTeam(final Member host, final Member... members) {
+        return getTeam(3, host, members);
+    }
+
+    protected Team getTeam(final long days,  final Member host, final Member... members) {
+        final Team team = teamRepository.save(
+                new Team("잠실 네오조", "트랙룸", LocalDateTime.now().plusDays(days), "jamsil.img", 1));
+
+        participantRepository.save(new Participant(team, host, true));
+
+        final List<Participant> participants = Arrays.stream(members)
+                .map(it -> new Participant(team, it, false))
+                .collect(Collectors.toList());
+        participantRepository.saveAll(participants);
+
+        return team;
+    }
+
+    protected Levellog getLevellog(final Member author, final Team team) {
+        final Levellog levellog = Levellog.of(author, team, "levellog content");
+        return levellogRepository.save(levellog);
+    }
+
+    protected InterviewQuestion getInterviewQuestion(final String content, final Levellog levellog, final Member author) {
+        final InterviewQuestionDto request = InterviewQuestionDto.from(content);
+        final InterviewQuestion interviewQuestion = request.toInterviewQuestion(author, levellog);
+        return interviewQuestionRepository.save(interviewQuestion);
+    }
 }

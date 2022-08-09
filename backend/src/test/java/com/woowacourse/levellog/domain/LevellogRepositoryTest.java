c999affe13b90614eedfb8b65c7ed33e2abc0c70
nailseong <nnis9857@gmail.com>
2022-08-09 21:53:15 +0900

    refactor: LevellogRepository 픽스처를 활용해서 개선

--- backend/src/test/java/com/woowacourse/levellog/domain/LevellogRepositoryTest.java
+++ backend/src/test/java/com/woowacourse/levellog/domain/LevellogRepositoryTest.java
@@ -8,7 +8,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import com.woowacourse.levellog.levellog.domain.Levellog;
 import com.woowacourse.levellog.member.domain.Member;
 import com.woowacourse.levellog.team.domain.Team;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
@@ -22,10 +21,9 @@ class LevellogRepositoryTest extends RepositoryTest {
     @DisplayName("findByAuthorIdAndTeamId 메서드는 memberId와 teamId이 모두 일치하는 레벨로그를 반환한다.")
     void findByAuthorIdAndTeamId() {
         // given
-        final Member author = memberRepository.save(new Member("pepper", 1111, "pepper.png"));
-        final Team team = teamRepository.save(
-                new Team("선릉 브라운조", "무중력 광장", LocalDateTime.now().plusDays(1), "campus.png", 1));
-        final Levellog levellog = levellogRepository.save(Levellog.of(author, team, "Spring을 학습하였습니다."));
+        final Member author = getMember("pepper");
+        final Team team = getTeam(author);
+        final Levellog levellog = getLevellog(author, team);
 
         final Long authorId = author.getId();
         final Long teamId = team.getId();
@@ -37,6 +35,30 @@ class LevellogRepositoryTest extends RepositoryTest {
         assertThat(actual).hasValue(levellog);
     }
 
+    @Test
+    @DisplayName("findAllByAuthor 메서드는 주어진 author가 작성한 레벨로그를 모두 반환한다.")
+    void findAllByAuthor() {
+        // given
+        final Member author = getMember("pepper");
+        final Member anotherAuthor = getMember("roma");
+
+        final Team team = getTeam(author);
+        final Team team2 = getTeam(anotherAuthor, author);
+
+        final Levellog authorLevellog1 = getLevellog(author, team);
+        final Levellog authorLevellog2 = getLevellog(author, team2);
+        getLevellog(anotherAuthor, team);
+
+        // when
+        final List<Levellog> levellogs = levellogRepository.findAllByAuthor(author);
+
+        // then
+        assertAll(
+                () -> assertThat(levellogs).hasSize(2),
+                () -> assertThat(levellogs).contains(authorLevellog1, authorLevellog2)
+        );
+    }
+
     @Nested
     @DisplayName("existsByAuthorIdAndTeamId 메서드는")
     class ExistsByAuthorIdAndTeamIdTest {
@@ -45,10 +67,9 @@ class LevellogRepositoryTest extends RepositoryTest {
         @DisplayName("memberId와 teamId이 모두 일치하는 레벨로그가 존재하는 경우 true를 반환한다.")
         void exists() {
             // given
-            final Member author = memberRepository.save(new Member("pepper", 1111, "pepper.png"));
-            final Team team = teamRepository.save(
-                    new Team("선릉 브라운조", "무중력 광장", LocalDateTime.now().plusDays(1), "campus.png", 1));
-            levellogRepository.save(Levellog.of(author, team, "Spring을 학습하였습니다."));
+            final Member author = getMember("pepper");
+            final Team team = getTeam(author);
+            getLevellog(author, team);
 
             final Long authorId = author.getId();
             final Long teamId = team.getId();
@@ -64,10 +85,9 @@ class LevellogRepositoryTest extends RepositoryTest {
         @DisplayName("memberId와 teamId이 모두 일치하는 레벨로그가 존재하지 않는 경우 false를 반환한다.")
         void notExists() {
             // given
-            final Member author = memberRepository.save(new Member("pepper", 1111, "pepper.png"));
-            final Team team = teamRepository.save(
-                    new Team("선릉 브라운조", "무중력 광장", LocalDateTime.now().plusDays(1), "campus.png", 1));
-            levellogRepository.save(Levellog.of(author, team, "Spring을 학습하였습니다."));
+            final Member author = getMember("pepper");
+            final Team team = getTeam(author);
+            getLevellog(author, team);
 
             final Long anotherAuthorId = author.getId() + 1;
             final Long teamId = team.getId();
@@ -78,30 +98,5 @@ class LevellogRepositoryTest extends RepositoryTest {
             // then
             assertFalse(actual);
         }
-    }
-
-    @Test
-    @DisplayName("findAllByAuthor 메서드는 주어진 author가 작성한 레벨로그를 모두 반환한다.")
-    void findAllByAuthor() {
-        // given
-        final Member author = memberRepository.save(new Member("pepper", 1111, "pepper.png"));
-        final Member anotherAuthor = memberRepository.save(new Member("roma", 12345, "roma.png"));
-        final Team team = teamRepository.save(
-                new Team("선릉 브라운조", "무중력 광장", LocalDateTime.now().plusDays(1), "campus.png", 1));
-        final Team team2 = teamRepository.save(
-                new Team("선릉 네오조", "수성 회의실", LocalDateTime.now().plusDays(1), "campus2.png", 1));
-
-        final Levellog authorLevellog1 = levellogRepository.save(Levellog.of(author, team, "Spring을 학습하였습니다."));
-        final Levellog authorLevellog2 = levellogRepository.save(Levellog.of(author, team2, "JPA를 학습하였습니다."));
-        final Levellog anotherLevelog = levellogRepository.save(Levellog.of(anotherAuthor, team, "리액트를 학습하였습니다."));
-
-        // when
-        final List<Levellog> levellogs = levellogRepository.findAllByAuthor(author);
-
-        // then
-        assertAll(
-                () -> assertThat(levellogs).hasSize(2),
-                () -> assertThat(levellogs).contains(authorLevellog1, authorLevellog2)
-        );
     }
 }

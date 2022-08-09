5447534b621adbf5bed40fcd74cffc3646142aa8
BeomsooKim <kbsat@naver.com>
2022-08-09 22:42:54 +0900

    refactor: DatabaseCleaner 테이블 이름 찾는 로직 수정

--- backend/src/test/java/com/woowacourse/levellog/config/DatabaseCleaner.java
+++ backend/src/test/java/com/woowacourse/levellog/config/DatabaseCleaner.java
@@ -1,18 +1,20 @@
 package com.woowacourse.levellog.config;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
+import java.util.stream.Collectors;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Table;
-import javax.persistence.metamodel.EntityType;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class DatabaseCleaner implements InitializingBean {
+
+    private static final String CONSTRAINT_OFF_SQL = "SET REFERENTIAL_INTEGRITY FALSE";
+    private static final String CONSTRAINT_ON_SQL = "SET REFERENTIAL_INTEGRITY TRUE";
+    private static final String TRUNCATE_TABLE_SQL = "TRUNCATE TABLE ";
+    private static final String SHOW_TABLES_SQL = "SHOW TABLES";
 
     @PersistenceContext
     private EntityManager entityManager;
@@ -25,46 +27,22 @@ public class DatabaseCleaner implements InitializingBean {
     }
 
     private List<String> findTableNamesByEntities() {
-        final List<String> tableNames = new ArrayList<>();
+        final List<Object[]> tables = (List<Object[]>) entityManager.createNativeQuery(SHOW_TABLES_SQL)
+                .getResultList();
 
-        final Set<EntityType<?>> entities = entityManager.getMetamodel()
-                .getEntities();
-
-        for (final EntityType<?> entity : entities) {
-            final Table t = entity.getJavaType()
-                    .getAnnotation(Table.class);
-
-            if (haveNoCustomTableName(t)) {
-                tableNames.add(toSnakeCase(entity.getName()));
-            } else {
-                tableNames.add(t.name());
-            }
-        }
-        return tableNames;
-    }
-
-    private boolean haveNoCustomTableName(final Table t) {
-        return t == null || t.name().isBlank();
-    }
-
-    private String toSnakeCase(final String text) {
-        final String regex = "([a-z])([A-Z]+)";
-        final String replacement = "$1_$2";
-
-        return text.replaceAll(regex, replacement)
-                .toLowerCase();
+        return tables.stream()
+                .map(it -> it[0].toString())
+                .collect(Collectors.toList());
     }
 
     @Transactional
     public void clean() {
         entityManager.flush();
-        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY FALSE").executeUpdate();
+        entityManager.createNativeQuery(CONSTRAINT_OFF_SQL).executeUpdate();
 
         for (final String tableName : tableNames) {
-            entityManager.createNativeQuery("TRUNCATE TABLE " + tableName).executeUpdate();
-            entityManager.createNativeQuery("ALTER TABLE " + tableName + " ALTER COLUMN id RESTART WITH 1")
-                    .executeUpdate();
+            entityManager.createNativeQuery(TRUNCATE_TABLE_SQL + tableName).executeUpdate();
         }
-        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY TRUE").executeUpdate();
+        entityManager.createNativeQuery(CONSTRAINT_ON_SQL).executeUpdate();
     }
 }

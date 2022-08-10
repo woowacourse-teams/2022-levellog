ea2456562ffb23a6f4a882a11c0734abc633463d
(yunjin) <sion11290805@gmail.com>
2022-08-10 15:47:09 +0900

    feat: 인터뷰 질문 기능 타입 구현

---
+++ frontend/src/types/interviewQuestion.ts
@@ -0,0 +1,11 @@
+export interface InterviewQuestionApiType {
+  accessToken: string | null;
+  levellogId: string;
+  interviewQuestionId: string;
+  interviewQuestion: string;
+}
+
+export interface interviewQuestionType {
+  id: string;
+  interviewQuestion: string;
+}

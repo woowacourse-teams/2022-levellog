5a47b9429c035fa47bdbe7923830ad58b5ac9517
2yujeong <76840965+2yujeong@users.noreply.github.com>
2022-08-10 20:13:00 +0900

    feat: 피드백 생성, 수정 시 바뀐 정책 적용 (#252)
    * refactor: startAt, isClosed 검증 메소드 리팩토링
    
    * feat: 인터뷰 시작 도중에만 피드백을 작성, 수정할 수 있도록 검증 로직 추가
    
    * test: 피드백 수정, 작성 예외 시나리오 테스트 추가
    
    * refactor: 피드백 삭제 API 삭제
    
    * test: Feedback 컨트롤러 테스트에 픽스처 적용
    
    * refactor: 예외 처리 메시지 메소드 내부로 숨김 처리
    
    * style: 코드 컨벤션 수정
    
    * refactor: 불필요한 쿼리 전송 코드 리팩토링
    
    * refactor: 팀에 속한 멤버인지 검증 시 find 대신 exists를 활용하도록 리팩토링
    
    * refactor: 인터뷰 종료 시간 이전인지 검증하는 메소드 네이밍 변경

--- backend/src/main/java/com/woowacourse/levellog/team/domain/Team.java
+++ backend/src/main/java/com/woowacourse/levellog/team/domain/Team.java
@@ -118,19 +118,19 @@ public class Team extends BaseEntity {
     }
 
     public void close(final LocalDateTime presentTime) {
-        validateInterviewStartTime(presentTime);
-        validateAlreadyClosed();
+        validateAfterStartAt(presentTime, "인터뷰가 시작되기 전에 종료할 수 없습니다.");
+        validateBeforeClose();
 
         isClosed = true;
     }
 
-    private void validateInterviewStartTime(final LocalDateTime presentTime) {
+    public void validateAfterStartAt(final LocalDateTime presentTime, final String message) {
         if (presentTime.isBefore(startAt)) {
-            throw new InterviewTimeException("인터뷰가 시작되기 전에 종료할 수 없습니다.", "[teamId : " + this.getId() + "]");
+            throw new InterviewTimeException(message, "[teamId : " + this.getId() + ", startAt : " + startAt + "]");
         }
     }
 
-    private void validateAlreadyClosed() {
+    public void validateBeforeClose() {
         if (isClosed) {
             throw new InterviewTimeException("이미 종료된 인터뷰입니다.", "[teamId : " + this.getId() + "]");
         }

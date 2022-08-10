46ac6452688e83695bcaae661bbea7985a96ffe6
(yunjin) <sion11290805@gmail.com>
2022-08-10 15:47:31 +0900

    feat: 인터뷰 질문 api 구현

---
+++ frontend/src/apis/interviewQuestion.ts
@@ -0,0 +1,55 @@
+import axios, { AxiosPromise } from 'axios';
+
+import { InterviewQuestionApiType, interviewQuestionType } from 'types/interviewQuestion';
+
+export const requestGetInterviewQuestion = ({
+  accessToken,
+  levellogId,
+}: Pick<InterviewQuestionApiType, 'accessToken' | 'levellogId'>): AxiosPromise<
+  Record<'interviewQuestions', interviewQuestionType[]>
+> => {
+  return axios({
+    method: 'get',
+    url: `${process.env.API_URI}/levellogs/${levellogId}/interview-questions`,
+    headers: { Authorization: `Bearer ${accessToken}` },
+  });
+};
+
+export const requestPostInterviewQuestion = ({
+  accessToken,
+  levellogId,
+  interviewQuestion,
+}: Omit<InterviewQuestionApiType, 'interviewQuestionId'>): AxiosPromise<void> => {
+  return axios({
+    method: 'post',
+    url: `${process.env.API_URI}/levellogs/${levellogId}/interview-questions`,
+    headers: { Authorization: `Bearer ${accessToken}` },
+    data: { interviewQuestion },
+  });
+};
+
+export const requestDeleteInterviewQuestion = ({
+  accessToken,
+  levellogId,
+  interviewQuestionId,
+}: Omit<InterviewQuestionApiType, 'interviewQuestion'>): AxiosPromise<void> => {
+  return axios({
+    method: 'delete',
+    url: `${process.env.API_URI}/levellogs/${levellogId}/interview-questions/${interviewQuestionId}`,
+    headers: { Authorization: `Bearer ${accessToken}` },
+  });
+};
+
+export const requestEditInterviewQuestion = ({
+  accessToken,
+  levellogId,
+  interviewQuestionId,
+  interviewQuestion,
+}: InterviewQuestionApiType) => {
+  return axios({
+    method: 'put',
+    url: `${process.env.API_URI}/levellogs/${levellogId}/interview-questions/${interviewQuestionId}`,
+    headers: { Authorization: `Bearer ${accessToken}` },
+    data: { interviewQuestion },
+  });
+};

344c5e68b19095167d5d3ee202504fa160201634
장원영 <32123302+OzRagwort@users.noreply.github.com>
2022-08-10 19:58:59 +0900

    feat: 레벨로그 상세 조회 시 body에 작성자의 정보도 제공하도록 수정한다. (#253)
    * refactor: 레벨로그 상세 조회 시 body에 작성자의 정보도 제공하도록 수정
    
    * refactor: 레벨로그 조회 테스트 시 author의 정보도 검증하도록 수정
    
    * refactor: 컨벤션 수정
    
    Co-authored-by: BeomsooKim <kbsat@naver.com>

--- backend/src/main/java/com/woowacourse/levellog/levellog/application/LevellogService.java
+++ backend/src/main/java/com/woowacourse/levellog/levellog/application/LevellogService.java
@@ -5,6 +5,7 @@ import com.woowacourse.levellog.levellog.domain.Levellog;
 import com.woowacourse.levellog.levellog.domain.LevellogRepository;
 import com.woowacourse.levellog.levellog.dto.LevellogDto;
 import com.woowacourse.levellog.levellog.dto.LevellogWithIdDto;
+import com.woowacourse.levellog.levellog.dto.LevellogWriteDto;
 import com.woowacourse.levellog.levellog.dto.LevellogsDto;
 import com.woowacourse.levellog.levellog.exception.LevellogAlreadyExistException;
 import com.woowacourse.levellog.levellog.exception.LevellogNotFoundException;
@@ -30,7 +31,7 @@ public class LevellogService {
     private final MemberRepository memberRepository;
 
     @Transactional
-    public Long save(final LevellogDto request, final Long authorId, final Long teamId) {
+    public Long save(final LevellogWriteDto request, final Long authorId, final Long teamId) {
         final Team team = getTeam(teamId);
         final Member author = getMember(authorId);
         validateLevellogExistence(authorId, teamId);
@@ -59,7 +60,7 @@ public class LevellogService {
     }
 
     @Transactional
-    public void update(final LevellogDto request, final Long levellogId, final Long memberId) {
+    public void update(final LevellogWriteDto request, final Long levellogId, final Long memberId) {
         final Levellog levellog = getById(levellogId);
         final Member member = getMember(memberId);
 

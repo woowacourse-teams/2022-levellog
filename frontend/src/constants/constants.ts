dee277593532869f265a36ff9912ed2888ac7855
(yunjin) <sion11290805@gmail.com>
2022-08-13 19:02:18 +0900

    feat: 요청시 토큰 올바르지 못한 경우 에러 처리

--- frontend/src/constants/constants.ts
+++ frontend/src/constants/constants.ts
@@ -25,15 +25,16 @@ export const SERVER_PATH = Object.freeze({
 });
 
 export const MESSAGE = Object.freeze({
-  WRONG_ACCESS: '잘못된 접근입니다',
-  NEED_LOGIN: '로그인이 필요한 페이지입니다',
+  WRONG_ACCESS: '잘못된 접근입니다.',
+  NEED_RELOGIN: '재로그인이 필요합니다.',
+  NEED_LOGIN: '로그인이 필요한 페이지입니다.',
   NEED_IN_TEAM: '해당 팀에 소속된 유저만 접근할 수 있습니다!',
   NEED_HOST: '호스트만 접근할 수 있습니다!',
-  NEED_ME: '본인이 작성하지 않은 글을 수정할 수 없습니다',
+  NEED_ME: '본인이 작성하지 않은 글을 수정할 수 없습니다.',
   NEED_NOT_ME: '본인이 본인에 대한 질문,피드백을 작성,수정할 수 없습니다.',
   TEAM_CREATE: '인터뷰팀 생성을 완료했습니다!',
   TEAM_DELETE_CONFIRM: '정말로 팀을 삭제하시겠습니까?',
-  WRITE_MORE: '3글자 이상 작성해주세요',
+  WRITE_MORE: '3글자 이상 작성해주세요.',
   CAN_EDIT: '수정하고 싶은 질문을 클릭해서 수정할 수 있어요!',
   INTERVIEW_CLOSE_CONFIRM: '정말로 인터뷰를 종료하시겠습니까?',
   INTERVIEW_STATUS_NOT_READY: '인터뷰가 시작 전이 아닙니다!',

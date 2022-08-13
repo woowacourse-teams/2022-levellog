1af06531cf42025030a7307fc453a0dacbc2f4a5
nailseong <nnis9857@gmail.com>
2022-08-13 21:18:19 +0900

    chore: http 요청 로그

--- backend/src/main/java/com/woowacourse/levellog/common/config/LogInterceptor.java
+++ backend/src/main/java/com/woowacourse/levellog/common/config/LogInterceptor.java
@@ -3,9 +3,11 @@ package com.woowacourse.levellog.common.config;
 import java.util.UUID;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+@Slf4j
 public class LogInterceptor implements HandlerInterceptor {
 
     @Override
@@ -15,6 +17,19 @@ public class LogInterceptor implements HandlerInterceptor {
                 .toString()
                 .substring(0, 8);
         MDC.put("correlationId", correlationId);
+
+        final String httpMethod = request.getMethod();
+        final String uri = request.getRequestURI();
+
+        String queryString = request.getQueryString();
+        if (queryString == null) {
+            queryString = "";
+        } else {
+            queryString = "?" + queryString;
+        }
+
+        log.info("[{}] {} {}{}", correlationId, httpMethod, uri, queryString);
+
         return true;
     }
 }

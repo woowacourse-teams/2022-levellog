2f213039fe2a70ae7830692f5906c56029644a94
Na Ilseong <ilseongdev@gmail.com>
2022-08-15 18:23:59 +0900

    feat: 특정 레벨로그에 작성된 인터뷰 질문 목록 조회 (#288)
    * refactor: 인터뷰 질문 목록 조회 -> 내가 작성한 인터뷰 질문 목록 조회
    
    * refactor: 인터뷰 질문 조회 응답 필드 key 이름 변경 interviewQuestion -> content
    
    * feat: 레벨로그에 작성된 인터뷰 질문 목록 조회 controller
    
    * feat: 레벨로그를 조건으로 인터뷰 질문 목록 조회 쿼리
    
    * feat: 레벨로그에 작성된 인터뷰 질문 목록 조회 Service
    
    * feat: 레벨로그에 작성된 인터뷰 질문 목록 조회를 PublicAPI로 변경
    
    * test: 인수 테스트
    
    * docs: 해피케이스 API 문서
    
    * docs: 레벨로그가 존재하지 않는 경우 API 문서
    
    * refactor: dto 이름 변경
    
    * refactor: Entity -> Dto 변환 로직 개선
    
    * docs: 인수 테스트 시나리오 수정
    
    * refactor: 테스트 코드를 더 풍부하게 변경
    
    * refactor: 인터뷰 질문 작성자를 나타내는 변수 명을 author로 통일

--- backend/src/test/java/com/woowacourse/levellog/acceptance/InterviewQuestionAcceptanceTest.java
+++ backend/src/test/java/com/woowacourse/levellog/acceptance/InterviewQuestionAcceptanceTest.java
@@ -1,15 +1,18 @@
 package com.woowacourse.levellog.acceptance;
 
 import static com.woowacourse.levellog.fixture.MemberFixture.PEPPER;
+import static com.woowacourse.levellog.fixture.MemberFixture.RICK;
 import static com.woowacourse.levellog.fixture.MemberFixture.ROMA;
 import static com.woowacourse.levellog.fixture.RestAssuredTemplate.get;
 import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 
 import com.woowacourse.levellog.fixture.MemberFixture;
-import com.woowacourse.levellog.interviewquestion.dto.InterviewQuestionDto;
+import com.woowacourse.levellog.interviewquestion.dto.InterviewQuestionWriteDto;
 import io.restassured.RestAssured;
 import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.DisplayName;
@@ -39,7 +42,7 @@ class InterviewQuestionAcceptanceTest extends AcceptanceTest {
         timeStandard.setInProgress();
 
         // when
-        final InterviewQuestionDto request = InterviewQuestionDto.from("Spring을 사용하는 이유?");
+        final InterviewQuestionWriteDto request = InterviewQuestionWriteDto.from("Spring을 사용하는 이유?");
 
         final ValidatableResponse response = RestAssured.given(specification).log().all()
                 .header(HttpHeaders.AUTHORIZATION, "Bearer " + ROMA.getToken())
@@ -56,14 +59,14 @@ class InterviewQuestionAcceptanceTest extends AcceptanceTest {
     }
 
     /*
-     * Scenario: 인터뷰 질문 목록 조회
-     *   given: 인터뷰 질문이 등록되어있다.
-     *   when: 등록된 모든 인터뷰 질문을 조회한다.
-     *   then: 200 OK 상태 코드와 모든 인터뷰 질문을 응답 받는다.
+     * Scenario: 내가 작성한 인터뷰 질문 목록 조회
+     *   given: 레벨로그와 인터뷰 질문이 등록되어있다.
+     *   when: 등록된 레벨로그에 내가 작성한 모든 인터뷰 질문을 조회한다.
+     *   then: 200 OK 상태 코드와 내가 작성한 모든 인터뷰 질문을 응답 받는다.
      */
     @Test
-    @DisplayName("인터뷰 질문 목록 조회")
-    void findAll() {
+    @DisplayName("내가 작성한 인터뷰 질문 목록 조회")
+    void findAllMyInterviewQuestion() {
         // given
         PEPPER.save();
         ROMA.save();
@@ -80,14 +83,71 @@ class InterviewQuestionAcceptanceTest extends AcceptanceTest {
         final ValidatableResponse response = RestAssured.given(specification).log().all()
                 .header(HttpHeaders.AUTHORIZATION, "Bearer " + ROMA.getToken())
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .filter(document("interview-question/findAll"))
+                .filter(document("interview-question/find-all-my-interview-question"))
+                .when()
+                .get("/api/levellogs/{levellogId}/interview-questions/my", levellogId)
+                .then().log().all();
+
+        // then
+        response.statusCode(HttpStatus.OK.value())
+                .body("interviewQuestions.content", contains("Spring을 사용하는 이유?", "스프링 빈이란?"));
+    }
+
+    /*
+     * Scenario: 레벨로그에 작성된 인터뷰 질문 목록 조회
+     *   given: 페퍼, 로마, 릭이 참가자인 팀이 있다.
+     *   given: 페퍼는 레벨로그를 작성한다.
+     *   given: 인터뷰를 시작한다.
+     *   given: 로마와 릭은 페퍼의 레벨로그에 대한 인터뷰 질문을 작성한다.
+     *   when: 페퍼의 레벨로그에 작성된 모든 인터뷰 질문 목록을 조회힌다.
+     *   then: 200 OK 상태 코드와 레벨로그에 작성된 모든 인터뷰 질문을 응답 받는다.
+     */
+    @Test
+    @DisplayName("레벨로그에 작성된 인터뷰 질문 목록 조회")
+    void findAllByLevellog() {
+        // given
+        PEPPER.save();
+        ROMA.save();
+        RICK.save();
+
+        final String teamId = saveTeam("잠실 제이슨조", PEPPER, 1, ROMA, RICK).getTeamId();
+        final String levellogId = saveLevellog("페퍼의 레벨로그", teamId, PEPPER).getLevellogId();
+
+        timeStandard.setInProgress(); // 인터뷰 시작
+
+        final String rickContent1 = "트랜잭션 전파 옵션 종류는?";
+        saveInterviewQuestion(rickContent1, levellogId, RICK);
+
+        final String romaContent1 = "AOP에 대해 설명해주세요.";
+        saveInterviewQuestion(romaContent1, levellogId, ROMA);
+
+        final String romaContent2 = "전략 패턴이 무엇인가요?";
+        saveInterviewQuestion(romaContent2, levellogId, ROMA);
+
+        final String rickContent2 = "프로세스와 스레드의 차이는?";
+        saveInterviewQuestion(rickContent2, levellogId, RICK);
+
+        final String romaContent3 = "프레임워크와 라이브러리의 차이는?";
+        saveInterviewQuestion(romaContent3, levellogId, ROMA);
+
+        // when
+        final ValidatableResponse response = RestAssured.given(specification).log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .filter(document("interview-question/find-all-by-levellog"))
                 .when()
                 .get("/api/levellogs/{levellogId}/interview-questions", levellogId)
                 .then().log().all();
 
         // then
         response.statusCode(HttpStatus.OK.value())
-                .body("interviewQuestions.interviewQuestion", contains("Spring을 사용하는 이유?", "스프링 빈이란?"));
+                .body("interviewQuestions", hasSize(2),
+
+                        "interviewQuestions[0].author.nickname", equalTo("릭"),
+                        "interviewQuestions[0].contents.content", contains(rickContent1, rickContent2),
+
+                        "interviewQuestions[1].author.nickname", equalTo("로마"),
+                        "interviewQuestions[1].contents.content", contains(romaContent1, romaContent2, romaContent3)
+                );
     }
 
     /*
@@ -113,7 +173,7 @@ class InterviewQuestionAcceptanceTest extends AcceptanceTest {
                 .getInterviewQuestionId();
 
         // when
-        final InterviewQuestionDto request = InterviewQuestionDto.from("수정된 인터뷰 질문");
+        final InterviewQuestionWriteDto request = InterviewQuestionWriteDto.from("수정된 인터뷰 질문");
 
         final ValidatableResponse response = RestAssured.given(specification).log().all()
                 .header(HttpHeaders.AUTHORIZATION, "Bearer " + ROMA.getToken())
@@ -127,8 +187,8 @@ class InterviewQuestionAcceptanceTest extends AcceptanceTest {
 
         // then
         response.statusCode(HttpStatus.NO_CONTENT.value());
-        requestFindAllInterviewQuestion(levellogId, ROMA)
-                .body("interviewQuestions.interviewQuestion", contains("수정된 인터뷰 질문"));
+        requestFindAllMyInterviewQuestion(levellogId, ROMA)
+                .body("interviewQuestions.content", contains("수정된 인터뷰 질문"));
     }
 
     /*
@@ -165,12 +225,12 @@ class InterviewQuestionAcceptanceTest extends AcceptanceTest {
 
         // then
         response.statusCode(HttpStatus.NO_CONTENT.value());
-        requestFindAllInterviewQuestion(levellogId, ROMA)
+        requestFindAllMyInterviewQuestion(levellogId, ROMA)
                 .body("interviewQuestions.id", not(contains(interviewQuestionId)));
     }
 
-    private ValidatableResponse requestFindAllInterviewQuestion(final String levellogId, final MemberFixture from) {
-        return get("/api/levellogs/" + levellogId + "/interview-questions", from.getToken())
+    private ValidatableResponse requestFindAllMyInterviewQuestion(final String levellogId, final MemberFixture from) {
+        return get("/api/levellogs/" + levellogId + "/interview-questions/my", from.getToken())
                 .getResponse();
     }
 }

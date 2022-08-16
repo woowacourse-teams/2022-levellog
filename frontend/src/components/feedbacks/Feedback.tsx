0c0d11bae0ac455a48b3bcd0bf4028498df99ef3
(yunjin) <sion11290805@gmail.com>
2022-08-16 21:25:11 +0900

    feat: 피드백 조회 스타일 수정

--- frontend/src/components/feedbacks/Feedback.tsx
+++ frontend/src/components/feedbacks/Feedback.tsx
@@ -6,15 +6,20 @@ import { TEAM_STATUS } from 'constants/constants';
 
 import Button from 'components/@commons/Button';
 import FlexBox from 'components/@commons/FlexBox';
+import Image from 'components/@commons/Image';
 import UiViewer from 'components/@commons/UiViewer';
 import { FeedbackType } from 'types/feedback';
 
 const Feedback = ({ loginUserId, feedbackInfo, teamId, levellogId, teamStatus }: FeedbackProps) => {
+  console.log('Feedback', feedbackInfo.from.id, loginUserId);
   return (
     <S.Container>
       <S.Header>
-        <h3>{feedbackInfo.from.nickname}의 피드백</h3>
-        {teamStatus === TEAM_STATUS.IN_PROGRESS && String(feedbackInfo.from.id) === loginUserId && (
+        <FlexBox alignItems={'center'} gap={0.375}>
+          <Image src={feedbackInfo.from.profileUrl} sizes={'MEDIUM'} />
+          <S.AuthorText>{feedbackInfo.from.nickname}의 피드백</S.AuthorText>
+        </FlexBox>
+        {teamStatus === TEAM_STATUS.IN_PROGRESS && feedbackInfo.from.id === loginUserId && (
           <Link
             to={`/teams/${teamId}/levellogs/${levellogId}/feedbacks/${feedbackInfo.id}/writer/${feedbackInfo.from.id}/edit`}
           >
@@ -22,26 +27,26 @@ const Feedback = ({ loginUserId, feedbackInfo, teamId, levellogId, teamStatus }:
           </Link>
         )}
       </S.Header>
-      <FlexBox gap={1.5}>
+      <S.Box>
         <S.FeedbackBox>
-          <S.Title>학습 측면에서 좋은 점과 부족한 점은?</S.Title>
+          <S.Question>학습 측면에서 좋은 점과 부족한 점은?</S.Question>
           <S.Content>
             <UiViewer content={feedbackInfo.feedback.study} />
           </S.Content>
         </S.FeedbackBox>
         <S.FeedbackBox>
-          <S.Title>인터뷰, 말하기 측면에서 좋은 점과 개선할 부분은?</S.Title>
+          <S.Question>인터뷰, 말하기 측면에서 좋은 점과 개선할 부분은?</S.Question>
           <S.Content>
             <UiViewer content={feedbackInfo.feedback.speak} />
           </S.Content>
         </S.FeedbackBox>
         <S.FeedbackBox>
-          <S.Title>기타 피드백 (위 2 질문 외에 다른 피드백을 주세요.)</S.Title>
+          <S.Question>기타 피드백 (위 2 질문 외에 다른 피드백을 주세요.)</S.Question>
           <S.Content>
             <UiViewer content={feedbackInfo.feedback.etc} />
           </S.Content>
         </S.FeedbackBox>
-      </FlexBox>
+      </S.Box>
     </S.Container>
   );
 };
@@ -56,46 +61,58 @@ interface FeedbackProps {
 
 const S = {
   Container: styled.div`
-    width: 46.75rem;
-    min-width: 46.75rem;
-    min-height: 35.25rem;
-    padding: 1.25rem 1.5625rem 1.25rem 1.5rem;
-    background-color: ${(props) => props.theme.default.WHITE};
+    margin: auto;
+    width: 100%;
+    max-width: 71rem;
+    min-width: 18.75rem;
+    margin-bottom: 3.125rem;
+    border: 0.0625rem solid ${(props) => props.theme.new_default.GRAY};
     border-radius: 0.5rem;
-    @media (max-width: 840px) {
-      min-width: 15rem;
-    }
+    box-shadow: 0 0 1rem ${(props) => props.theme.new_default.GRAY};
   `,
 
-  Header: styled.header`
+  Header: styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
-    margin-bottom: 1.875rem;
+    gap: 0.375rem;
+    padding: 0.875rem;
+    width: 100%;
+    border-bottom: 0.0625rem solid ${(props) => props.theme.new_default.GRAY};
+  `,
+
+  AuthorText: styled.p`
+    font-size: 2rem;
+    font-weight: 300;
+  `,
+
+  Box: styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1.25rem;
+    padding: 0.875rem;
   `,
 
   FeedbackBox: styled.div`
     display: flex;
     flex-direction: column;
-    gap: 1.25rem;
+    gap: 0.875rem;
     width: 100%;
   `,
 
-  Title: styled.h2`
+  Question: styled.h2`
     font-size: 1.25rem;
+    font-weight: 300;
+    color: ${(props) => props.theme.new_default.DARK_BLACK};
   `,
 
   Content: styled.div`
     width: 100%;
     min-height: 6.25rem;
     padding: 0 1rem;
-    border-radius: 0.5rem;
-    background-color: ${(props) => props.theme.default.LIGHT_GRAY};
+    border: 1px solid ${(props) => props.theme.new_default.LIGHT_GRAY};
     word-spacing: 0.0625rem;
     line-height: 1.875rem;
-    @media (max-width: 560px) {
-      width: 100%;
-    }
   `,
 };
 

18236a979aec73887145ce9645ca4f340cafc181
yunjin-kim <79692272+yunjin-kim@users.noreply.github.com>
2022-08-16 15:44:34 +0900

    feat: 피드백 페이지에서 사전질문 조회 (#305)
    * feat: 레벨로그 관련 타입 추가
    
    * feat: 레벨로그 api 수정으로 인한 훅 수정
    
    * feat: 레벨로그 api 수정으로 인한 컴포넌트 수정
    
    * feat: alert 메시지 추가
    
    * feat: 레벨로그 사전질문 태그 훅 추가
    
    * feat: 레벨로그 사전질문 태그 컴포넌트  추가

--- frontend/src/hooks/useFeedbackAddPage.ts
+++ frontend/src/hooks/useFeedbackAddPage.ts
@@ -1,9 +1,11 @@
 import { useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
+import useContentTag from 'hooks/useContentTag';
 import useFeedback from 'hooks/useFeedback';
 import useInterviewQuestion from 'hooks/useInterviewQuestion';
 import useLevellog from 'hooks/useLevellog';
+import usePreQuestion from 'hooks/usePreQuestion';
 import useRole from 'hooks/useRole';
 
 import { MESSAGE, ROUTES_PATH } from 'constants/constants';
@@ -12,6 +14,7 @@ const useFeedbackAddPage = () => {
   const { levellogInfo, getLevellog } = useLevellog();
   const { feedbackRef, onClickFeedbackAddButton } = useFeedback();
   const { feedbackWriterRole, getWriterInfo } = useRole();
+  const { preQuestion, getPreQuestion } = usePreQuestion();
   const {
     interviewQuestionInfos,
     interviewQuestionRef,
@@ -21,6 +24,7 @@ const useFeedbackAddPage = () => {
     onSubmitEditInterviewQuestion,
     handleSubmitInterviewQuestion,
   } = useInterviewQuestion();
+  const { whichContentShow, handleClickLevellogTag, handleClickPreQuestionTag } = useContentTag();
   const { teamId, levellogId } = useParams();
   const navigate = useNavigate();
 
@@ -39,6 +43,7 @@ const useFeedbackAddPage = () => {
       const res = await getLevellog({ teamId, levellogId });
       await getWriterInfo({ teamId, participantId: res!.author.id });
       getInterviewQuestion();
+      getPreQuestion({ levellogId });
 
       return;
     }
@@ -52,13 +57,21 @@ const useFeedbackAddPage = () => {
   }, []);
 
   return {
-    state: { levellogInfo, feedbackWriterRole, interviewQuestionInfos },
+    state: {
+      levellogInfo,
+      feedbackWriterRole,
+      interviewQuestionInfos,
+      preQuestion,
+      whichContentShow,
+    },
     ref: { feedbackRef, interviewQuestionRef, interviewQuestionContentRef },
     handler: {
       onClickDeleteInterviewQuestionButton,
       onSubmitEditInterviewQuestion,
       handleClickFeedbackAddButton,
       handleSubmitInterviewQuestion,
+      handleClickLevellogTag,
+      handleClickPreQuestionTag,
     },
   };
 };

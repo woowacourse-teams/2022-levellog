325db9eaf308363458b7a22293871cf2a84913fe
ozragwort <ozragwort@gmail.com>
2022-08-16 12:59:26 +0900

    feat: 사전 질문 상세 조회 시 작성자를 응답에 포함하도록 수정
    Signed-off-by: ozragwort <ozragwort@gmail.com>

---
+++ backend/src/main/java/com/woowacourse/levellog/prequestion/dto/PreQuestionWriteDto.java
@@ -0,0 +1,29 @@
+package com.woowacourse.levellog.prequestion.dto;
+
+import com.woowacourse.levellog.levellog.domain.Levellog;
+import com.woowacourse.levellog.member.domain.Member;
+import com.woowacourse.levellog.prequestion.domain.PreQuestion;
+import javax.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@EqualsAndHashCode
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class PreQuestionWriteDto {
+
+    @NotBlank
+    private String preQuestion;
+
+    public static PreQuestionWriteDto from(String content) {
+        return new PreQuestionWriteDto(content);
+    }
+
+    public PreQuestion toEntity(final Levellog levellog, final Member from) {
+        return new PreQuestion(levellog, from, preQuestion);
+    }
+}

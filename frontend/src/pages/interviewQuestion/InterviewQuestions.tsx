dbbebef8f489234fe9a8d896ddaec2d99b16a691
(yunjin) <sion11290805@gmail.com>
2022-08-16 20:24:50 +0900

    feat: 전체 인터뷰 질문 조회  뷰 구현

--- frontend/src/pages/interviewQuestion/InterviewQuestions.tsx
+++ frontend/src/pages/interviewQuestion/InterviewQuestions.tsx
@@ -1,53 +1,97 @@
-import { useEffect } from 'react';
-
 import styled from 'styled-components';
 
 import useInterviewQuestion from 'hooks/useInterviewQuestion';
 
+import { checkFirstWordFinalConsonant } from 'constants/util';
+
 import ContentHeader from 'components/@commons/ContentHeader';
-import { InterviewQuestionType } from 'types/interviewQuestion';
+import Image from 'components/@commons/Image';
+import {
+  InterviewQuestionsInLevellogType,
+  InterviewQuestionInfoType,
+} from 'types/interviewQuestion';
 
 const InterviewQuestions = () => {
-  const { interviewQuestionInfos, getInterviewQuestion } = useInterviewQuestion();
-
-  useEffect(() => {
-    getInterviewQuestion();
-  }, []);
+  const { interviewQuestionInfosInLevellog } = useInterviewQuestion();
 
   return (
     <>
       <ContentHeader title={'내가 받은 인터뷰 질문'}></ContentHeader>
-      <S.InterviewQuestionContent>
-        {interviewQuestionInfos.length !== 0 &&
-          interviewQuestionInfos.map((interviewQuestionInfo: InterviewQuestionType) => (
-            <S.InterviewQuestionBox key={interviewQuestionInfo.id}>
-              <S.InterviewQuestionText>
-                {interviewQuestionInfo.interviewQuestion}
-              </S.InterviewQuestionText>
-            </S.InterviewQuestionBox>
-          ))}
-      </S.InterviewQuestionContent>
+      <S.Container>
+        {interviewQuestionInfosInLevellog.length !== 0 &&
+          interviewQuestionInfosInLevellog.map(
+            (interviewQuestionInfoInLevellog: InterviewQuestionsInLevellogType) => (
+              <S.Box key={interviewQuestionInfoInLevellog.author.id}>
+                <S.AuthorBox>
+                  <Image src={interviewQuestionInfoInLevellog.author.profileUrl} sizes={'MEDIUM'} />
+                  <S.AuthorText>
+                    {checkFirstWordFinalConsonant({
+                      word: interviewQuestionInfoInLevellog.author.nickname,
+                    })
+                      ? `${interviewQuestionInfoInLevellog.author.nickname}이 `
+                      : `${interviewQuestionInfoInLevellog.author.nickname}가 `}
+                    기록해준 질문들
+                  </S.AuthorText>
+                </S.AuthorBox>
+                <S.Content>
+                  <ul>
+                    {interviewQuestionInfoInLevellog.contents.length !== 0 &&
+                      interviewQuestionInfoInLevellog.contents.map(
+                        (interviewQuestionContent: InterviewQuestionInfoType) => (
+                          <S.Text key={interviewQuestionContent.id}>
+                            <p>{interviewQuestionContent.content}</p>
+                          </S.Text>
+                        ),
+                      )}
+                  </ul>
+                </S.Content>
+              </S.Box>
+            ),
+          )}
+      </S.Container>
     </>
   );
 };
 
 const S = {
-  InterviewQuestionContent: styled.div`
-    display: flex;
+  Container: styled.div`
+    margin: auto;
     width: 100%;
-    flex-direction: column;
-    gap: 0.5rem;
+    max-width: 71rem;
+    min-width: 18.75rem;
   `,
 
-  InterviewQuestionBox: styled.div`
-    padding: 0.5rem 1rem;
+  Box: styled.div`
+    margin-bottom: 3.125rem;
+    border: 0.0625rem solid ${(props) => props.theme.new_default.BLACK};
     border-radius: 0.5rem;
-    background-color: ${(props) => props.theme.default.GRAY};
+    box-shadow: 0 0 1rem ${(props) => props.theme.new_default.GRAY};
   `,
 
-  InterviewQuestionText: styled.p`
+  AuthorBox: styled.div`
+    display: flex;
+    align-items: center;
+    gap: 0.375rem;
+    padding: 0.875rem;
+    width: 100%;
+    border-bottom: 0.0625rem solid ${(props) => props.theme.new_default.BLACK};
+  `,
+
+  AuthorText: styled.p`
+    font-size: 2rem;
+    font-weight: 300;
+  `,
+
+  Content: styled.div`
+    margin: 0.875rem;
+    padding: 0.625rem 0.625rem 0.625rem 1.25rem;
+    border: 0.0625rem solid ${(props) => props.theme.new_default.LIGHT_GRAY};
     line-height: 1.5rem;
     word-break: keep-all;
+  `,
+
+  Text: styled.li`
+    list-style-type: circle;
   `,
 };
 

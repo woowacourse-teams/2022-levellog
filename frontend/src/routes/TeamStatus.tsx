120528913e6dbfa4f8845536d0078088e88b2cf5
jihyeok-um <75592315+jihyeok-um@users.noreply.github.com>
2022-08-16 15:17:41 +0900

    fix: 팀 수정에 관한 버그, 다중역할에 관한 버그를 해결한다. (#295)
    * fix: 팀 수정시 바로 동기화 되지 않는 버그를 수정
    
    * fix: 팀을 수정할 때 "인터뷰어의 수"가 로드되지 않는 버그를 수정
    
    * feat: 2개의 역할을 가진 경우에 대한 대응
    
    * fix: 인증 & 인가 버그 기록
    
    * fix: 약간의 인증&인가 수정
    
    * fix: 블록변수 이중선언 제거

--- frontend/src/routes/TeamStatus.tsx
+++ frontend/src/routes/TeamStatus.tsx
@@ -5,19 +5,24 @@ import useTeam from 'hooks/useTeam';
 
 import { MESSAGE, ROUTES_PATH } from 'constants/constants';
 
-const TeamStatus = ({ allowedStatuses, children }: any) => {
+const TeamStatus = ({ allowedStatuses, children }: TeamStatusProps) => {
   const { team } = useTeam();
   const navigate = useNavigate();
 
-  // 추가로 수정 필요함
   useEffect(() => {
-    if (!allowedStatuses.some((needStatus: any) => needStatus === team.status)) {
-      alert(MESSAGE.WRONG_ACCESS);
-      navigate(ROUTES_PATH.HOME);
+    if (allowedStatuses.some((allowedStatus: any) => allowedStatus === team.status)) {
+      return;
     }
+    alert(MESSAGE.WRONG_ACCESS);
+    navigate(ROUTES_PATH.HOME);
   }, [navigate]);
 
   return children;
 };
+
+interface TeamStatusProps {
+  allowedStatuses: string[];
+  children: JSX.Element;
+}
 
 export default TeamStatus;

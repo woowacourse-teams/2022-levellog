f11e2d763832507e4c4157c9c9d845cf807d7ef6
yunjin-kim <79692272+yunjin-kim@users.noreply.github.com>
2022-08-16 15:44:34 +0900

    feat: 피드백 페이지에서 사전질문 조회 (#305)
    * feat: 레벨로그 관련 타입 추가
    
    * feat: 레벨로그 api 수정으로 인한 훅 수정
    
    * feat: 레벨로그 api 수정으로 인한 컴포넌트 수정
    
    * feat: alert 메시지 추가
    
    * feat: 레벨로그 사전질문 태그 훅 추가
    
    * feat: 레벨로그 사전질문 태그 컴포넌트  추가

---
+++ frontend/src/components/WriterDocument.tsx
@@ -0,0 +1,103 @@
+import styled from 'styled-components';
+
+import Button from 'components/@commons/Button';
+import UiViewer from 'components/@commons/UiViewer';
+import { LevellogInfoType } from 'types/levellog';
+
+const WriterDocument = ({
+  levellogInfo,
+  preQuestion,
+  whichContentShow,
+  handleClickLevellogTag,
+  handleClickPreQuestionTag,
+}: LevellogReportProps) => {
+  return (
+    <S.Container>
+      <S.ButtonBox>
+        <S.LevellogButton whichContentShow={whichContentShow} onClick={handleClickLevellogTag}>
+          레벨로그
+        </S.LevellogButton>
+        <S.PreQuestionButton
+          whichContentShow={whichContentShow}
+          onClick={handleClickPreQuestionTag}
+        >
+          사전질문
+        </S.PreQuestionButton>
+      </S.ButtonBox>
+      <S.Content>
+        {whichContentShow.levellog && <UiViewer content={levellogInfo.content} />}
+        {whichContentShow.preQuestion && <UiViewer content={preQuestion} />}
+      </S.Content>
+    </S.Container>
+  );
+};
+
+interface LevellogReportProps {
+  levellogInfo: LevellogInfoType;
+  preQuestion: string;
+  whichContentShow: {
+    levellog: boolean;
+    preQuestion: boolean;
+  };
+  handleClickLevellogTag: () => void;
+  handleClickPreQuestionTag: () => void;
+}
+
+const S = {
+  Container: styled.div`
+    position: relative;
+    width: 100%;
+    @media (max-width: 520px) {
+      max-width: 22.875rem;
+    }
+  `,
+
+  ButtonBox: styled.div`
+    position: absolute;
+    top: 0;
+    width: 100%;
+    z-index: 10;
+  `,
+
+  LevellogButton: styled(Button)`
+    width: 6.25rem;
+    height: 1.5rem;
+    padding: 0;
+    border-radius: 0 0 0.375rem 0.375rem;
+    background-color: ${(props) =>
+      props.whichContentShow.levellog && props.theme.default.LIGHT_GRAY};
+    font-size: 0.875rem;
+    :hover {
+      background-color: ${(props) => props.theme.default.LIGHT_GRAY};
+    }
+  `,
+
+  PreQuestionButton: styled(Button)`
+    width: 6.25rem;
+    height: 1.5rem;
+    padding: 0;
+    border-radius: 0 0 0.375rem 0.375rem;
+    background-color: ${(props) =>
+      props.whichContentShow.preQuestion && props.theme.default.LIGHT_GRAY};
+    font-size: 0.875rem;
+    :hover {
+      background-color: ${(props) => props.theme.default.LIGHT_GRAY};
+    }
+  `,
+
+  Content: styled.div`
+    overflow: auto;
+    width: 100%;
+    height: 60rem;
+    padding: 2rem 1rem;
+    border-radius: 0.5rem;
+    background-color: ${(props) => props.theme.default.WHITE};
+    @media (max-width: 520px) {
+      flex-direction: column;
+      max-width: 22.875rem;
+      min-height: 0;
+    }
+  `,
+};
+
+export default WriterDocument;

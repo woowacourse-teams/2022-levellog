9088e72c6047c840705df14bba2f6c5285568a03
yunjin-kim <79692272+yunjin-kim@users.noreply.github.com>
2022-08-16 16:52:35 +0900

    feat: 디자인 변경 (#312)
    Co-authored-by: jihyeok-um <visualstudio530@gmail.com>
    
    Co-authored-by: jihyeok-um <visualstudio530@gmail.com>

--- frontend/src/styles/theme.ts
+++ frontend/src/styles/theme.ts
@@ -1,7 +1,8 @@
-import { COLORS } from './colorPalette';
+import { COLORS, NEW_COLORS } from './colorPalette';
 import { IMAGE_SIZE_LIST } from './sizeList';
 
 export const theme = {
   default: COLORS,
+  new_default: NEW_COLORS,
   imageSize: IMAGE_SIZE_LIST,
 };

723986a41231b6de9553a70ab19a59ebefa8ea6e
yunjin-kim <79692272+yunjin-kim@users.noreply.github.com>
2022-08-16 16:52:35 +0900

    feat: 디자인 변경 (#312)
    Co-authored-by: jihyeok-um <visualstudio530@gmail.com>
    
    Co-authored-by: jihyeok-um <visualstudio530@gmail.com>

--- frontend/src/styles/colorPalette.ts
+++ frontend/src/styles/colorPalette.ts
@@ -1,12 +1,31 @@
 export const COLORS = {
   BLACK: '#000000',
-  OPACITY_BLACK: 'rgba(0, 0, 0, 0.4)',
-  INVISIBLE: 'rgba(0, 0, 0, 0)',
+
   DARK_GRAY: '#333333',
   GRAY: '#D9D9D9',
   LIGHT_GRAY: '#F1F1F1',
+
+  OPACITY_BLACK: 'rgba(0, 0, 0, 0.4)',
+  INVISIBLE: 'rgba(0, 0, 0, 0)',
   WHITE: '#FFFFFF',
-  RED: '#FF0000',
+
   WINE: '#B40404',
+  RED: '#FF0000',
   BLUE: '#0050FF',
+};
+
+export const NEW_COLORS = {
+  BLACK: '#000000',
+  DARK_BLACK: '#222222',
+  DARK_GRAY: '#3E3E3E',
+  GRAY: '#BEBEBE',
+  LIGHT_GRAY: '#E2E2E2',
+  WHITE: '#FFFFFF',
+
+  WINE: '#B40404',
+  RED: '#FF0000',
+  BLUE: '#0050FF',
+
+  OPACITY_BLACK: 'rgba(0, 0, 0, 0.4)',
+  INVISIBLE: 'rgba(0, 0, 0, 0)',
 };

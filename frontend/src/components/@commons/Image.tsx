b005c7cc0c755ec0c8266c915fd7fc2db6dcd1d3
jihyeok-um <75592315+jihyeok-um@users.noreply.github.com>
2022-08-17 19:39:15 +0900

    feat: 헤더,푸터,컨텐트헤더의 UI 변경을 반영 (#332)
    * chore: 잘못된 이미지 제거
    
    * design: Header컴포넌트 css 변경
    
    * design: 모바일 반응형 수정
    
    * design: ContentHeader CSS 변경
    
    * design: ContentHeader 반응형 추가
    
    * design: Footer CSS 변경
    
    * refactor: 컨벤션에 맞게 수정

--- frontend/src/components/@commons/Image.tsx
+++ frontend/src/components/@commons/Image.tsx
@@ -1,18 +1,20 @@
 import styled from 'styled-components';
 import { ImageSizeType } from 'types';
 
-const Image = ({ src, sizes = 'LARGE' }: ImageProps) => {
-  return <ImageStyle src={src} sizes={sizes} />;
+const Image = ({ src, margin = '0', sizes = 'LARGE' }: ImageProps) => {
+  return <ImageStyle src={src} sizes={sizes} margin={margin} />;
 };
 
 interface ImageProps {
   src: string;
   sizes: ImageSizeType;
+  margin?: string;
 }
 
-const ImageStyle = styled.img`
+const ImageStyle = styled.img<{ margin: string }>`
   width: ${(props) => props.theme.imageSize[props.sizes!]};
   height: ${(props) => props.theme.imageSize[props.sizes!]};
+  margin: ${(props) => props.margin};
   border-radius: ${(props) => props.theme.imageSize[props.sizes!]};
 `;
 
--- frontend/src/components/header/Header.stories.tsx
+++ frontend/src/components/header/Header.stories.tsx
@@ -1,5 +1,4 @@
 import Header from './Header';
-
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 export default {

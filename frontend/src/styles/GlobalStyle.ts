bc5262992f705b81d85cf3f919ad3f2480c556e6
yunjin-kim <79692272+yunjin-kim@users.noreply.github.com>
2022-08-17 15:56:07 +0900

    feat: 피드백 조회 스타일 수정 (#323)
    * feat: 피드백 조회 스타일 수정
    
    * feat: console 삭제

--- frontend/src/styles/GlobalStyle.ts
+++ frontend/src/styles/GlobalStyle.ts
@@ -42,7 +42,6 @@ const GlobalStyles = createGlobalStyle`
   body {
     height: 100%;
     width: 100%;
-    background-color: #F1F1F1;
     font-family: 'Font_Regular';
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;

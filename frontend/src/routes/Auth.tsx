c1903493b1f7943dfa677b019898c027670f0000
yunjin-kim <79692272+yunjin-kim@users.noreply.github.com>
2022-08-18 15:57:58 +0900

    feat: 예외처리 페이지 추가 (#342)
    * feat: 모달 디자인 수정
    
    * feat: 예외 페이지 추가
    
    * feat: 예외 페이지 라우터 수정
    
    * feat: 이미지 컴포넌트 사이즈 규격 추가
    
    * feat: 예외 페이지 적용
    
    * feat: exception페이지 스타일 수정
    
    * feat: levellogmodal contianer 스타일 수정
    
    * rename: 파일명 수정
    
    * feat: interviewer 스타일 수정
    
    * feat: Auth 수정

--- frontend/src/routes/Auth.tsx
+++ frontend/src/routes/Auth.tsx
@@ -1,13 +1,21 @@
+import { useNavigate } from 'react-router-dom';
+
 import useAuth from 'hooks/useAuth';
 
-import Error from 'pages/status/Error';
 import Loading from 'pages/status/Loading';
+
+import { ROUTES_PATH } from 'constants/constants';
 
 const Auth = ({ children, requireAuth }: AuthProps) => {
   const { isLoad, isError } = useAuth({ requireAuth });
+  const navigate = useNavigate();
 
   if (isLoad) return <Loading />;
-  if (isError) return <Error />;
+  // 동작 체크 필요
+  if (isError) {
+    navigate(ROUTES_PATH.HOME);
+    return <Loading />;
+  }
 
   return children;
 };

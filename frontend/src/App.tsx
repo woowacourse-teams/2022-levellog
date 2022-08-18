2f9ac9823a5a3f769e86e1d2dc2b6c700c751e49
jihyeok-um <75592315+jihyeok-um@users.noreply.github.com>
2022-08-18 22:59:29 +0900

    feat: 피드백 수정 기능을 구현한다 (#351)
    * fix: 페이지 스크롤 수정
    
    * feat: 피드백 수정 기능 추가
    
    * feat: 전체 피드백수정 화면 완료
    
    * chore: 사소한 px -> rem 등의 수정
    
    * design: 디자인 수정
    
    * test: 테스트 코드 수정
    
    * chore: cypress CI 추가
    
    Signed-off-by: ozragwort <ozragwort@gmail.com>
    
    * chore: cypress CI 제거
    
    Signed-off-by: ozragwort <ozragwort@gmail.com>
    
    * feat: 의미없는 함수 제거
    
    Signed-off-by: ozragwort <ozragwort@gmail.com>
    Co-authored-by: ozragwort <ozragwort@gmail.com>
    Co-authored-by: yunjin-kim <79692272+yunjin-kim@users.noreply.github.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useRoutes } from 'react-router-dom';
 
 import styled from 'styled-components';
@@ -9,6 +10,12 @@ import GlobalStyles from 'styles/GlobalStyle';
 
 const App = () => {
   const content = useRoutes(routes);
+  const [prevPathname, setPrevPathname] = useState('');
+
+  if (location.pathname !== prevPathname) {
+    window.scrollTo(0, 0);
+    setPrevPathname(location.pathname);
+  }
 
   return (
     <>
@@ -21,7 +28,7 @@ const App = () => {
 };
 
 const PageContainer = styled.div`
-  min-height: calc(100vh - 8.75rem);
+  min-height: calc(100vh - 15rem);
 `;
 
 export default App;

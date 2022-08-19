2b81e5259941ac2f6d3e0219501489eb8c9a88b3
nailseong <nnis9857@gmail.com>
2022-08-20 01:11:42 +0900

    refactor: 관리자 인수 테스트 개선

--- backend/src/test/java/com/woowacourse/levellog/acceptance/AdminAcceptanceTest.java
+++ backend/src/test/java/com/woowacourse/levellog/acceptance/AdminAcceptanceTest.java
@@ -2,18 +2,15 @@ package com.woowacourse.levellog.acceptance;
 
 import static com.woowacourse.levellog.fixture.MemberFixture.PEPPER;
 import static com.woowacourse.levellog.fixture.MemberFixture.RICK;
+import static com.woowacourse.levellog.fixture.RestAssuredTemplate.delete;
+import static com.woowacourse.levellog.fixture.RestAssuredTemplate.post;
 import static org.hamcrest.Matchers.notNullValue;
-import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 
-import com.woowacourse.levellog.admin.dto.AdminAccessTokenDto;
 import com.woowacourse.levellog.admin.dto.PasswordDto;
-import com.woowacourse.levellog.fixture.RestAssuredTemplate;
-import io.restassured.RestAssured;
 import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 
 @DisplayName("관리자 관련 기능")
 class AdminAcceptanceTest extends AcceptanceTest {
@@ -31,13 +28,7 @@ class AdminAcceptanceTest extends AcceptanceTest {
         final PasswordDto request = new PasswordDto("levellog1!");
 
         // when
-        final ValidatableResponse response = RestAssured.given(specification).log().all()
-                .body(request)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .filter(document("admin/login"))
-                .when()
-                .post("/admin/login")
-                .then().log().all();
+        final ValidatableResponse response = post("/admin/login", request).getResponse();
 
         // then
         response.statusCode(HttpStatus.OK.value())
@@ -46,35 +37,27 @@ class AdminAcceptanceTest extends AcceptanceTest {
 
     /*
      * Scenario: 팀 삭제
+     *   given: 진행 중 인 팀이 존재한다.
      *   given: 관리자가 로그인을 한다.
      *   when: url에 토큰을 포함시켜서 삭제 요청을 한다.
      *   then: 204 상태 코드를 담아 응답받는다.
      */
     @Test
     @DisplayName("진행 중 인 팀 삭제")
-    void delete() {
+    void deleteTeam() {
         // given
         PEPPER.save();
         RICK.save();
 
-        final String teamId = saveTeam("잠실 제이슨조", PEPPER, 1, RICK)
-                .getTeamId();
+        final String teamId = saveTeam("잠실 제이슨조", PEPPER, 1, RICK).getTeamId();
 
         final PasswordDto request = new PasswordDto("levellog1!");
-        final String token = RestAssuredTemplate.post("/admin/login", request)
-                .getResponse()
-                .extract()
-                .as(AdminAccessTokenDto.class)
-                .getAccessToken();
+        final String token = post("/admin/login", request).getAdminToken();
 
         timeStandard.setInProgress();
 
         // when
-        final ValidatableResponse response = RestAssured.given(specification).log().all()
-                .filter(document("admin/delete-team"))
-                .when()
-                .delete("/admin/teams/{teamId}?token={token}", teamId, token)
-                .then().log().all();
+        final ValidatableResponse response = delete("/admin/teams/" + teamId + "?token=" + token).getResponse();
 
         // then
         response.statusCode(HttpStatus.NO_CONTENT.value());

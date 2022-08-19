8099a521b137a2e029c054baea24f0b51e93b3e7
nailseong <nnis9857@gmail.com>
2022-08-20 00:31:18 +0900

    test: 관리자 팀 삭제 기능 테스트

--- backend/src/test/java/com/woowacourse/levellog/acceptance/AdminAcceptanceTest.java
+++ backend/src/test/java/com/woowacourse/levellog/acceptance/AdminAcceptanceTest.java
@@ -1,9 +1,13 @@
 package com.woowacourse.levellog.acceptance;
 
+import static com.woowacourse.levellog.fixture.MemberFixture.PEPPER;
+import static com.woowacourse.levellog.fixture.MemberFixture.RICK;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 
+import com.woowacourse.levellog.admin.dto.AdminAccessTokenDto;
 import com.woowacourse.levellog.admin.dto.PasswordDto;
+import com.woowacourse.levellog.fixture.RestAssuredTemplate;
 import io.restassured.RestAssured;
 import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.DisplayName;
@@ -38,5 +42,39 @@ class AdminAcceptanceTest extends AcceptanceTest {
         // then
         response.statusCode(HttpStatus.OK.value())
                 .body("accessToken", notNullValue());
+    }
+
+    /*
+     * Scenario: 팀 삭제
+     *   given: 관리자가 로그인을 한다.
+     *   when: url에 토큰을 포함시켜서 삭제 요청을 한다.
+     *   then: 204 상태 코드를 담아 응답받는다.
+     */
+    @Test
+    @DisplayName("팀 삭제")
+    void delete() {
+        // given
+        PEPPER.save();
+        RICK.save();
+
+        final String teamId = saveTeam("잠실 제이슨조", PEPPER, 1, RICK)
+                .getTeamId();
+
+        final PasswordDto request = new PasswordDto("levellog1!");
+        final String token = RestAssuredTemplate.post("/admin/login", request)
+                .getResponse()
+                .extract()
+                .as(AdminAccessTokenDto.class)
+                .getAccessToken();
+
+        // when
+        final ValidatableResponse response = RestAssured.given(specification).log().all()
+                .filter(document("admin/delete-team"))
+                .when()
+                .delete("/admin/teams/{teamId}?token={token}", teamId, token)
+                .then().log().all();
+
+        // then
+        response.statusCode(HttpStatus.NO_CONTENT.value());
     }
 }

8441ffa030536f5a77f39a5d97aa149d884dd9ab
2yujeong <yjfflq4872@gmail.com>
2022-08-19 23:29:51 +0900

    test: 페이징이 적용된 findAll 쿼리 메소드 테스트

--- backend/src/main/java/com/woowacourse/levellog/team/domain/TeamRepository.java
+++ backend/src/main/java/com/woowacourse/levellog/team/domain/TeamRepository.java
@@ -6,7 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
 
-    List<Team> findAll(Sort sort);
-
     List<Team> findAllByIsClosed(boolean isClosed, Sort sort);
 }

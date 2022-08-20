2faaff12829523303d2bf7ffc117be0dd64bca82
nailseong <nnis9857@gmail.com>
2022-08-20 12:51:01 +0900

    refactor: 예외 클래스에 컨벤션 반영

--- backend/src/main/java/com/woowacourse/levellog/admin/exception/WrongPasswordException.java
+++ backend/src/main/java/com/woowacourse/levellog/admin/exception/WrongPasswordException.java
@@ -7,9 +7,8 @@ import org.springframework.http.HttpStatus;
 public class WrongPasswordException extends LevellogException {
 
     private static final String CLIENT_MESSAGE = "비밀번호가 틀렸습니다.";
-    private static final HttpStatus HTTP_STATUS = HttpStatus.BAD_REQUEST;
 
     public WrongPasswordException(final DebugMessage debugMessage) {
-        super(CLIENT_MESSAGE, debugMessage, HTTP_STATUS);
+        super(CLIENT_MESSAGE, debugMessage, HttpStatus.BAD_REQUEST);
     }
 }

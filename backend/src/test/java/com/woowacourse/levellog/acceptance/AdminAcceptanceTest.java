61f3d8c1f2863e81421b1e76d68767284918c2cb
Na Ilseong <ilseongdev@gmail.com>
2022-08-22 15:27:02 +0900

    feat: 관리자 기능 추가 (#371)
    * test: 관리자 로그인 인수 테스트
    
    * feat: 관리자 로그인 controller
    
    * docs: 관리자 로그인 기능
    
    * feat: 관리자 로그인 service
    
    * refactor: 테스트에서 bean을 override 하도록 변경
    
    * feat: 관리자 로그인 API를 PublicAPI로 변경
    
    * refactor: url 변경
    
    * feat: 로그인 get 매핑
    
    * chore: handlebars 의존성 추가
    
    Signed-off-by: ozragwort <ozragwort@gmail.com>
    
    * feat: 관리자 로그인 페이지 추가
    
    Signed-off-by: ozragwort <ozragwort@gmail.com>
    
    * feat: 팀 목록 조회 기능
    
    * feat: 관리자 로그인 인터셉터
    
    * feat: 관리자 로그인 인터셉터 등록
    
    * feat: 관리자 로그인 인터셉터 적용 url 변경
    
    * feat: 팀 삭제 기능
    
    * feat: 관리자 토큰 검증 기능
    
    * test: 관리자 팀 삭제 기능 테스트
    
    * fix: 팀 상태에 상관없이 관리자가 팀을 삭제할 수 있도록 변경
    
    * feat: 팀 조회 페이지 추가
    
    Signed-off-by: ozragwort <ozragwort@gmail.com>
    
    * docs: 관리자 API 문서 삭제
    
    * refactor: 관리자 인수 테스트 개선
    
    * test: 관리자 팀 삭제 기능 테스트 추가
    
    * test: 관리자 팀 목록 조회 기능 테스트
    
    * chore: 관리자 관련 코드 jacoco 제외
    
    * style: 코드 재정렬
    
    * refactor: JwtTokenProvider 메서드 & 상수 추출
    
    * refactor: 네이밍 변경
    
    * refactor: 예외 클래스에 컨벤션 반영
    
    * refactor: js 코드 가독성 향상
    
    Signed-off-by: ozragwort <ozragwort@gmail.com>
    
    * refactor: html 코드 품질 향상
    
    Signed-off-by: ozragwort <ozragwort@gmail.com>
    
    * refactor: ddl-auto create로 수정
    
    Signed-off-by: ozragwort <ozragwort@gmail.com>
    
    * style: 테스트 코드 컨벤션 적용
    
    * style: 코드 컨벤션 적용
    
    * chore: 불필요한 테스트 제거
    
    Signed-off-by: ozragwort <ozragwort@gmail.com>
    Co-authored-by: ozragwort <ozragwort@gmail.com>
    Co-authored-by: Roma <52696169+kbsat@users.noreply.github.com>

---
+++ backend/src/test/java/com/woowacourse/levellog/acceptance/AdminAcceptanceTest.java
@@ -0,0 +1,65 @@
+package com.woowacourse.levellog.acceptance;
+
+import static com.woowacourse.levellog.fixture.MemberFixture.PEPPER;
+import static com.woowacourse.levellog.fixture.MemberFixture.RICK;
+import static com.woowacourse.levellog.fixture.RestAssuredTemplate.delete;
+import static com.woowacourse.levellog.fixture.RestAssuredTemplate.post;
+import static org.hamcrest.Matchers.notNullValue;
+
+import com.woowacourse.levellog.admin.dto.AdminPasswordDto;
+import io.restassured.response.ValidatableResponse;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+@DisplayName("관리자 관련 기능")
+class AdminAcceptanceTest extends AcceptanceTest {
+
+    /*
+     * Scenario: 관리자 로그인
+     *   given: 비밀번호를 암호화한 hash값이 서버에 저장되어 있다.
+     *   when: 사전에 전달 받은 비밀번호를 이용해 로그인을 요청한다.
+     *   then: 200 OK 상태 코드와 body에 accessToken을 담아 응답받는다.
+     */
+    @Test
+    @DisplayName("관리자 로그인")
+    void login() {
+        // given
+        final AdminPasswordDto request = new AdminPasswordDto("levellog1!");
+
+        // when
+        final ValidatableResponse response = post("/admin/login", request).getResponse();
+
+        // then
+        response.statusCode(HttpStatus.OK.value())
+                .body("accessToken", notNullValue());
+    }
+
+    /*
+     * Scenario: 팀 삭제
+     *   given: 진행 중 인 팀이 존재한다.
+     *   given: 관리자가 로그인을 한다.
+     *   when: url에 토큰을 포함시켜서 삭제 요청을 한다.
+     *   then: 204 상태 코드를 담아 응답받는다.
+     */
+    @Test
+    @DisplayName("진행 중 인 팀 삭제")
+    void deleteTeam() {
+        // given
+        PEPPER.save();
+        RICK.save();
+
+        final String teamId = saveTeam("잠실 제이슨조", PEPPER, 1, RICK).getTeamId();
+
+        final AdminPasswordDto request = new AdminPasswordDto("levellog1!");
+        final String token = post("/admin/login", request).getAdminToken();
+
+        timeStandard.setInProgress();
+
+        // when
+        final ValidatableResponse response = delete("/admin/teams/" + teamId + "?token=" + token).getResponse();
+
+        // then
+        response.statusCode(HttpStatus.NO_CONTENT.value());
+    }
+}
---
+++ backend/src/test/java/com/woowacourse/levellog/application/AdminServiceTest.java
@@ -0,0 +1,161 @@
+package com.woowacourse.levellog.application;
+
+import static com.woowacourse.levellog.authentication.support.JwtTokenProvider.ADMIN_TOKEN_PAYLOAD;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import com.woowacourse.levellog.admin.dto.AdminAccessTokenDto;
+import com.woowacourse.levellog.admin.dto.AdminPasswordDto;
+import com.woowacourse.levellog.admin.dto.AdminTeamDto;
+import com.woowacourse.levellog.admin.exception.WrongPasswordException;
+import com.woowacourse.levellog.fixture.TimeFixture;
+import com.woowacourse.levellog.member.domain.Member;
+import com.woowacourse.levellog.team.domain.Team;
+import com.woowacourse.levellog.team.exception.TeamNotFoundException;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("AdminService의")
+public class AdminServiceTest extends ServiceTest {
+
+    @Nested
+    @DisplayName("login 메서드는")
+    class Login {
+
+        @Test
+        @DisplayName("암호화된 비밀번호와 일치하는 비밀번호를 입력하면 token을 발급한다.")
+        void success() {
+            // given
+            final AdminPasswordDto request = new AdminPasswordDto("levellog1!");
+
+            // when
+            final AdminAccessTokenDto actual = adminService.login(request);
+
+            // then
+            final String token = actual.getAccessToken();
+            final String payload = jwtTokenProvider.getPayload(token);
+
+            assertThat(payload).isEqualTo(ADMIN_TOKEN_PAYLOAD);
+        }
+
+        @Test
+        @DisplayName("잘못된 비밀번호를 입력하면 예외를 던진다.")
+        void login_wrongPassword_exception() {
+            // given
+            final AdminPasswordDto request = new AdminPasswordDto("wrong-password");
+
+            // when & then
+            assertThatThrownBy(() -> adminService.login(request))
+                    .isInstanceOf(WrongPasswordException.class);
+        }
+    }
+
+    @Nested
+    @DisplayName("findAllTeam 메서드는")
+    class FindAllTeam {
+
+        @Test
+        @DisplayName("팀 목록을 조회한다.")
+        void success() {
+            // given
+            final Member rick = saveMember("릭");
+            final Member eve = saveMember("이브");
+            final Member alien = saveMember("알린");
+
+            saveTeam(rick, eve);
+            saveTeam(eve, alien);
+
+            // when
+            final List<AdminTeamDto> actual = adminService.findAllTeam();
+
+            // then
+            assertThat(actual).hasSize(2);
+        }
+    }
+
+    @Nested
+    @DisplayName("deleteTeamById 메서드는")
+    class DeleteTeamById {
+
+        @Test
+        @DisplayName("팀 id가 주어지면 해당하는 팀을 삭제한다. - status: ready")
+        void deleteTeamById_ready_success() {
+            // given
+            final Member rick = saveMember("릭");
+            final Member alien = saveMember("알린");
+
+            final Team team = saveTeam(rick, alien);
+            final Long teamId = team.getId();
+
+            // when
+            adminService.deleteTeamById(teamId);
+
+            entityManager.flush();
+            entityManager.clear();
+
+            // then
+            final Optional<Team> actual = teamRepository.findById(teamId);
+            assertThat(actual).isEmpty();
+        }
+
+        @Test
+        @DisplayName("팀 id가 주어지면 해당하는 팀을 삭제한다. - status: inProgress")
+        void deleteTeamById_inProgress_success() {
+            // given
+            final Member rick = saveMember("릭");
+            final Member alien = saveMember("알린");
+
+            final Team team = saveTeam(rick, alien);
+            final Long teamId = team.getId();
+
+            timeStandard.setInProgress();
+
+            // when
+            adminService.deleteTeamById(teamId);
+
+            entityManager.flush();
+            entityManager.clear();
+
+            // then
+            final Optional<Team> actual = teamRepository.findById(teamId);
+            assertThat(actual).isEmpty();
+        }
+
+        @Test
+        @DisplayName("팀 id가 주어지면 해당하는 팀을 삭제한다. - status: closed")
+        void deleteTeamById_closed_success() {
+            // given
+            final Member rick = saveMember("릭");
+            final Member alien = saveMember("알린");
+
+            final Team team = saveTeam(rick, alien);
+            final Long teamId = team.getId();
+
+            team.close(TimeFixture.AFTER_START_TIME);
+
+            // when
+            adminService.deleteTeamById(teamId);
+
+            entityManager.flush();
+            entityManager.clear();
+
+            // then
+            final Optional<Team> actual = teamRepository.findById(teamId);
+            assertThat(actual).isEmpty();
+        }
+
+        @Test
+        @DisplayName("id에 해당하는 팀이 존재하지 않으면 예외를 던진다.")
+        void deleteTeamById_notFound_exception() {
+            // given
+            final Long teamId = 999L;
+
+            // when & then
+            assertThatThrownBy(() -> adminService.deleteTeamById(teamId))
+                    .isInstanceOf(TeamNotFoundException.class);
+        }
+    }
+}

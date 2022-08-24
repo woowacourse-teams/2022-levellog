bc5754458d54c819f1d40a3caadcd7d933e17ace
nailseong <nnis9857@gmail.com>
2022-08-24 20:49:10 +0900

    refactor: 패키지 분리

--- backend/src/main/resources/static/js/team.js
+++ backend/src/main/resources/static/js/team.js
@@ -2,32 +2,6 @@ function getAdminToken() {
     return localStorage.getItem("admin-token");
 }
 
-function adminLogin() {
-    const password = document.getElementById('admin-password').value;
-
-    fetch(`/admin/login`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({value: password}),
-    }).then(
-        res => res.json()
-    ).then(value => {
-        if (value['message'] !== undefined) {
-            alert(value['message']);
-            document.getElementById('admin-password').value = "";
-            return;
-        }
-
-        localStorage.setItem("admin-token", value['accessToken']);
-        const token = localStorage.getItem("admin-token");
-        location.replace(`teams?token=${token}`);
-    });
-
-    return false;
-}
-
 function deleteTeam(teamId) {
     const checkDelete = prompt(`정말 삭제하시겠습니까? 삭제하려면 팀 ID(${teamId}) 입력`);
 

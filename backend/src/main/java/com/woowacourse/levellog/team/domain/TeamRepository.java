c6ba2ff95d573a61ca88874f3ab6f02f34407d43
2yujeong <76840965+2yujeong@users.noreply.github.com>
2022-08-31 19:29:21 +0900

    feature: 팀 목록 조회 기능에 페이징 적용 (#374)
    * test: 페이징이 적용된 findAll 쿼리 메소드 테스트
    
    * feat: Team 전체 조회 기능에 페이징 적용
    
    * feat: 필터링 된 Team 목록 조회 기능에 페이징 적용
    
    * docs: 팀 목록 조회 API 문서 업데이트
    
    * refactor: defaultValue 설정, Optional 제거
    
    * test: 테스트 내용을 명확하게 이해할 수 있도록 URL, 네이밍 수정
    
    * refactor: Code Smells 제거
    
    * refactor: Paging size defaultValue 수정
    
    * docs: 팀 API 문서 수정

--- backend/src/main/java/com/woowacourse/levellog/team/domain/TeamRepository.java
+++ backend/src/main/java/com/woowacourse/levellog/team/domain/TeamRepository.java
@@ -1,12 +1,10 @@
 package com.woowacourse.levellog.team.domain;
 
-import java.util.List;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
 
-    List<Team> findAll(Sort sort);
-
-    List<Team> findAllByIsClosed(boolean isClosed, Sort sort);
+    Page<Team> findAllByIsClosed(boolean isClosed, Pageable pageable);
 }

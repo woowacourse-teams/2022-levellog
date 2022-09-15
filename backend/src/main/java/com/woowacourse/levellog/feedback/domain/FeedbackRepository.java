0ead66c45a0ed4362c8234a71a697632667a206a
nailseong <nnis9857@gmail.com>
2022-09-15 21:10:44 +0900

    refactor: 피드백 전체 조회 N+1 쿼리 개선

--- backend/src/main/java/com/woowacourse/levellog/feedback/domain/FeedbackRepository.java
+++ backend/src/main/java/com/woowacourse/levellog/feedback/domain/FeedbackRepository.java
@@ -4,12 +4,15 @@ import com.woowacourse.levellog.levellog.domain.Levellog;
 import com.woowacourse.levellog.member.domain.Member;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
 
-    List<Feedback> findAllByLevellog(Levellog levellog);
+    @Query("SELECT f FROM Feedback f JOIN FETCH f.from WHERE f.levellog = :levellog")
+    List<Feedback> findAllByLevellog(@Param("levellog") Levellog levellog);
 
     List<Feedback> findAllByToOrderByUpdatedAtDesc(Member member);
 

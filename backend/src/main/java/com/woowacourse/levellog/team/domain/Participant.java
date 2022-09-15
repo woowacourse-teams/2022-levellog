30caa23de0c98aa923ed18e57fa925338ce3b1e1
nailseong <nnis9857@gmail.com>
2022-09-15 21:13:01 +0900

    refactor: Participant Entity 연관관계에 지연로딩 전략 적용

--- backend/src/main/java/com/woowacourse/levellog/team/domain/Participant.java
+++ backend/src/main/java/com/woowacourse/levellog/team/domain/Participant.java
@@ -3,6 +3,7 @@ package com.woowacourse.levellog.team.domain;
 import com.woowacourse.levellog.common.domain.BaseEntity;
 import com.woowacourse.levellog.member.domain.Member;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.ForeignKey;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -17,11 +18,11 @@ import lombok.NoArgsConstructor;
 @Getter
 public class Participant extends BaseEntity {
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(nullable = false, foreignKey = @ForeignKey(name = "fk_participant_team"))
     private Team team;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(nullable = false, foreignKey = @ForeignKey(name = "fk_participant_member"))
     private Member member;
 

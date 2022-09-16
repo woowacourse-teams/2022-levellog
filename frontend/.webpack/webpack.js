d99eeb3295a271f8f786592864dab3e0433b3289
(yunjin) <sion11290805@gmail.com>
2022-09-16 14:14:29 +0900

    feat: webpack, babel 설정 수정

--- frontend/.webpack/webpack.js
+++ frontend/.webpack/webpack.js
@@ -1,15 +1,30 @@
 const webpack = require('webpack');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = merge(common, {
-  mode: 'none',
+  mode: 'development',
   devtool: 'eval-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              plugins: [require.resolve('react-refresh/babel')],
+            },
+          },
+        ],
+      },
+    ],
+  },
   devServer: {
     static: {
       directory: '../dist',
     },
-    hot: true,
     compress: true,
     port: 3000,
     historyApiFallback: true,
@@ -20,5 +35,6 @@ module.exports = merge(common, {
       API_URI: 'https://local.levellog.app/api',
       CLIENT_ID: '7a432a0c919a356b4efa',
     }),
+    new ReactRefreshWebpackPlugin(),
   ],
 });

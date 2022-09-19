8ed08838e7fe928a777202350dfc4cdfaf116db0
ozragwort <ozragwort@gmail.com>
2022-09-19 17:59:42 +0900

    refactor: 필요없는 검증 제거
    Signed-off-by: ozragwort <ozragwort@gmail.com>

--- backend/src/main/java/com/woowacourse/levellog/interviewquestion/domain/InterviewQuestionLikes.java
+++ backend/src/main/java/com/woowacourse/levellog/interviewquestion/domain/InterviewQuestionLikes.java
@@ -1,7 +1,6 @@
 package com.woowacourse.levellog.interviewquestion.domain;
 
 import com.woowacourse.levellog.common.domain.BaseEntity;
-import com.woowacourse.levellog.common.exception.InvalidFieldException;
 import com.woowacourse.levellog.common.support.DebugMessage;
 import com.woowacourse.levellog.interviewquestion.exception.InvalidInterviewQuestionException;
 import com.woowacourse.levellog.member.domain.Member;
@@ -32,23 +31,6 @@ public class InterviewQuestionLikes extends BaseEntity {
     }
 
     public static InterviewQuestionLikes of(final InterviewQuestion interviewQuestion, final Member liker) {
-        validateInterviewQuestion(interviewQuestion);
-        validateLiker(liker);
-
         return new InterviewQuestionLikes(interviewQuestion.getId(), liker.getId());
-    }
-
-    private static void validateInterviewQuestion(InterviewQuestion interviewQuestion) {
-        if (interviewQuestion.getId() == null) {
-            throw new InvalidInterviewQuestionException(DebugMessage.init()
-                    .append("interviewQuestionId", interviewQuestion.getId()));
-        }
-    }
-
-    private static void validateLiker(Member liker) {
-        if (liker.getId() == null) {
-            throw new InvalidMemberException(DebugMessage.init()
-                    .append("memberId", liker.getId()));
-        }
     }
 }

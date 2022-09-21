e7900a8a75ac558d6620355b8a815d758294b9c5
jihyeok-um <75592315+jihyeok-um@users.noreply.github.com>
2022-09-21 19:59:16 +0900

    feat: 저작권 페이지 추가

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,9 +1,16 @@
+import { Link } from 'react-router-dom';
+
 import styled from 'styled-components';
+
+import { ROUTES_PATH } from 'constants/constants';
 
 const Footer = () => {
   return (
     <S.Container>
-      <S.Title>@2022 Level Log</S.Title>
+      <S.Title>@ 2022 LevelLog</S.Title>
+      <Link to={ROUTES_PATH.COPYRIGHT}>
+        <S.Content>저작권</S.Content>
+      </Link>
     </S.Container>
   );
 };
@@ -12,17 +19,35 @@ const S = {
   Container: styled.footer`
     bottom: 0;
     display: flex;
-    justify-content: center;
+    justify-content: flex-start;
     align-items: center;
     width: 100%;
     height: 4.375rem;
     border-top: 0.125rem solid ${(props) => props.theme.new_default.LIGHT_GRAY};
     background-color: ${(props) => props.theme.new_default.WHITE};
+    @media (min-width: 1620px) {
+      padding: 0 calc((100vw - 100rem) / 2);
+    }
+    @media (max-width: 1620px) {
+      padding: 0 1.25rem;
+    }
   `,
 
   Title: styled.p`
-    font-size: 1.25rem;
-    font-weight: 300;
+    margin-right: 0.625rem;
+    color: ${(props) => props.theme.new_default.DARK_GRAY};
+    font-size: 1rem;
+    font-weight: 500;
+  `,
+
+  Content: styled.p`
+    margin-right: 0.625rem;
+    color: ${(props) => props.theme.new_default.DARK_GRAY};
+    font-size: 1rem;
+    font-weight: 500;
+    &:hover {
+      border-bottom: 1px solid ${(props) => props.theme.new_default.DARK_GRAY};
+    }
   `,
 };
 

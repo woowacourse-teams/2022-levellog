f767b202f87f479bd147c79a417c43996a9bd194
(yunjin) <sion11290805@gmail.com>
2022-09-21 17:40:09 +0900

    feat: 유티함수 추가

--- frontend/src/utils/util.ts
+++ frontend/src/utils/util.ts
@@ -1,5 +1,65 @@
+import { NavigateFunction } from 'react-router-dom';
+
+import axios, { AxiosResponse } from 'axios';
+
+import { ShowSnackbarProps } from 'hooks/useSnackbar';
+
+import { MESSAGE } from 'constants/constants';
+
 import { Team } from 'types/team';
 import { UriCustomHookType } from 'types/uri';
+
+export const debounce: DebounceType = {
+  flag: '',
+  action({ func, args, setState, navigate }) {
+    if (this.flag) {
+      clearTimeout(this.flag);
+    }
+    this.flag = setTimeout(async () => {
+      const res = await func(args && { ...args });
+      setState && setState(res.data);
+      res && navigate && navigate[0](navigate[1]);
+    }, 300);
+  },
+};
+
+interface DebounceActionArgsType {
+  func: Function;
+  args?: { [props: string]: any };
+  setState?: React.Dispatch<React.SetStateAction<any>>;
+  navigate?: [NavigateFunction, string];
+}
+
+interface DebounceType {
+  flag: '' | ReturnType<typeof setTimeout>;
+  action: ({ func, args }: DebounceActionArgsType) => any;
+}
+
+export const tryCatch = async ({ func, args, snackbar }: TryCatchProps) => {
+  try {
+    const res = await func(args && { ...args });
+    return res.data;
+  } catch (err: unknown) {
+    if (axios.isAxiosError(err)) {
+      const responseBody: AxiosResponse = err.response!;
+      if (err instanceof Error) {
+        snackbar({
+          message: responseBody.data.message || MESSAGE.CAN_NOT_EXPECT_ERROR,
+        });
+      }
+    } else {
+      snackbar({ message: MESSAGE.CAN_NOT_EXPECT_ERROR });
+    }
+
+    return false;
+  }
+};
+
+interface TryCatchProps {
+  func: Function;
+  args?: { [props: string]: any };
+  snackbar: ({ message }: ShowSnackbarProps) => any;
+}
 
 export const checkFirstWordFinalConsonant = ({ word }: CheckFirstWordFinalConsonantType) => {
   if (typeof word !== 'string') return;
@@ -10,6 +70,18 @@ export const checkFirstWordFinalConsonant = ({ word }: CheckFirstWordFinalConson
   if (uniCode < 44032 || uniCode > 55203) return;
 
   return (uniCode - 44032) % 28 != 0;
+};
+interface CheckFirstWordFinalConsonantType {
+  word: string;
+}
+
+export const convertDateAndTime = ({ startAt }: Pick<Team, 'startAt'>) => {
+  const year = startAt.slice(0, 4);
+  const month = startAt.slice(5, 7);
+  const day = startAt.slice(8, 10);
+  const time = `${startAt.slice(11, 13)}시 ${startAt.slice(14, 16)}분`;
+
+  return `${year}년 ${month}월 ${day}일 ${time}`;
 };
 
 export const feedbackAddUriBuilder = ({
@@ -85,16 +157,3 @@ export const interviewQuestionsGetUriBuilder = ({
 }: Pick<UriCustomHookType, 'teamId' | 'levellogId'>) => {
   return `/teams/${teamId}/levellogs/${levellogId}/interview-questions`;
 };
-
-export const convertDateAndTime = ({ startAt }: Pick<Team, 'startAt'>) => {
-  const year = startAt.slice(0, 4);
-  const month = startAt.slice(5, 7);
-  const day = startAt.slice(8, 10);
-  const time = `${startAt.slice(11, 13)}시 ${startAt.slice(14, 16)}분`;
-
-  return `${year}년 ${month}월 ${day}일 ${time}`;
-};
-
-interface CheckFirstWordFinalConsonantType {
-  word: string;
-}

90821a738487f5baad8406baadb65ba274117752
ozragwort <ozragwort@gmail.com>
2022-09-22 11:28:06 +0900

    fix: 인젝션 시 사용하는 문자를 제거하여 쿼리를 만들도록 수정
    Signed-off-by: ozragwort <ozragwort@gmail.com>

--- backend/src/main/java/com/woowacourse/levellog/interviewquestion/domain/InterviewQuestionQueryRepository.java
+++ backend/src/main/java/com/woowacourse/levellog/interviewquestion/domain/InterviewQuestionQueryRepository.java
@@ -1,5 +1,6 @@
 package com.woowacourse.levellog.interviewquestion.domain;
 
+import com.woowacourse.levellog.common.support.StringConverter;
 import com.woowacourse.levellog.interviewquestion.dto.InterviewQuestionSearchResultDto;
 import java.util.List;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -25,16 +26,15 @@ public class InterviewQuestionQueryRepository {
     public List<InterviewQuestionSearchResultDto> searchByKeyword(final String keyword, final Long memberId,
                                                                   final Long size, final Long page,
                                                                   final InterviewQuestionSort sort) {
-        final String wrappedKeyword = "'%" + keyword + "%'";
         final String sql = "SELECT id, content, "
                 + "(select CASE WHEN (id IN (select interview_question_id from interview_question_likes where liker_id = ?)) "
                 + "THEN true ELSE false END ) AS press, "
                 + "like_count AS likeCount "
                 + "FROM interview_question "
-                + "WHERE content LIKE ? "
+                + "WHERE content LIKE '%" + StringConverter.toSafeString(keyword) + "%' "
                 + String.format("ORDER BY %s %s ", sort.getField(), sort.getOrder())
                 + "LIMIT ? OFFSET ?";
 
-        return jdbcTemplate.query(sql, searchRowMapper, memberId, wrappedKeyword, size, page * size);
+        return jdbcTemplate.query(sql, searchRowMapper, memberId, size, page * size);
     }
 }

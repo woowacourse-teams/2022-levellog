c4dd77ec524b2c007542404a71855c67556d2e1e
nailseong <nnis9857@gmail.com>
2022-09-24 22:18:28 +0900

    feat: getPreQuestion

--- backend/src/test/java/com/woowacourse/levellog/domain/PreQuestionRepositoryTest.java
+++ backend/src/test/java/com/woowacourse/levellog/domain/PreQuestionRepositoryTest.java
@@ -1,12 +1,14 @@
 package com.woowacourse.levellog.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.woowacourse.levellog.levellog.domain.Levellog;
 import com.woowacourse.levellog.member.domain.Member;
 import com.woowacourse.levellog.prequestion.domain.PreQuestion;
+import com.woowacourse.levellog.prequestion.exception.PreQuestionNotFoundException;
 import com.woowacourse.levellog.team.domain.Team;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
@@ -15,6 +17,41 @@ import org.junit.jupiter.api.Test;
 
 @DisplayName("PreQuestionRepository의")
 class PreQuestionRepositoryTest extends RepositoryTest {
+
+    @Nested
+    @DisplayName("getPreQuestion 메서드는")
+    class GetPreQuestion {
+
+        @Test
+        @DisplayName("preQuestionId에 해당하는 레코드가 존재하면 id에 해당하는 PreQuestion 엔티티를 반환한다.")
+        void success() {
+            // given
+            final Member to = saveMember("릭");
+            final Member from = saveMember("로마");
+            final Team team = saveTeam(to, from);
+            final Levellog levellog = saveLevellog(to, team);
+            final Long expected = savePreQuestion(levellog, from)
+                    .getId();
+
+            // when
+            final Long actual = preQuestionRepository.getPreQuestion(expected)
+                    .getId();
+
+            // then
+            assertThat(actual).isEqualTo(expected);
+        }
+
+        @Test
+        @DisplayName("preQuestionId에 해당하는 레코드가 존재하지 않으면 예외를 던진다.")
+        void getPreQuestion_notExist_execption() {
+            // given
+            final Long preQuestionId = 999L;
+
+            // when & then
+            assertThatThrownBy(() -> preQuestionRepository.getPreQuestion(preQuestionId))
+                    .isInstanceOf(PreQuestionNotFoundException.class);
+        }
+    }
 
     @Test
     @DisplayName("findByLevellogAndAuthor 메서드는 Levellog와 Author가 같은 사전 질문을 반환한다.")

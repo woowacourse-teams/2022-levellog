f1fae846e353a4cb8b5a613c41a77b9f099eec07
nailseong <nnis9857@gmail.com>
2022-09-24 22:23:57 +0900

    feat: getTeam

--- backend/src/main/java/com/woowacourse/levellog/team/domain/TeamRepository.java
+++ backend/src/main/java/com/woowacourse/levellog/team/domain/TeamRepository.java
@@ -1,6 +1,14 @@
 package com.woowacourse.levellog.team.domain;
 
+import com.woowacourse.levellog.common.support.DebugMessage;
+import com.woowacourse.levellog.team.exception.TeamNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
+
+    default Team getTeam(final Long teamId) {
+        return findById(teamId)
+                .orElseThrow(() -> new TeamNotFoundException(DebugMessage.init()
+                        .append("teamId", teamId)));
+    }
 }

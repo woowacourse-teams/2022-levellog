00f1f168afd236b70a5681aff6914e864b37aedf
jihyeok-um <75592315+jihyeok-um@users.noreply.github.com>
2022-10-03 19:15:18 +0900

    refactor: 의미없는 await 제거, 에러 처리 로직 추가

--- frontend/src/hooks/useTeams.ts
+++ frontend/src/hooks/useTeams.ts
@@ -1,12 +1,18 @@
 import { useState } from 'react';
 
+import axios, { AxiosResponse } from 'axios';
+
 import { TEAMS_CONDITION } from 'constants/constants';
 
+import useSnackbar from './useSnackbar';
 import { requestGetMyTeams } from 'apis/myInfo';
 import { requestGetTeams } from 'apis/teams';
+import { NotCorrectToken } from 'apis/utils';
 import { InterviewTeamType, TeamApiType, TeamConditionsType } from 'types/team';
 
 const useTeams = () => {
+  const { showSnackbar } = useSnackbar();
+
   const [teams, setTeams] = useState<InterviewTeamType[]>([]);
   const [teamsCondition, setTeamsCondition] = useState({
     open: true,
@@ -17,46 +23,55 @@ const useTeams = () => {
   const accessToken = localStorage.getItem('accessToken');
 
   const getTeams = async ({ teamsCondition }: Record<'teamsCondition', TeamConditionsType>) => {
-    if (teamsCondition.open) {
-      const res = await requestGetTeams({ accessToken, teamsCondition: TEAMS_CONDITION.OPEN });
-      setTeams(res.data.teams);
+    try {
+      if (teamsCondition.open) {
+        const res = await requestGetTeams({ accessToken, teamsCondition: TEAMS_CONDITION.OPEN });
+        setTeams(res.data.teams);
 
-      return res.data;
-    }
+        return res.data;
+      }
 
-    if (teamsCondition.close) {
-      const res = await requestGetTeams({ accessToken, teamsCondition: TEAMS_CONDITION.CLOSE });
-      setTeams(res.data.teams);
+      if (teamsCondition.close) {
+        const res = await requestGetTeams({ accessToken, teamsCondition: TEAMS_CONDITION.CLOSE });
+        setTeams(res.data.teams);
 
-      return res.data;
-    }
+        return res.data;
+      }
 
-    if (teamsCondition.my) {
-      const res = await requestGetMyTeams({ accessToken });
-      setTeams(res.data.teams);
+      if (teamsCondition.my) {
+        const res = await requestGetMyTeams({ accessToken });
+        setTeams(res.data.teams);
 
-      return res.data;
+        return res.data;
+      }
+    } catch (err: unknown) {
+      if (axios.isAxiosError(err) && err instanceof Error) {
+        const responseBody: AxiosResponse = err.response!;
+        if (NotCorrectToken({ message: responseBody.data.message, showSnackbar })) {
+          showSnackbar({ message: responseBody.data.message });
+        }
+      }
     }
   };
 
-  const handleClickFilterButtons = async (e: React.MouseEvent<HTMLElement>) => {
+  const handleClickFilterButtons = (e: React.MouseEvent<HTMLElement>) => {
     const eventTarget = e.target as HTMLElement;
 
     switch (eventTarget.innerText) {
       case '진행중인 인터뷰':
         if (teamsCondition.open) return;
 
-        await setTeamsCondition({ open: true, close: false, my: false });
+        setTeamsCondition({ open: true, close: false, my: false });
         break;
       case '종료된 인터뷰':
         if (teamsCondition.close === true) return;
 
-        await setTeamsCondition({ open: false, close: true, my: false });
+        setTeamsCondition({ open: false, close: true, my: false });
         break;
       case '나의 인터뷰':
         if (teamsCondition.my) return;
 
-        await setTeamsCondition({ open: false, close: false, my: true });
+        setTeamsCondition({ open: false, close: false, my: true });
         break;
     }
   };

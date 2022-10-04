707e50ed12448eb8ddee58569de2f62a898c0d99
ozragwort <ozragwort@gmail.com>
2022-10-04 19:03:27 +0900

    fix: API 에 맞도록 팀 상세 조회 시 멤버 프로필 url 추가
    Signed-off-by: ozragwort <ozragwort@gmail.com>

--- backend/src/main/java/com/woowacourse/levellog/team/dto/query/AllParticipantQueryResult.java
+++ backend/src/main/java/com/woowacourse/levellog/team/dto/query/AllParticipantQueryResult.java
@@ -12,32 +12,34 @@ public class AllParticipantQueryResult {
     private final String title;
     private final String place;
     private final LocalDateTime startAt;
-    private final String profileUrl;
+    private final String teamProfileUrl;
     private final int interviewerNumber;
     private final boolean isClosed;
     private final Long memberId;
     private final Long levellogId;
     private final Long preQuestionId;
     private final String nickname;
+    private final String profileUrl;
     private final boolean isHost;
     private final boolean isWatcher;
 
     public AllParticipantQueryResult(final Long id, final String title, final String place, final LocalDateTime startAt,
-                                     final String profileUrl, final int interviewerNumber, final boolean isClosed,
+                                     final String teamProfileUrl, final int interviewerNumber, final boolean isClosed,
                                      final Long memberId, final Long levellogId, final Long preQuestionId,
-                                     final String nickname, final boolean isHost,
-                                     final boolean isWatcher) {
+                                     final String nickname, final String profileUrl,
+                                     final boolean isHost, final boolean isWatcher) {
         this.teamId = id;
         this.title = title;
         this.place = place;
         this.startAt = startAt;
-        this.profileUrl = profileUrl;
+        this.teamProfileUrl = teamProfileUrl;
         this.interviewerNumber = interviewerNumber;
         this.isClosed = isClosed;
         this.memberId = memberId;
         this.levellogId = levellogId;
         this.preQuestionId = preQuestionId;
         this.nickname = nickname;
+        this.profileUrl = profileUrl;
         this.isHost = isHost;
         this.isWatcher = isWatcher;
     }

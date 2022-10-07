7043af699c0f9a18eb43cbe0d81c3c80325bf910
BeomsooKim <kbsat@naver.com>
2022-10-07 20:10:32 +0900

    test: isParticipant, toInterviewRole 테스트 추가
    Co-authored-by: 2yujeong <yjfflq4872@gmail.com>

--- backend/src/test/java/com/woowacourse/levellog/domain/ParticipantsTest.java
+++ backend/src/test/java/com/woowacourse/levellog/domain/ParticipantsTest.java
@@ -2,122 +2,69 @@ package com.woowacourse.levellog.domain;
 
 
 import static com.woowacourse.levellog.fixture.TimeFixture.TEAM_START_TIME;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import com.woowacourse.levellog.common.domain.MockEntityFactory;
+import com.woowacourse.levellog.team.domain.InterviewRole;
+import com.woowacourse.levellog.team.domain.Participants;
 import com.woowacourse.levellog.team.domain.ParticipantsIngredient;
 import com.woowacourse.levellog.team.domain.Team;
 import com.woowacourse.levellog.team.domain.TeamDetail;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 @DisplayName("Participants의")
 class ParticipantsTest {
 
-    private Team saveTeam() {
-        final TeamDetail teamDetail = new TeamDetail("네오와 함께하는 레벨 인터뷰", "선릉 트랙룸", TEAM_START_TIME, "profileUrl", 2);
-        final ParticipantsIngredient participantsIngredient = new ParticipantsIngredient(1L, List.of(1L, 2L, 3L),
-                List.of(4L, 5L, 6L));
+    @Nested
+    @DisplayName("toInterviewRole 메서드는")
+    class ToInterviewRole {
 
-        return new Team(teamDetail, participantsIngredient);
+        @ParameterizedTest(name = "참가자 아이디가 [1, 2, 3, 4, 5]이고 인터뷰어 수가 2명인 팀에서 아이디가 {0}인 타겟 멤버에 대해 아이디가 {1}인 참가자의 인터뷰 역할은 {2}이다.")
+        @CsvSource(value = {"1,2,INTERVIEWER", "1,3,INTERVIEWER", "1,4,OBSERVER", "1,5,OBSERVER"})
+        void success(final Long targetMemberId, final Long memberId, final InterviewRole expected) {
+            // given
+            final int interviewerNumber = 2;
+            final TeamDetail teamDetail = new TeamDetail("레벨로그팀", "선릉 트랙룸", TEAM_START_TIME, "레벨로그팀.com",
+                    interviewerNumber);
+            final List<Long> participantIds = List.of(1L, 2L, 3L, 4L, 5L);
+            final ParticipantsIngredient ingredient = new ParticipantsIngredient(1L, participantIds,
+                    Collections.emptyList());
+            final Team team = MockEntityFactory.setId(1L, new Team(teamDetail, ingredient));
+
+            final Participants participants = team.getParticipants();
+
+            // when
+            final InterviewRole actual = participants.toInterviewRole(targetMemberId, memberId, interviewerNumber);
+
+            // then
+            assertThat(actual).isEqualTo(expected);
+        }
+
+        @Test
+        @DisplayName("타겟 멤버가 요청한 멤버와 같으면 ME를 반환한다.")
+        void toInterviewRole_me_success() {
+            // given
+            final int interviewerNumber = 2;
+            final TeamDetail teamDetail = new TeamDetail("레벨로그팀", "선릉 트랙룸", TEAM_START_TIME, "레벨로그팀.com",
+                    interviewerNumber);
+            final List<Long> participantIds = List.of(1L, 2L, 3L, 4L, 5L);
+            final ParticipantsIngredient ingredient = new ParticipantsIngredient(1L, participantIds,
+                    Collections.emptyList());
+            final Team team = MockEntityFactory.setId(1L, new Team(teamDetail, ingredient));
+
+            final Participants participants = team.getParticipants();
+
+            // when
+            final InterviewRole actual = participants.toInterviewRole(1L, 1L, interviewerNumber);
+
+            // then
+            assertThat(actual).isEqualTo(InterviewRole.ME);
+        }
     }
-
-    private List<Long> toIdList(final String s) {
-        return Arrays.stream(s.split(", "))
-                .map(Long::parseLong)
-                .collect(Collectors.toList());
-    }
-
-//    @Nested
-//    @DisplayName("toInterviewRole 메서드는")
-//    class ToInterviewRole {
-//
-//        @ParameterizedTest(name = "참가자 아이디가 [1, 2, 3, 4, 5]이고 인터뷰어 수가 2명인 팀에서 아이디가 {0}인 타겟 멤버에 대해 아이디가 {1}인 참가자의 인터뷰 역할은 {2}이다.")
-//        @CsvSource(value = {"1,2,INTERVIEWER", "1,3,INTERVIEWER", "1,4,OBSERVER", "1,5,OBSERVER"})
-//        void success(final Long targetMemberId, final Long memberId, final InterviewRole expected) {
-//            // given
-//            final int interviewerNumber = 2;
-//            final Team team = MockEntityFactory.setId(1L,
-//                    new Team("레벨로그팀", "선릉 트랙룸", TEAM_START_TIME, "레벨로그팀.com", interviewerNumber));
-//
-//            final List<Participant> values = List.of(
-//                    new Participant(null, 1L, true, false),
-//                    new Participant(null, 2L, false, false),
-//                    new Participant(null, 3L, false, false),
-//                    new Participant(null, 4L, false, false),
-//                    new Participant(null, 5L, false, false));
-//            final Participants participants = new Participants(values);
-//
-//            // when
-//            final InterviewRole actual = participants.toInterviewRole(team.getId(), targetMemberId, memberId,
-//                    interviewerNumber);
-//
-//            // then
-//            assertThat(actual).isEqualTo(expected);
-//        }
-//
-//        @Test
-//        @DisplayName("타겟 멤버가 요청한 멤버와 같으면 ME를 반환한다.")
-//        void toInterviewRole_me_success() {
-//            // given
-//            final int interviewerNumber = 2;
-//            final Team team = MockEntityFactory.setId(1L,
-//                    new Team("레벨로그팀", "선릉 트랙룸", TEAM_START_TIME, "레벨로그팀.com", interviewerNumber));
-//            final List<Participant> values = List.of(
-//                    new Participant(team, 1L, true, false),
-//                    new Participant(team, 2L, false, false),
-//                    new Participant(team, 3L, false, false),
-//                    new Participant(team, 4L, false, false),
-//                    new Participant(team, 5L, false, false));
-//            final Participants participants = new Participants(values);
-//
-//            // when
-//            final InterviewRole actual = participants.toInterviewRole(team.getId(), 1L, 1L, interviewerNumber);
-//
-//            // then
-//            assertThat(actual).isEqualTo(InterviewRole.ME);
-//        }
-//
-//        @Test
-//        @DisplayName("요청한 멤버가 참가자가 아니라면 예외를 던진다.")
-//        void toInterviewRole_requestMemberIdNotContains_exception() {
-//            // given
-//            final int interviewerNumber = 2;
-//            final Team team = MockEntityFactory.setId(1L,
-//                    new Team("레벨로그팀", "선릉 트랙룸", TEAM_START_TIME, "레벨로그팀.com", interviewerNumber));
-//
-//            final List<Participant> values = List.of(
-//                    new Participant(team, 1L, true, false),
-//                    new Participant(team, 2L, false, false),
-//                    new Participant(team, 3L, false, false),
-//                    new Participant(team, 4L, false, false),
-//                    new Participant(team, 5L, false, false));
-//            final Participants participants = new Participants(values);
-//
-//            // when & then
-//            assertThatThrownBy(() -> participants.toInterviewRole(team.getId(), 1L, 9L, interviewerNumber))
-//                    .isInstanceOf(ParticipantNotSameTeamException.class);
-//        }
-//
-//        @Test
-//        @DisplayName("타겟 멤버가 참가자가 아니라면 예외를 던진다.")
-//        void toInterviewRole_targetMemberIdNotContains_exception() {
-//            // given
-//            final int interviewerNumber = 2;
-//            final Team team = MockEntityFactory.setId(1L,
-//                    new Team("레벨로그팀", "선릉 트랙룸", TEAM_START_TIME, "레벨로그팀.com", interviewerNumber));
-//
-//            final List<Participant> values = List.of(
-//                    new Participant(team, 1L, true, false),
-//                    new Participant(team, 2L, false, false),
-//                    new Participant(team, 3L, false, false),
-//                    new Participant(team, 4L, false, false),
-//                    new Participant(team, 5L, false, false));
-//            final Participants participants = new Participants(values);
-//
-//            // when & then
-//            assertThatThrownBy(() -> participants.toInterviewRole(team.getId(), 9L, 1L, interviewerNumber))
-//                    .isInstanceOf(ParticipantNotFoundException.class);
-//        }
-//    }
 }

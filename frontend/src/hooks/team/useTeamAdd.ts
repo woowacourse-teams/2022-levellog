96a668b0922191fc802274af44529264117ba4b4
(yunjin) <sion11290805@gmail.com>
2022-10-11 01:29:33 +0900

    feat: 팀 추가 커스텀 훅 수정

--- frontend/src/hooks/team/useTeamAdd.ts
+++ frontend/src/hooks/team/useTeamAdd.ts
@@ -1,19 +1,14 @@
-import { useEffect, useRef, useState } from 'react';
+import { useRef, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import axios, { AxiosResponse } from 'axios';
-import { queryClient } from 'index';
+import { useMutation } from '@tanstack/react-query';
 
-import { useMutation, useQuery } from '@tanstack/react-query';
-
-import useTeam from 'hooks/team/useTeam';
 import useSnackbar from 'hooks/utils/useSnackbar';
 
 import { MESSAGE, ROUTES_PATH } from 'constants/constants';
 
-import { requestGetMembers } from 'apis/member';
 import { requestPostTeam } from 'apis/teams';
-import { MembersCustomHookType, MemberType } from 'types/member';
+import { MemberContext } from 'contexts/memberContext';
 import { TeamCustomHookType } from 'types/team';
 import {
   interviewDateValidate,
@@ -25,47 +20,12 @@ import {
 } from 'utils/validate';
 
 const useTeamAdd = () => {
-  const { participants, watchers, members, setParticipants, setWatchers } = useTeam();
   const { showSnackbar } = useSnackbar();
+  const participantAndWatcher = useContext(MemberContext);
   const teamInfoRef = useRef<HTMLInputElement[]>([]);
-
-  const [participantNicknameValue, setParticipantNicknameValue] = useState('');
-  const [watcherNicknameValue, setWatcherNicknameValue] = useState('');
-  const [participantMembers, setParticipantMembers] = useState<MemberType[]>([]);
-  const [watcherMembers, setWatcherMembers] = useState<MemberType[]>([]);
   const navigate = useNavigate();
 
   const accessToken = localStorage.getItem('accessToken');
-
-  const convertTeamInfoFormat = () => {
-    const [title, place, date, time, interviewerNumber] = teamInfoRef.current;
-    const teamInfo = {
-      title: title.value,
-      place: place.value,
-      startAt: `${date.value}T${time.value}`,
-      interviewerNumber: Number(interviewerNumber.value),
-      watcherIds: Object.values(watchers).map((watcher) => watcher.id),
-      participantIds: Object.values(participants).map((participant) => participant.id),
-    };
-
-    return teamInfo;
-  };
-
-  const validateTeamInfo = () => {
-    const [title, place, date, time, interviewerNumber] = teamInfoRef.current;
-    if (
-      interviewTitleValidate({ value: title.value }) &&
-      interviewLocationValidate({ value: place.value }) &&
-      interviewDateValidate({ value: date.value }) &&
-      interviewTimeValidate({ value: time.value }) &&
-      interviewInterviewerValidate({ value: +interviewerNumber.value })
-      // interviewParticipantValidate({ value: participants.length })
-    ) {
-      return true;
-    }
-
-    return false;
-  };
 
   const { mutate: postTeam } = useMutation(
     ({ teamInfo }: Record<'teamInfo', TeamCustomHookType>) => {
@@ -82,82 +42,9 @@ const useTeamAdd = () => {
     },
   );
 
-  const { mutate: getMembersInParticipants } = useMutation(
-    ({ nicknameValue = ' ' }: MembersCustomHookType) => {
-      return requestGetMembers({ accessToken, nickname: nicknameValue });
-    },
-    {
-      onSuccess: (res) => {
-        const membersFilter = res.members
-          .filter((member) => participants.every((participant) => participant.id !== member.id))
-          .filter((member) => watchers.every((watcher) => watcher.id !== member.id));
-        setParticipantMembers(membersFilter);
-      },
-      onError: (err) => {
-        if (axios.isAxiosError(err)) {
-          const responseBody: AxiosResponse = err.response!;
-          if (err instanceof Error) {
-            showSnackbar({ message: responseBody.data.message });
-          }
-        }
-      },
-    },
-  );
-
-  const { mutate: getMembersInWatchers } = useMutation(
-    ({ nicknameValue = ' ' }: MembersCustomHookType) => {
-      return requestGetMembers({ accessToken, nickname: nicknameValue });
-    },
-    {
-      onSuccess: (res) => {
-        const membersFilter = res.members
-          .filter((member) => watchers.every((watcher) => watcher.id !== member.id))
-          .filter((member) => participants.every((participant) => participant.id !== member.id));
-        setWatcherMembers(membersFilter);
-      },
-      onError: (err) => {
-        if (axios.isAxiosError(err)) {
-          const responseBody: AxiosResponse = err.response!;
-          if (err instanceof Error) {
-            showSnackbar({ message: responseBody.data.message });
-          }
-        }
-      },
-    },
-  );
-
-  const updateMembers = async ({ nicknameValue = ' ' }: MembersCustomHookType) => {
-    // if (isDebounce()) return;
-    getMembersInParticipants({ nicknameValue });
-  };
-
-  const updateWatchers = async ({ nicknameValue = ' ' }: MembersCustomHookType) => {
-    // if (isDebounce()) return;
-    getMembersInWatchers({ nicknameValue });
-  };
-
-  const addToWatcherParticipants = ({ id, nickname, profileUrl }: MemberType) => {
-    if (watchers.every((watcher) => id !== watcher.id)) {
-      setWatchers((prev) => prev.concat({ id, nickname, profileUrl }));
-    }
-  };
-
-  const addToParticipants = ({ id, nickname, profileUrl }: MemberType) => {
-    if (participants.every((participant) => id !== participant.id)) {
-      setParticipants((prev) => prev.concat({ id, nickname, profileUrl }));
-    }
-  };
-
-  const removeToParticipants = ({ id }: Pick<MemberType, 'id'>) => {
-    setParticipants(participants.filter((participant) => id !== participant.id));
-  };
-
-  const removeToWatcherParticipants = ({ id }: Pick<MemberType, 'id'>) => {
-    setWatchers(watchers.filter((watcher) => id !== watcher.id));
-  };
-
   const handleClickTeamAddButton = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
+
     if (validateTeamInfo()) {
       postTeam({ teamInfo: convertTeamInfoFormat() });
 
@@ -166,40 +53,41 @@ const useTeamAdd = () => {
     showSnackbar({ message: MESSAGE.INTERVIEW_HOLE_VALUE_VALIDATE });
   };
 
-  const handleChangeParticipantInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setParticipantNicknameValue(e.target.value);
+  const validateTeamInfo = () => {
+    const [title, place, date, time, interviewerNumber] = teamInfoRef.current;
+
+    if (
+      interviewTitleValidate({ value: title.value }) &&
+      interviewLocationValidate({ value: place.value }) &&
+      interviewDateValidate({ value: date.value }) &&
+      interviewTimeValidate({ value: time.value }) &&
+      interviewInterviewerValidate({ value: +interviewerNumber.value }) &&
+      interviewParticipantValidate({ value: participantAndWatcher.participants.length })
+    ) {
+      return true;
+    }
+
+    return false;
   };
 
-  const handleChangeWatcherInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setWatcherNicknameValue(e.target.value);
-  };
+  const convertTeamInfoFormat = () => {
+    const [title, place, date, time, interviewerNumber] = teamInfoRef.current;
+    const teamInfo = {
+      title: title.value,
+      place: place.value,
+      startAt: `${date.value}T${time.value}`,
+      interviewerNumber: Number(interviewerNumber.value),
+      watcherIds: Object.values(participantAndWatcher.watchers).map((watcher) => watcher.id),
+      participantIds: Object.values(participantAndWatcher.participants).map(
+        (participant) => participant.id,
+      ),
+    };
 
-  useEffect(() => {
-    const membersFilter = members
-      .filter((member) => watchers.every((watcher) => watcher.id !== member.id))
-      .filter((member) => participants.every((participant) => participant.id !== member.id));
-    setParticipantMembers(membersFilter);
-    setWatcherMembers(membersFilter);
-  }, [participants, watchers]);
+    return teamInfo;
+  };
 
   return {
-    participantNicknameValue,
-    watcherNicknameValue,
-    watcherMembers,
-    participantMembers,
-    watchers,
-    participants,
     teamInfoRef,
-    setParticipantNicknameValue,
-    setWatcherNicknameValue,
-    updateMembers,
-    updateWatchers,
-    addToParticipants,
-    addToWatcherParticipants,
-    removeToParticipants,
-    removeToWatcherParticipants,
-    handleChangeParticipantInput,
-    handleChangeWatcherInput,
     handleClickTeamAddButton,
   };
 };

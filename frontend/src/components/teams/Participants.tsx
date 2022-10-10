dfa218da409311e5a1732a4bfe8dc0c77e182eb9
(yunjin) <sion11290805@gmail.com>
2022-10-10 15:33:36 +0900

    style: px -> rem 으로 변경

--- frontend/src/components/teams/Participants.tsx
+++ frontend/src/components/teams/Participants.tsx
@@ -34,7 +34,7 @@ const S = {
     gap: 0.25rem;
     padding: 0.5rem;
     box-shadow: 0.25rem 0.25rem 0.25rem ${(props) => props.theme.new_default.GRAY};
-    border: 1px solid ${(props) => props.theme.new_default.LIGHT_GRAY};
+    border: 0.0625rem solid ${(props) => props.theme.new_default.LIGHT_GRAY};
     border-radius: 1.5625rem;
     background-color: ${(props) => props.theme.new_default.WHITE};
     font-size: 1rem;

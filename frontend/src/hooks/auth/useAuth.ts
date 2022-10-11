56e2cd6ac208a481a09ddcb2b28b85467d884e6c
(yunjin) <sion11290805@gmail.com>
2022-10-11 15:35:09 +0900

    feat: useAuth 커스텀 훅 수정
    useQuery onSuccess에서 state 변경을 하는 것이 아닌 useMutate onSuccess에서 하는 것으로 변경한다.

--- frontend/src/hooks/auth/useAuth.ts
+++ frontend/src/hooks/auth/useAuth.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useMutation } from '@tanstack/react-query';
 
 import useSnackbar from 'hooks/utils/useSnackbar';
 
@@ -9,102 +9,99 @@ import { MESSAGE, REQUIRE_AUTH } from 'constants/constants';
 
 import { requestGetTeam } from 'apis/teams';
 
-const QUERY_KEY = {
-  TEAM: 'team',
-};
 const useAuth = ({ requireAuth }: AuthCustomHookProps) => {
-  const { teamId } = useParams();
-  const accessToken = localStorage.getItem('accessToken');
-
-  const { data: team } = useQuery(
-    [QUERY_KEY.TEAM],
-    () => {
-      return requestGetTeam({ accessToken, teamId });
-    },
-    {
-      onSuccess: () => {
-        checkAuth();
-      },
-    },
-  );
   const { showSnackbar } = useSnackbar();
   const [isLoad, setIsLoad] = useState(true);
   const [isError, setIsError] = useState(false);
   const { levellogId, authorId } = useParams();
+  const { teamId } = useParams();
+
+  const accessToken = localStorage.getItem('accessToken');
+
+  const { mutate: getTeam } = useMutation(
+    () => {
+      return requestGetTeam({ accessToken, teamId });
+    },
+    {
+      onSuccess: (res) => {
+        if (!res) {
+          setIsLoad(false);
+          setIsError(true);
+          showSnackbar({ message: MESSAGE.WRONG_ACCESS });
+
+          return;
+        }
+
+        const idsAndLevellogIds = Object.values(res.participants).map((participant) => [
+          participant.memberId,
+          participant.levellogId,
+        ]);
+
+        if (requireAuth === (REQUIRE_AUTH.IN_TEAM || REQUIRE_AUTH.NOT_ME)) {
+          if (
+            res &&
+            Object.values(res.participants)
+              .map((participant) => participant.memberId)
+              .every((memberId) => {
+                return String(memberId) !== String(loginUserId);
+              }) &&
+            Object.values(res.watchers)
+              .map((watcher) => watcher.memberId)
+              .every((memberId) => {
+                return String(memberId) !== String(loginUserId);
+              })
+          ) {
+            setIsLoad(false);
+            setIsError(true);
+            showSnackbar({ message: MESSAGE.NEED_IN_TEAM });
+
+            return;
+          }
+        }
+
+        if (requireAuth === REQUIRE_AUTH.HOST) {
+          if (String(res.hostId) !== String(loginUserId)) {
+            setIsLoad(false);
+            setIsError(true);
+            showSnackbar({ message: MESSAGE.NEED_HOST });
+
+            return;
+          }
+        }
+
+        if (requireAuth === REQUIRE_AUTH.NOT_ME) {
+          if (
+            idsAndLevellogIds.some(
+              (idAndLevellogId) =>
+                String(levellogId) === String(idAndLevellogId[1]) &&
+                String(loginUserId) === String(idAndLevellogId[0]),
+            )
+          ) {
+            setIsLoad(false);
+            setIsError(true);
+            showSnackbar({ message: MESSAGE.NEED_NOT_ME });
+
+            return;
+          }
+        }
+
+        if (requireAuth === REQUIRE_AUTH.AUTHOR) {
+          if (String(loginUserId) !== authorId) {
+            setIsLoad(false);
+            setIsError(true);
+            showSnackbar({ message: MESSAGE.NEED_AUTHOR });
+          }
+        }
+        setIsLoad(false);
+      },
+    },
+  );
 
   const loginUserId = localStorage.getItem('userId');
 
-  const checkAuth = () => {
-    if (!team) {
-      setIsLoad(false);
-      setIsError(true);
-      showSnackbar({ message: MESSAGE.WRONG_ACCESS });
-
-      return;
-    }
-
-    const idsAndLevellogIds = Object.values(team.data.participants).map((participant) => [
-      participant.memberId,
-      participant.levellogId,
-    ]);
-
-    if (requireAuth === (REQUIRE_AUTH.IN_TEAM || REQUIRE_AUTH.NOT_ME)) {
-      if (
-        team &&
-        Object.values(team.data.participants)
-          .map((participant) => participant.memberId)
-          .every((memberId) => {
-            return String(memberId) !== String(loginUserId);
-          }) &&
-        Object.values(team.data.watchers)
-          .map((watcher) => watcher.memberId)
-          .every((memberId) => {
-            return String(memberId) !== String(loginUserId);
-          })
-      ) {
-        setIsLoad(false);
-        setIsError(true);
-        showSnackbar({ message: MESSAGE.NEED_IN_TEAM });
-
-        return;
-      }
-    }
-
-    if (requireAuth === REQUIRE_AUTH.HOST) {
-      if (String(team.data.hostId) !== String(loginUserId)) {
-        setIsLoad(false);
-        setIsError(true);
-        showSnackbar({ message: MESSAGE.NEED_HOST });
-
-        return;
-      }
-    }
-
-    if (requireAuth === REQUIRE_AUTH.NOT_ME) {
-      if (
-        idsAndLevellogIds.some(
-          (idAndLevellogId) =>
-            String(levellogId) === String(idAndLevellogId[1]) &&
-            String(loginUserId) === String(idAndLevellogId[0]),
-        )
-      ) {
-        setIsLoad(false);
-        setIsError(true);
-        showSnackbar({ message: MESSAGE.NEED_NOT_ME });
-
-        return;
-      }
-    }
-
-    if (requireAuth === REQUIRE_AUTH.AUTHOR) {
-      if (String(loginUserId) !== authorId) {
-        setIsLoad(false);
-        setIsError(true);
-        showSnackbar({ message: MESSAGE.NEED_AUTHOR });
-      }
-    }
-    setIsLoad(false);
-  };
+  useEffect(() => {
+    getTeam();
+  }, []);
 
   return { isLoad, isError };
 };

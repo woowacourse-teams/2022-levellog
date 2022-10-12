8d7defdde7b6350103e89d0064ea3306f0873775
SuyeonChoi <tndus970120@gmail.com>
2022-10-12 21:22:52 +0900

    refactor: mariaDB를 연결했을 때 깨지는 쿼리 및 테스트 수정 (#485)
    * feat: mariaDB와 연결했을 때 깨지는 쿼리 및 코드 변경
    
    * feat: TestContainer Docker-compose 환경으로 데이터베이스 연결
    
    * refactor: 사용하지 않는 메서드 제거
    
    * feat: TestContainer 제거
    
    * fix: my.cnf 추가
    
    * refactor: 테스트 환경에서 sql 문이 중복으로 출력되는 오류 해결
    
    * refactor: 불필요한 필드 조회 제거
    
    * refactor: 불필요한 필드 조회 제거

--- backend/src/test/java/com/woowacourse/levellog/application/PreQuestionServiceTest.java
+++ backend/src/test/java/com/woowacourse/levellog/application/PreQuestionServiceTest.java
@@ -177,7 +177,7 @@ public class PreQuestionServiceTest extends ServiceTest {
             // when, then
             assertThatThrownBy(() -> preQuestionService.findMy(levellog.getId(), getLoginStatus(author)))
                     .isInstanceOf(PreQuestionNotFoundException.class)
-                    .hasMessageContainingAll("사전 질문이 존재하지 않습니다.", "1");
+                    .hasMessageContainingAll("사전 질문이 존재하지 않습니다.", String.valueOf(levellog.getId()));
         }
     }
 

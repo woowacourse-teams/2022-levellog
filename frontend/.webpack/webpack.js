283364001ffab6613a009fb20b182f35b5e6bd4e
jihyeok-um <visualstudio530@gmail.com>
2022-10-16 21:07:18 +0900

    chore: 사용하지 않는 plugin 제거

--- frontend/.webpack/webpack.js
+++ frontend/.webpack/webpack.js
@@ -1,7 +1,6 @@
 const webpack = require('webpack');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const { ESBuildMinifyPlugin } = require('esbuild-loader');
 
 module.exports = merge(common, {
   mode: 'development',

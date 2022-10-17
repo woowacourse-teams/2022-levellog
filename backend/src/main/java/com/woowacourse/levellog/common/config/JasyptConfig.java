140f9f3648ef8733fa8bd0e611415f6f1c4e2b69
ozragwort <ozragwort@gmail.com>
2022-10-17 20:39:41 +0900

    chore: mail, jasypt 설정 수정
    Signed-off-by: ozragwort <ozragwort@gmail.com>

---
+++ backend/src/main/java/com/woowacourse/levellog/common/config/JasyptConfig.java
@@ -0,0 +1,31 @@
+package com.woowacourse.levellog.common.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptConfig {
+
+    @Value("${jasypt.encryptor.password}")
+    private String password;
+
+    @Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+        final PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        final SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+
+        config.setPassword(password);
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+
+        return encryptor;
+    }
+}

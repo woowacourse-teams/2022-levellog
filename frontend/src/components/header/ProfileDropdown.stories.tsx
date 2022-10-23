6ad7a4c680a110c31b4188322c6ebb610ede60fd
(yunjin) <sion11290805@gmail.com>
2022-10-23 15:50:58 +0900

    test: ProfileDropdown 스토리북 추가

---
+++ frontend/src/components/header/ProfileDropdown.stories.tsx
@@ -0,0 +1,19 @@
+import ProfileDropdown from './ProfileDropdown';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+export default {
+  title: 'header/ProfileDropdown',
+  component: ProfileDropdown,
+} as ComponentMeta<typeof ProfileDropdown>;
+
+const HeaderTemplate: ComponentStory<typeof ProfileDropdown> = (args) => (
+  <ProfileDropdown {...args} />
+);
+
+export const Base = HeaderTemplate.bind({});
+
+Base.args = {
+  isShowProfileDropdown: true,
+  loginUserNickname: '결',
+  handleClickLogoutButton: () => {},
+};

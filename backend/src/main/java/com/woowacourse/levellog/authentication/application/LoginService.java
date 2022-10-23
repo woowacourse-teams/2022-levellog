9b30f2df5b466767947cd106b146a0084657ff06
SuyeonChoi <tndus970120@gmail.com>
2022-10-23 14:06:57 +0900

    refactor: 트랜잭션 범위에서 외부 리소스 요청 분리 (#511)
    * refactor: Github API를 호출하는 로직을 트랜잭션 범위 밖으로 분리
    
    * feat: RestTemplate 사용 시 connect timeout, read timeout 설정
    
    * refactor: 컨트롤러에서 두개의 서비스를 의존하도록 변경
    
    클래스명 변경
    - MemberLogin ➡️ OAuthService
    - OAuthService ➡️ LoginService
    
    * test: 사용하지 않는 인자 제거
    
    * refactor: 메서드명 변경

---
+++ backend/src/main/java/com/woowacourse/levellog/authentication/application/LoginService.java
@@ -0,0 +1,32 @@
+package com.woowacourse.levellog.authentication.application;
+
+import com.woowacourse.levellog.authentication.dto.response.GithubProfileResponse;
+import com.woowacourse.levellog.authentication.dto.response.LoginResponse;
+import com.woowacourse.levellog.authentication.support.JwtTokenProvider;
+import com.woowacourse.levellog.member.application.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final MemberService memberService;
+
+    @Transactional
+    public LoginResponse login(final GithubProfileResponse githubProfile) {
+        final Long memberId = getMemberIdByGithubProfile(githubProfile);
+        final String token = jwtTokenProvider.createToken(memberId.toString());
+
+        return new LoginResponse(memberId, token, githubProfile.getNickname(), githubProfile.getProfileUrl());
+    }
+
+    private Long getMemberIdByGithubProfile(final GithubProfileResponse githubProfile) {
+        final int githubId = Integer.parseInt(githubProfile.getGithubId());
+
+        return memberService.saveIfNotExist(githubProfile, githubId);
+    }
+}

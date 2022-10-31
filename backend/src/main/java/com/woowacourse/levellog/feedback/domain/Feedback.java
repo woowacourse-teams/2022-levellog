545a50b13d92cd8176fdd9523cc8df3a949cc895
nailseong <nnis9857@gmail.com>
2022-10-31 13:30:13 +0900

    refactor: 피드백 길이 관련 코드 & 테스트 제거

--- backend/src/main/java/com/woowacourse/levellog/feedback/domain/Feedback.java
+++ backend/src/main/java/com/woowacourse/levellog/feedback/domain/Feedback.java
@@ -1,7 +1,6 @@
 package com.woowacourse.levellog.feedback.domain;
 
 import com.woowacourse.levellog.common.domain.BaseEntity;
-import com.woowacourse.levellog.common.exception.InvalidFieldException;
 import com.woowacourse.levellog.common.support.DebugMessage;
 import com.woowacourse.levellog.feedback.exception.InvalidFeedbackException;
 import com.woowacourse.levellog.levellog.domain.Levellog;
@@ -21,7 +20,6 @@ import lombok.NoArgsConstructor;
 @Getter
 public class Feedback extends BaseEntity {
 
-    private static final int FEEDBACK_CONTENT_MAX_LENGTH = 1000;
     private static final String CONTENT_TYPE_SPEAK = "Speak";
     private static final String CONTENT_TYPE_ETC = "Etc";
     private static final String CONTENT_TYPE_STUDY = "Study";
@@ -47,8 +45,6 @@ public class Feedback extends BaseEntity {
 
     public Feedback(final Long fromId, final Levellog levellog, final String study, final String speak,
                     final String etc) {
-        validateFeedback(study, speak, etc);
-
         this.fromId = fromId;
         this.toId = levellog.getAuthorId();
         this.levellog = levellog;
@@ -57,23 +53,7 @@ public class Feedback extends BaseEntity {
         this.etc = etc;
     }
 
-    private void validateFeedback(final String study, final String speak, final String etc) {
-        validateFeedbackLength(study, CONTENT_TYPE_STUDY);
-        validateFeedbackLength(speak, CONTENT_TYPE_SPEAK);
-        validateFeedbackLength(etc, CONTENT_TYPE_ETC);
-    }
-
-    private void validateFeedbackLength(final String feedbackContent, final String contentType) {
-        if (feedbackContent.length() > FEEDBACK_CONTENT_MAX_LENGTH) {
-            throw new InvalidFieldException(contentType + " 피드백은 " + FEEDBACK_CONTENT_MAX_LENGTH + "자 이하여야 합니다.",
-                    DebugMessage.init()
-                            .append("content 길이", feedbackContent.length()));
-        }
-    }
-
     public void updateFeedback(final String study, final String speak, final String etc) {
-        validateFeedback(study, speak, etc);
-
         this.study = study;
         this.speak = speak;
         this.etc = etc;

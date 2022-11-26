8949bb1aba597c7d34c8e08d6b03bd1cff44491e
SuyeonChoi <tndus970120@gmail.com>
2022-11-26 23:22:53 +0900

    refactor: 픽스처 사용

--- backend/src/test/java/com/woowacourse/levellog/acceptance/ConcurrentAcceptanceTest.java
+++ backend/src/test/java/com/woowacourse/levellog/acceptance/ConcurrentAcceptanceTest.java
@@ -2,20 +2,16 @@ package com.woowacourse.levellog.acceptance;
 
 import static com.woowacourse.levellog.fixture.MemberFixture.EVE;
 import static com.woowacourse.levellog.fixture.MemberFixture.PEPPER;
+import static com.woowacourse.levellog.fixture.RestAssuredTemplate.get;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.woowacourse.levellog.feedback.dto.request.FeedbackWriteRequest;
 import com.woowacourse.levellog.feedback.dto.response.FeedbackResponses;
-import com.woowacourse.levellog.levellog.dto.request.LevellogWriteRequest;
 import com.woowacourse.levellog.team.dto.response.TeamDetailResponse;
-import io.restassured.RestAssured;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 
 @DisplayName("동시성 관련 기능")
 class ConcurrentAcceptanceTest extends AcceptanceTest {
@@ -33,8 +29,6 @@ class ConcurrentAcceptanceTest extends AcceptanceTest {
         EVE.save();
         final String teamId = saveTeam("잠실 제이슨조", PEPPER, 1, PEPPER, EVE).getTeamId();
         final String levellogId = saveLevellog("동시성을 학습했습니다.", teamId, PEPPER).getLevellogId();
-        final FeedbackWriteRequest request = new FeedbackWriteRequest("동시성에 대한 학습을 충분히 하였습니다.",
-                "아이 컨텍이 좋습니다.", "자신감 있게 말해보세요.");
         timeStandard.setInProgress();
 
         final ExecutorService executorService = Executors.newFixedThreadPool(2);
@@ -43,28 +37,16 @@ class ConcurrentAcceptanceTest extends AcceptanceTest {
         // when
         for (int i = 0; i < 2; i++) {
             executorService.execute(() -> {
-                RestAssured.given(specification).log().all()
-                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + EVE.getToken())
-                        .body(request)
-                        .contentType(MediaType.APPLICATION_JSON_VALUE)
-                        .when()
-                        .post("/api/levellogs/{levellogId}/feedbacks", levellogId)
-                        .then().log().all();
+                saveFeedback("이브가 페퍼의 레벨로그에 작성한 피드백", levellogId, EVE);
                 countDownLatch.countDown();
             });
         }
         countDownLatch.await();
 
         // then
-        final FeedbackResponses response = RestAssured.given(specification).log().all()
-                .header(HttpHeaders.AUTHORIZATION, "Bearer " + PEPPER.getToken())
-                .when()
-                .get("/api/levellogs/{levellogId}/feedbacks", levellogId)
-                .then().log().all()
-                .extract().body().as(FeedbackResponses.class);
-
-        final long numberOfPepperFeedbacks = response.getFeedbacks()
-                .stream()
+        final FeedbackResponses response = get("/api/levellogs/" + levellogId + "/feedbacks", PEPPER.getToken())
+                .getResponse().extract().body().as(FeedbackResponses.class);
+        final long numberOfPepperFeedbacks = response.getFeedbacks().stream()
                 .filter(it -> it.getFrom().getId().equals(EVE.getId()) && it.getTo().getId().equals(PEPPER.getId()))
                 .count();
         assertThat(numberOfPepperFeedbacks).isEqualTo(1);
@@ -82,7 +64,6 @@ class ConcurrentAcceptanceTest extends AcceptanceTest {
         PEPPER.save();
         EVE.save();
         final String teamId = saveTeam("잠실 제이슨조", PEPPER, 1, PEPPER, EVE).getTeamId();
-        final LevellogWriteRequest request = new LevellogWriteRequest("Spring과 React를 학습했습니다.");
 
         final ExecutorService executorService = Executors.newFixedThreadPool(2);
         final CountDownLatch countDownLatch = new CountDownLatch(2);
@@ -90,25 +71,14 @@ class ConcurrentAcceptanceTest extends AcceptanceTest {
         // when
         for (int i = 0; i < 2; i++) {
             executorService.execute(() -> {
-                RestAssured.given(specification).log().all()
-                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + PEPPER.getToken())
-                        .body(request)
-                        .contentType(MediaType.APPLICATION_JSON_VALUE)
-                        .when()
-                        .post("/api/teams/{teamId}/levellogs", teamId)
-                        .then().log().all();
+                saveLevellog("Spring과 React를 학습했습니다.", teamId, PEPPER);
                 countDownLatch.countDown();
             });
         }
         countDownLatch.await();
 
         // then
-        final TeamDetailResponse response = RestAssured.given(specification).log().all()
-                .header(HttpHeaders.AUTHORIZATION, "Bearer " + PEPPER.getToken())
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when()
-                .get("/api/teams/{teamId}", teamId)
-                .then().log().all()
+        final TeamDetailResponse response = get("/api/teams/" + teamId, PEPPER.getToken()).getResponse()
                 .extract().body().as(TeamDetailResponse.class);
         final long numberOfLevellogs = response.getParticipants()
                 .stream()
